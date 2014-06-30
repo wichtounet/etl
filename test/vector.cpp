394eeaf7ad15864953c4639a8697ccd6b3177b8f
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-06-30 19:58:23 +0200

    Upgrade test

--- test/vector.cpp
+++ test/vector.cpp
@@ -34,10 +34,20 @@ TEST_CASE( "fast_vector/init_3", "fast_vector::fast_vector(initializer_list)" ) 
     REQUIRE(test_vector[2] == 3.0);
 }
 
-TEST_CASE( "fast_vector/add_scalar", "fast_vector::operator+" ) {
+TEST_CASE( "fast_vector/add_scalar_1", "fast_vector::operator+" ) {
     etl::fast_vector<double, 3> test_vector = {-1.0, 2.0, 5.5};
 
     test_vector = 1.0 + test_vector;
+
+    REQUIRE(test_vector[0] == 0.0);
+    REQUIRE(test_vector[1] == 3.0);
+    REQUIRE(test_vector[2] == 6.5);
+}
+
+TEST_CASE( "fast_vector/add_scalar_2", "fast_vector::operator+" ) {
+    etl::fast_vector<double, 3> test_vector = {-1.0, 2.0, 5.5};
+
+    test_vector = test_vector + 1.0;
 
     REQUIRE(test_vector[0] == 0.0);
     REQUIRE(test_vector[1] == 3.0);
@@ -55,7 +65,17 @@ TEST_CASE( "fast_vector/add", "fast_vector::operator+" ) {
     REQUIRE(c[2] ==  9.0);
 }
 
-TEST_CASE( "fast_vector/sub_scalar", "fast_vector::operator+" ) {
+TEST_CASE( "fast_vector/sub_scalar_1", "fast_vector::operator+" ) {
+    etl::fast_vector<double, 3> test_vector = {-1.0, 2.0, 5.5};
+
+    test_vector = 1.0 - test_vector;
+
+    REQUIRE(test_vector[0] == 2.0);
+    REQUIRE(test_vector[1] == -1.0);
+    REQUIRE(test_vector[2] == -4.5);
+}
+
+TEST_CASE( "fast_vector/sub_scalar_2", "fast_vector::operator+" ) {
     etl::fast_vector<double, 3> test_vector = {-1.0, 2.0, 5.5};
 
     test_vector = test_vector - 1.0;
@@ -107,13 +127,13 @@ TEST_CASE( "fast_vector/div_scalar", "fast_vector::operator/" ) {
     REQUIRE(test_vector[2] ==  5.0 / 2.5);
 }
 
-TEST_CASE( "fast_vector/mul", "fast_vector::operator*" ) {
+TEST_CASE( "fast_vector/div", "fast_vector::operator/" ) {
     etl::fast_vector<double, 3> a = {-1.0, 2.0, 5.0};
     etl::fast_vector<double, 3> b = {2.5, 3.0, 4.0};
 
-    etl::fast_vector<double, 3> c = a * b;
+    etl::fast_vector<double, 3> c = a / b;
 
-    REQUIRE(c[0] == 1.0 / 2.5);
+    REQUIRE(c[0] == -1.0 / 2.5);
     REQUIRE(c[1] == 2.0 / 3.0);
     REQUIRE(c[2] == 5.0 / 4.0);
 }
