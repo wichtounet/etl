6a01587e488b549c799a811da275d9acf519a14a
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-17 21:55:28 +0200

    Add copyright

--- src/test.cpp
+++ src/test.cpp
@@ -1,3 +1,10 @@
+//=======================================================================
+// Copyright (c) 2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
 #include "etl/fast_vector.hpp"
     
 etl::fast_vector<double, 11> a = {-1.0, 2.0, 5.0, 2.0, 5.0, 1.2, 2.5, 1.2, -3.0, 3.5, 1.0};
--- src/test_dim.cpp
+++ src/test_dim.cpp
@@ -1,3 +1,10 @@
+//=======================================================================
+// Copyright (c) 2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
 #include "etl/fast_matrix.hpp"
 #include "etl/fast_vector.hpp"
 
