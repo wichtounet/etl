e377d9332645ca261747ef7bdc7c8016ec5c81cc
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-17 21:14:01 +0200

    Add test for special cases of assignment

---
+++ test/special_cases.cpp
@@ -0,0 +1,40 @@
+#include "catch.hpp"
+
+#include "etl/fast_vector.hpp"
+#include "etl/fast_matrix.hpp"
+
+TEST_CASE( "deep_assign/vec<mat>", "deep_assign" ) {
+    etl::fast_vector<etl::fast_matrix<double, 2, 3>, 2> a;
+
+    a = 0.0;
+
+    for(auto& v : a){
+        for(auto& v2 : v){
+            REQUIRE(v2 == 0.0);
+        }
+    }
+}
+
+TEST_CASE( "deep_assign/mat<vec>", "deep_assign" ) {
+    etl::fast_matrix<etl::fast_vector<double, 2>, 2, 3> a;
+
+    a = 0.0;
+
+    for(auto& v : a){
+        for(auto& v2 : v){
+            REQUIRE(v2 == 0.0);
+        }
+    }
+}
+
+TEST_CASE( "deep_assign/mat<mat>", "deep_assign" ) {
+    etl::fast_matrix<etl::fast_matrix<double, 2, 3>, 2, 3> a;
+
+    a = 0.0;
+
+    for(auto& v : a){
+        for(auto& v2 : v){
+            REQUIRE(v2 == 0.0);
+        }
+    }
+}
