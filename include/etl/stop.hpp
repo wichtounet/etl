ff525c76e87cfafdd8fa3b84db6f0085f69d1099
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-09-19 15:15:12 +0200

    Don't be too optimist

--- include/etl/stop.hpp
+++ include/etl/stop.hpp
@@ -19,6 +19,7 @@ template<typename T, enable_if_u<
         not_u<etl_traits<T>::is_fast>::value
     >::value> = detail::dummy>
 auto s(const T& value){
+    //Sizes will be directly propagated
     return dyn_matrix<typename T::value_type>(value);
 }
 
@@ -26,10 +27,22 @@ template<typename T, enable_if_u<
     and_u<
         is_etl_expr<T>::value,
         not_u<etl_traits<T>::is_value>::value,
-        etl_traits<T>::is_fast
+        etl_traits<T>::is_fast,
+        etl_traits<T>::is_vector
     >::value> = detail::dummy>
 auto s(const T& value){
     return fast_matrix<typename T::value_type, etl_traits<T>::size()>(value);
+}
+
+template<typename T, enable_if_u<
+    and_u<
+        is_etl_expr<T>::value,
+        not_u<etl_traits<T>::is_value>::value,
+        etl_traits<T>::is_fast,
+        etl_traits<T>::is_matrix
+    >::value> = detail::dummy>
+auto s(const T& value){
+    return fast_matrix<typename T::value_type, etl_traits<T>::template dim<0>(), etl_traits<T>::template dim<1>()>(value);
 }
 
 } //end of namespace etl
