3a7e478fa63995737c564e662f5cdf7d782c40b6
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-08 19:51:05 +0200

    Add test for generators

---
+++ test/generators.cpp
@@ -0,0 +1,73 @@
+//=======================================================================
+// Copyright (c) 2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include "catch.hpp"
+
+#include "etl/fast_matrix.hpp"
+#include "etl/fast_vector.hpp"
+#include "etl/dyn_matrix.hpp"
+#include "etl/dyn_vector.hpp"
+
+///{{{ sequence
+
+TEST_CASE( "sequence/fast_vector_1", "generator" ) {
+    etl::fast_vector<double, 3> b;
+
+    b = etl::sequence_generator();
+
+    REQUIRE(b[0] == 0.0);
+    REQUIRE(b[1] == 1.0);
+    REQUIRE(b[2] == 2.0);
+}
+
+TEST_CASE( "sequence/fast_vector_2", "generator" ) {
+    etl::fast_vector<double, 3> b;
+
+    b = etl::sequence_generator(99);
+
+    REQUIRE(b[0] == 99.0);
+    REQUIRE(b[1] == 100.0);
+    REQUIRE(b[2] == 101.0);
+}
+
+TEST_CASE( "sequence/fast_matrix_1", "generator" ) {
+    etl::fast_matrix<double, 3,2> b;
+
+    b = etl::sequence_generator();
+
+    REQUIRE(b(0,0) == 0.0);
+    REQUIRE(b(0,1) == 1.0);
+    REQUIRE(b(1,0) == 2.0);
+    REQUIRE(b(1,1) == 3.0);
+    REQUIRE(b(2,0) == 4.0);
+    REQUIRE(b(2,1) == 5.0);
+}
+
+TEST_CASE( "sequence/dyn_vector_1", "generator" ) {
+    etl::dyn_vector<double> b(3);
+
+    b = etl::sequence_generator();
+
+    REQUIRE(b[0] == 0.0);
+    REQUIRE(b[1] == 1.0);
+    REQUIRE(b[2] == 2.0);
+}
+
+TEST_CASE( "sequence/dyn_matrix_1", "generator" ) {
+    etl::dyn_matrix<double> b(3,2);
+
+    b = etl::sequence_generator();
+
+    REQUIRE(b(0,0) == 0.0);
+    REQUIRE(b(0,1) == 1.0);
+    REQUIRE(b(1,0) == 2.0);
+    REQUIRE(b(1,1) == 3.0);
+    REQUIRE(b(2,0) == 4.0);
+    REQUIRE(b(2,1) == 5.0);
+}
+
+///}}}
