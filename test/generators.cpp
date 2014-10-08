2314073c19ea3866e46f3f87a428bdc9fdc60460
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-08 19:58:13 +0200

    More tests

--- test/generators.cpp
+++ test/generators.cpp
@@ -12,7 +12,7 @@
 #include "etl/dyn_matrix.hpp"
 #include "etl/dyn_vector.hpp"
 
-///{{{ sequence
+///{{{ sequence_generator
 
 TEST_CASE( "sequence/fast_vector_1", "generator" ) {
     etl::fast_vector<double, 3> b;
@@ -68,6 +68,40 @@ TEST_CASE( "sequence/dyn_matrix_1", "generator" ) {
     REQUIRE(b(1,1) == 3.0);
     REQUIRE(b(2,0) == 4.0);
     REQUIRE(b(2,1) == 5.0);
+}
+
+///}}}
+
+///{{{ normal_generator
+
+TEST_CASE( "normal/fast_vector_1", "generator" ) {
+    etl::fast_vector<double, 3> b;
+
+    b = etl::normal_generator();
+}
+
+TEST_CASE( "normal/fast_vector_2", "generator" ) {
+    etl::fast_vector<double, 3> b;
+
+    b = etl::normal_generator();
+}
+
+TEST_CASE( "normal/fast_matrix_1", "generator" ) {
+    etl::fast_matrix<double, 3,2> b;
+
+    b = etl::normal_generator();
+}
+
+TEST_CASE( "normal/dyn_vector_1", "generator" ) {
+    etl::dyn_vector<double> b(3);
+
+    b = etl::normal_generator();
+}
+
+TEST_CASE( "normal/dyn_matrix_1", "generator" ) {
+    etl::dyn_matrix<double> b(3,2);
+
+    b = etl::normal_generator();
 }
 
 ///}}}
