a788a7d84b4877ab28ccc85d14c85f67f7571616
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-11 10:44:21 +0200

    Check sizes of mmul

--- include/etl/multiplication.hpp
+++ include/etl/multiplication.hpp
@@ -12,12 +12,36 @@
 
 namespace etl {
 
+namespace detail {
+
+template<typename A, typename B, typename C, cpp::disable_if_all_u<etl_traits<A>::is_fast, etl_traits<B>::is_fast, etl_traits<C>::is_fast> = cpp::detail::dummy>
+void check_mmul_sizes(const A& a, const B& b, C& c){
+    cpp_assert(
+            dim(a,1) == dim(b,0)          //interior dimensions
+        &&  dim(a,0) == dim(c,0)          //exterior dimension 1
+        &&  dim(b,1) == dim(c,1),         //exterior dimension 2
+        "Invalid sizes for multiplication");
+    cpp_unused(a);
+    cpp_unused(b);
+    cpp_unused(c);
+}
+
+template<typename A, typename B, typename C, cpp::enable_if_all_u<etl_traits<A>::is_fast, etl_traits<B>::is_fast, etl_traits<C>::is_fast> = cpp::detail::dummy>
+void check_mmul_sizes(const A&, const B&, C&){
+    static_assert(
+            etl_traits<A>::template dim<1>() == etl_traits<B>::template dim<0>()          //interior dimensions
+        &&  etl_traits<A>::template dim<0>() == etl_traits<C>::template dim<0>()          //exterior dimension 1
+        &&  etl_traits<B>::template dim<1>() == etl_traits<C>::template dim<1>(),         //exterior dimension 2
+        "Invalid sizes for multiplication");
+}
+
+} //end of namespace detail
+
 template<typename A, typename B, typename C>
 static C& mmul(const A& a, const B& b, C& c){
     static_assert(is_etl_expr<A>::value && is_etl_expr<B>::value && is_etl_expr<C>::value, "Matrix multiplication only supported for ETL expressions");
     static_assert(etl_traits<A>::dimensions() == 2 && etl_traits<B>::dimensions() == 2 && etl_traits<C>::dimensions() == 2, "Matrix multiplication only works in 2D");
-    //static_assert(A::columns == B::rows, "The central dimensions of the multiplied matrices must be the same");
-    //static_assert(C::rows == A::rows && C::columns == B::columns, "The output matrix is not of the good dimension");
+    detail::check_mmul_sizes(a,b,c);
 
     c = 0;
 
