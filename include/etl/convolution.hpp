ddad52f6c6fda3d1868ad9b53f5b960e4541a753
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-17 10:52:40 +0200

    Validate sizes

--- include/etl/convolution.hpp
+++ include/etl/convolution.hpp
@@ -15,11 +15,76 @@
 
 namespace etl {
 
+template<std::size_t T, typename I, typename K, typename C, cpp::disable_if_all_u<etl_traits<I>::is_fast, etl_traits<K>::is_fast, etl_traits<C>::is_fast> = cpp::detail::dummy>
+void check_conv_1d_sizes(const I& input, const K& kernel, const C& conv){
+    if(T == 1){
+        cpp_assert(dim<0>(conv) == dim<0>(input) + dim<0>(kernel) - 1, "Invalid sizes for 'full' convolution");
+    } else if(T == 2){
+        cpp_assert(dim<0>(conv) == dim<0>(input), "Invalid sizes for 'same' convolution");
+    } else if(T == 3){
+        cpp_assert(dim<0>(conv) == dim<0>(input) - dim<0>(kernel) + 1, "Invalid sizes for 'valid' convolution");
+    }
+    cpp_unused(input);
+    cpp_unused(kernel);
+    cpp_unused(conv);
+}
+
+template<std::size_t T, typename I, typename K, typename C, cpp::enable_if_all_u<etl_traits<I>::is_fast, etl_traits<K>::is_fast, etl_traits<C>::is_fast> = cpp::detail::dummy>
+void check_conv_1d_sizes(const I&, const K&, const C&){
+    static_assert(
+        T != 1 || etl_traits<C>::template dim<0>() == etl_traits<I>::template dim<0>() + etl_traits<K>::template dim<0>() - 1,
+        "Invalid sizes for 'full'convolution");
+    static_assert(
+        T != 2 || etl_traits<C>::template dim<0>() == etl_traits<I>::template dim<0>(),
+        "Invalid sizes for 'same'convolution");
+    static_assert(
+        T != 3 || etl_traits<C>::template dim<0>() == etl_traits<I>::template dim<0>() - etl_traits<K>::template dim<0>() + 1,
+        "Invalid sizes for 'valid'convolution");
+}
+
+template<std::size_t T, typename I, typename K, typename C, cpp::disable_if_all_u<etl_traits<I>::is_fast, etl_traits<K>::is_fast, etl_traits<C>::is_fast> = cpp::detail::dummy>
+void check_conv_2d_sizes(const I& input, const K& kernel, const C& conv){
+    if(T == 1){
+        cpp_assert(
+                dim<0>(conv) == dim<0>(input) + dim<0>(kernel) - 1
+            &&  dim<1>(conv) == dim<1>(input) + dim<1>(kernel) - 1,
+            "Invalid sizes for 'valid' convolution");
+    } else if(T == 2){
+        cpp_assert(dim<0>(conv) == dim<0>(input) && dim<1>(conv) == dim<1>(input), "Invalid sizes for 'same' convolution");
+    } else if(T == 3){
+        cpp_assert(
+                dim<0>(conv) == dim<0>(input) - dim<0>(kernel) + 1
+            &&  dim<1>(conv) == dim<1>(input) - dim<1>(kernel) + 1,
+            "Invalid sizes for 'valid' convolution");
+    }
+    cpp_unused(input);
+    cpp_unused(kernel);
+    cpp_unused(conv);
+}
+
+template<std::size_t T, typename I, typename K, typename C, cpp::enable_if_all_u<etl_traits<I>::is_fast, etl_traits<K>::is_fast, etl_traits<C>::is_fast> = cpp::detail::dummy>
+void check_conv_2d_sizes(const I&, const K&, const C&){
+    static_assert(
+        T != 1 || (
+                etl_traits<C>::template dim<0>() == etl_traits<I>::template dim<0>() + etl_traits<K>::template dim<0>() - 1
+            &&  etl_traits<C>::template dim<1>() == etl_traits<I>::template dim<1>() + etl_traits<K>::template dim<1>() - 1),
+        "Invalid sizes for 'full'convolution");
+    static_assert(
+        T != 2 || (
+                etl_traits<C>::template dim<0>() == etl_traits<I>::template dim<0>()
+            &&  etl_traits<C>::template dim<1>() == etl_traits<I>::template dim<1>()),
+        "Invalid sizes for 'same'convolution");
+    static_assert(
+        T != 3 || (
+                etl_traits<C>::template dim<0>() == etl_traits<I>::template dim<0>() - etl_traits<K>::template dim<0>() + 1
+            &&  etl_traits<C>::template dim<1>() == etl_traits<I>::template dim<1>() - etl_traits<K>::template dim<1>() + 1),
+        "Invalid sizes for 'valid'convolution");
+}
+
 template<typename I, typename K, typename C>
 static C& convolve_1d_full(const I& input, const K& kernel, C&& conv){
     static_assert(is_etl_expr<I>::value && is_etl_expr<K>::value && is_etl_expr<C>::value, "Convolution only supported for ETL expressions");
-    //static_assert(C::etl_size == I::etl_size + K::etl_size - 1, "Invalid output vector size");
-    cpp_assert(size(conv) == size(input) + size(kernel) - 1, "Invalid output vector size");
+    check_conv_1d_sizes<1>(input, kernel, conv);
 
     for(std::size_t i = 0; i < size(conv); ++i) {
         const auto lo = i >= size(kernel) - 1 ? i - (size(kernel) - 1) : 0;
@@ -40,7 +105,7 @@ static C& convolve_1d_full(const I& input, const K& kernel, C&& conv){
 template<typename I, typename K, typename C>
 static C& convolve_1d_same(const I& input, const K& kernel, C&& conv){
     static_assert(is_etl_expr<I>::value && is_etl_expr<K>::value && is_etl_expr<C>::value, "Convolution only supported for ETL expressions");
-    //static_assert(C::etl_size == I::etl_size, "Invalid output vector size");
+    check_conv_1d_sizes<2>(input, kernel, conv);
 
     for(std::size_t j = 0 ; j < size(conv) ; ++j){
         int l_lo = std::max<int>(0, j - (size(kernel) - 1) / 2);
@@ -61,7 +126,7 @@ static C& convolve_1d_same(const I& input, const K& kernel, C&& conv){
 template<typename I, typename K, typename C>
 static C& convolve_1d_valid(const I& input, const K& kernel, C&& conv){
     static_assert(is_etl_expr<I>::value && is_etl_expr<K>::value && is_etl_expr<C>::value, "Convolution only supported for ETL expressions");
-    //static_assert(C::etl_size == I::etl_size - K::etl_size + 1, "Invalid output vector size");
+    check_conv_1d_sizes<3>(input, kernel, conv);
 
     for(std::size_t j = 0 ; j < size(conv) ; ++j){
         double temp = 0.0;
@@ -79,6 +144,7 @@ static C& convolve_1d_valid(const I& input, const K& kernel, C&& conv){
 template<typename I, typename K, typename C>
 static C& convolve_2d_full(const I& input, const K& kernel, C&& conv){
     static_assert(is_etl_expr<I>::value && is_etl_expr<K>::value && is_etl_expr<C>::value, "Convolution only supported for ETL expressions");
+    check_conv_2d_sizes<1>(input, kernel, conv);
 
     for(std::size_t i = 0 ; i < rows(conv) ; ++i){
         auto k_lo = std::max<int>(0, i - rows(kernel) + 1);
@@ -106,6 +172,7 @@ static C& convolve_2d_full(const I& input, const K& kernel, C&& conv){
 template<typename I, typename K, typename C>
 static C& convolve_2d_same(const I& input, const K& kernel, C&& conv){
     static_assert(is_etl_expr<I>::value && is_etl_expr<K>::value && is_etl_expr<C>::value, "Convolution only supported for ETL expressions");
+    check_conv_2d_sizes<2>(input, kernel, conv);
 
     for(std::size_t i = 0 ; i < rows(conv); ++i){
         auto k_lo = std::max<int>(0, i - (rows(kernel)-1)/2);
@@ -133,6 +200,7 @@ static C& convolve_2d_same(const I& input, const K& kernel, C&& conv){
 template<typename I, typename K, typename C>
 static C& convolve_2d_valid(const I& input, const K& kernel, C&& conv){
     static_assert(is_etl_expr<I>::value && is_etl_expr<K>::value && is_etl_expr<C>::value, "Convolution only supported for ETL expressions");
+    check_conv_2d_sizes<3>(input, kernel, conv);
 
     for(std::size_t i = 0 ; i < rows(conv) ; ++i){
         for(std::size_t j = 0 ; j < columns(conv) ; ++j){
