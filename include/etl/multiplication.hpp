cc1a768178d67f7cea593ffc9028dbdcc667bd12
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-27 21:42:47 +0100

    Increase support for expressions

--- include/etl/multiplication.hpp
+++ include/etl/multiplication.hpp
@@ -38,9 +38,9 @@ void check_mmul_sizes(const A&, const B&, C&){
 } //end of namespace detail
 
 template<typename A, typename B, typename C>
-static C& mmul(const A& a, const B& b, C& c){
+static C& mmul(A&& a, B&& b, C&& c){
     static_assert(is_etl_expr<A>::value && is_etl_expr<B>::value && is_etl_expr<C>::value, "Matrix multiplication only supported for ETL expressions");
-    static_assert(etl_traits<A>::dimensions() == 2 && etl_traits<B>::dimensions() == 2 && etl_traits<C>::dimensions() == 2, "Matrix multiplication only works in 2D");
+    static_assert(decay_traits<A>::dimensions() == 2 && decay_traits<B>::dimensions() == 2 && decay_traits<C>::dimensions() == 2, "Matrix multiplication only works in 2D");
     detail::check_mmul_sizes(a,b,c);
 
     c = 0;
@@ -280,55 +280,55 @@ static C& strassen_mmul(const A& a, const B& b, C& c){
 }
 
 template<typename A, typename B, typename C, cpp::enable_if_all_u<
-    etl_traits<A>::is_fast, etl_traits<B>::is_fast, etl_traits<C>::is_fast,
-    etl_traits<A>::dimensions() == 1, etl_traits<B>::dimensions() == 2
+    decay_traits<A>::is_fast, decay_traits<B>::is_fast, decay_traits<C>::is_fast,
+    decay_traits<A>::dimensions() == 1, decay_traits<B>::dimensions() == 2
 > = cpp::detail::dummy>
-static C& auto_vmmul(const A& a, const B& b, C& c){
-    return mmul(reshape<1, etl_traits<B>::template dim<0>()>(a), b, c);
+static C& auto_vmmul(A&& a, B&& b, C& c){
+    return mmul(reshape<1, decay_traits<B>::template dim<0>()>(a), b, c);
 }
 
 template<typename A, typename B, typename C, cpp::enable_if_all_u<
-    etl_traits<A>::is_fast, etl_traits<B>::is_fast, etl_traits<C>::is_fast,
-    etl_traits<A>::dimensions() == 2, etl_traits<B>::dimensions() == 1
+    decay_traits<A>::is_fast, decay_traits<B>::is_fast, decay_traits<C>::is_fast,
+    decay_traits<A>::dimensions() == 2, decay_traits<B>::dimensions() == 1
 > = cpp::detail::dummy>
-static C& auto_vmmul(const A& a, const B& b, C& c){
-    return mmul(a, reshape<etl_traits<A>::template dim<1>(),1>(b), c);
+static C& auto_vmmul(A&& a, B&& b, C& c){
+    return mmul(a, reshape<decay_traits<A>::template dim<1>(),1>(b), c);
 }
 
 template<typename A, typename B, typename C, cpp::enable_if_all_u<
-    cpp::or_u<!etl_traits<A>::is_fast, !etl_traits<B>::is_fast, !etl_traits<C>::is_fast>::value,
-    etl_traits<A>::dimensions() == 1, etl_traits<B>::dimensions() == 2,
-    cpp::not_u<etl_traits<A>::is_fast>::value
+    cpp::or_u<!decay_traits<A>::is_fast, !decay_traits<B>::is_fast, !decay_traits<C>::is_fast>::value,
+    decay_traits<A>::dimensions() == 1, decay_traits<B>::dimensions() == 2,
+    cpp::not_u<decay_traits<A>::is_fast>::value
 > = cpp::detail::dummy>
-static C& auto_vmmul(const A& a, const B& b, C& c){
+static C& auto_vmmul(A&& a, B&& b, C& c){
     return mmul(reshape(a, 1, dim<0>(b)), b, c);
 }
 
 template<typename A, typename B, typename C, cpp::enable_if_all_u<
-    cpp::or_u<!etl_traits<A>::is_fast, !etl_traits<B>::is_fast, !etl_traits<C>::is_fast>::value,
-    etl_traits<A>::dimensions() == 2, etl_traits<B>::dimensions() == 1,
-    cpp::not_u<etl_traits<B>::is_fast>::value
+    cpp::or_u<!decay_traits<A>::is_fast, !decay_traits<B>::is_fast, !decay_traits<C>::is_fast>::value,
+    decay_traits<A>::dimensions() == 2, decay_traits<B>::dimensions() == 1,
+    cpp::not_u<decay_traits<B>::is_fast>::value
 > = cpp::detail::dummy>
-static C& auto_vmmul(const A& a, const B& b, C& c){
+static C& auto_vmmul(A&& a, B&& b, C& c){
     return mmul(a, reshape(b, dim<1>(a), 1), c);
 }
 
 template<typename A, typename B, typename C, cpp::enable_if_all_u<
-    cpp::or_u<!etl_traits<A>::is_fast, !etl_traits<B>::is_fast, !etl_traits<C>::is_fast>::value,
-    etl_traits<A>::dimensions() == 1, etl_traits<B>::dimensions() == 2,
-    etl_traits<A>::is_fast
+    cpp::or_u<!decay_traits<A>::is_fast, !decay_traits<B>::is_fast, !decay_traits<C>::is_fast>::value,
+    decay_traits<A>::dimensions() == 1, decay_traits<B>::dimensions() == 2,
+    decay_traits<A>::is_fast
 > = cpp::detail::dummy>
-static C& auto_vmmul(const A& a, const B& b, C& c){
-    return mmul(reshape<1, etl_traits<B>::template dim<0>()>(a), b, c);
+static C& auto_vmmul(A&& a, B&& b, C& c){
+    return mmul(reshape<1, decay_traits<B>::template dim<0>()>(a), b, c);
 }
 
 template<typename A, typename B, typename C, cpp::enable_if_all_u<
-    cpp::or_u<!etl_traits<A>::is_fast, !etl_traits<B>::is_fast, !etl_traits<C>::is_fast>::value,
-    etl_traits<A>::dimensions() == 2, etl_traits<B>::dimensions() == 1,
-    etl_traits<B>::is_fast
+    cpp::or_u<!decay_traits<A>::is_fast, !decay_traits<B>::is_fast, !decay_traits<C>::is_fast>::value,
+    decay_traits<A>::dimensions() == 2, decay_traits<B>::dimensions() == 1,
+    decay_traits<B>::is_fast
 > = cpp::detail::dummy>
-static C& auto_vmmul(const A& a, const B& b, C& c){
-    return mmul(a, reshape<etl_traits<A>::template dim<1>(),1>(b), c);
+static C& auto_vmmul(A&& a, B&& b, C& c){
+    return mmul(a, reshape<decay_traits<A>::template dim<1>(),1>(b), c);
 }
 
 } //end of namespace etl
