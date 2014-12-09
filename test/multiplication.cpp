34506526a0883070907da042ac8352bacb081dc7
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-12-09 16:27:24 +0100

    Prepare test case for lazy mmul (WIP)

--- test/multiplication.cpp
+++ test/multiplication.cpp
@@ -289,6 +289,82 @@ TEST_CASE( "multiplication/mmul_2_5", "mmul" ) {
     REQUIRE(c(1,1) == 154);
 }
 
+//}}}
+
+//{{{ mmul_2
+
+TEST_CASE( "multiplication/lazy_mmul_1", "mmul" ) {
+    etl::fast_matrix<double, 2, 3> a = {1,2,3,4,5,6};
+    etl::fast_matrix<double, 3, 2> b = {7,8,9,10,11,12};
+    etl::fast_matrix<double, 2, 2> c;
+
+    c = etl::lazy_mmul(a, b);
+
+    REQUIRE(c(0,0) == 58);
+    REQUIRE(c(0,1) == 64);
+    REQUIRE(c(1,0) == 139);
+    REQUIRE(c(1,1) == 154);
+}
+
+TEST_CASE( "multiplication/lazy_mmul_2", "mmul" ) {
+    etl::fast_matrix<double, 3, 3> a = {1,2,3,4,5,6,7,8,9};
+    etl::fast_matrix<double, 3, 3> b = {7,8,9,9,10,11,11,12,13};
+    etl::fast_matrix<double, 3, 3> c;
+
+    c = etl::lazy_mmul(a, b);
+
+    REQUIRE(c(0,0) == 58);
+    REQUIRE(c(0,1) == 64);
+    REQUIRE(c(0,2) == 70);
+    REQUIRE(c(1,0) == 139);
+    REQUIRE(c(1,1) == 154);
+    REQUIRE(c(1,2) == 169);
+    REQUIRE(c(2,0) == 220);
+    REQUIRE(c(2,1) == 244);
+    REQUIRE(c(2,2) == 268);
+}
+
+TEST_CASE( "multiplication/lazy_mmul_3", "mmul" ) {
+    etl::dyn_matrix<double> a(4,4, etl::values(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16));
+    etl::dyn_matrix<double> b(4,4, etl::values(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16));
+    etl::dyn_matrix<double> c(4,4);
+
+    c = etl::lazy_mmul(a, b);
+
+    REQUIRE(c(0,0) == 90);
+    REQUIRE(c(0,1) == 100);
+    REQUIRE(c(1,0) == 202);
+    REQUIRE(c(1,1) == 228);
+    REQUIRE(c(2,0) == 314);
+    REQUIRE(c(2,1) == 356);
+    REQUIRE(c(3,0) == 426);
+    REQUIRE(c(3,1) == 484);
+}
+
+TEST_CASE( "multiplication/lazy_mmul_4", "mmul" ) {
+    etl::dyn_matrix<double> a(2,2, etl::values(1,2,3,4,5,6,7,8));
+    etl::dyn_matrix<double> b(2,2, etl::values(1,2,3,4,5,6,7,8));
+    etl::dyn_matrix<double> c(2,2);
+
+    c = etl::lazy_mmul(a, b);
+
+    REQUIRE(c(0,0) == 7);
+    REQUIRE(c(0,1) == 10);
+    REQUIRE(c(1,0) == 15);
+    REQUIRE(c(1,1) == 22);
+}
+
+TEST_CASE( "multiplication/lazy_mmul_5", "mmul" ) {
+    etl::fast_matrix<double, 2, 3> a = {1,2,3,4,5,6};
+    etl::fast_matrix<double, 3, 2> b = {7,8,9,10,11,12};
+
+    auto c = etl::lazy_mmul(a, b);
+
+    REQUIRE(c(0,0) == 58);
+    REQUIRE(c(0,1) == 64);
+    REQUIRE(c(1,0) == 139);
+    REQUIRE(c(1,1) == 154);
+}
 
 //}}}
 
