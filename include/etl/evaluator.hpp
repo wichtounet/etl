1bb7d86292d03ec6027c028a7d8455d80df8311d
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-29 17:45:45 +0200

    Evaluation must be done at each level

--- include/etl/evaluator.hpp
+++ include/etl/evaluator.hpp
@@ -15,7 +15,6 @@ namespace etl {
 namespace detail {
 
 //TODO Disable completely the visitor if there are no temporary_expr in the tree (use TMP)
-
 struct temporary_allocator_static_visitor {
     template <typename T, typename Expr, typename UnaryOp>
     void operator()(etl::unary_expr<T, Expr, UnaryOp>& v) const {
@@ -49,14 +48,51 @@ struct temporary_allocator_static_visitor {
     }
 };
 
+//TODO Disable completely the visitor if there are no temporary_expr in the tree (use TMP)
+struct evaluator_static_visitor {
+    template <typename T, typename Expr, typename UnaryOp>
+    void operator()(etl::unary_expr<T, Expr, UnaryOp>& v) const {
+        (*this)(v.value());
+    }
+
+    template <typename T, typename Expr>
+    void operator()(etl::stable_transform_expr<T, Expr>& v) const {
+        (*this)(v.value());
+    }
+
+    template <typename T, typename LeftExpr, typename BinaryOp, typename RightExpr>
+    void operator()(etl::binary_expr<T, LeftExpr, BinaryOp, RightExpr>& v) const {
+        (*this)(v.lhs());
+        (*this)(v.rhs());
+    }
+
+    template <typename T, typename AExpr, typename BExpr, typename Op, typename Forced>
+    void operator()(etl::temporary_binary_expr<T, AExpr, BExpr, Op, Forced>& v) const {
+        (*this)(v.a());
+        (*this)(v.b());
+
+        v.evaluate();
+    }
+
+    //TODO Handle transformers and views
+
+    template<typename T>
+    void operator()(T&) const {
+        //fallback
+    }
+};
+
 } //end of namespace detail
 
 template<typename Expr, typename Result>
 struct standard_evaluator {
     template<typename E, typename R, cpp_disable_if(cpp::is_specialization_of<etl::temporary_binary_expr, std::decay_t<E>>::value)>
     static void evaluate(E&& expr, R&& result){
-        detail::temporary_allocator_static_visitor visitor;
-        visitor(expr);
+        detail::temporary_allocator_static_visitor allocator_visitor;
+        allocator_visitor(expr);
+
+        detail::evaluator_static_visitor evaluator_visitor;
+        evaluator_visitor(expr);
 
         for(std::size_t i = 0; i < etl::size(result); ++i){
             result[i] = expr[i];
@@ -65,19 +101,24 @@ struct standard_evaluator {
 
     template<typename E, typename R, cpp_enable_if(cpp::is_specialization_of<etl::temporary_binary_expr, std::decay_t<E>>::value)>
     static void evaluate(E&& expr, R&& result){
-        detail::temporary_allocator_static_visitor visitor;
-        visitor(expr.a());
-        visitor(expr.b());
+        detail::temporary_allocator_static_visitor allocator_visitor;
+        allocator_visitor(expr.a());
+        allocator_visitor(expr.b());
+
+        detail::evaluator_static_visitor evaluator_visitor;
+        evaluator_visitor(expr.a());
+        evaluator_visitor(expr.b());
 
         expr.direct_evaluate(result);
     }
 
     template<typename E, cpp_enable_if(cpp::is_specialization_of<etl::temporary_binary_expr, std::decay_t<E>>::value)>
     static void evaluate(E&& expr){
-        detail::temporary_allocator_static_visitor visitor;
-        visitor(expr);
+        detail::temporary_allocator_static_visitor allocator_visitor;
+        allocator_visitor(expr);
 
-        expr.evaluate();
+        detail::evaluator_static_visitor evaluator_visitor;
+        evaluator_visitor(expr);
     }
 };
 
