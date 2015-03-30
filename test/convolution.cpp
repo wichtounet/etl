95ce41375f9ab7ce42533e4f92c24b7b6c060eec
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-30 11:20:15 +0200

    Update tests

--- test/convolution.cpp
+++ test/convolution.cpp
@@ -593,6 +593,22 @@ TEMPLATE_TEST_CASE_2( "convolution_2d/full_8", "convolution_2d_full", Z, float, 
     CHECK(c(3, 3) == 516210);
 }
 
+TEMPLATE_TEST_CASE_2( "convolution_2d/full_9", "convmtx conv", Z, float, double ) {
+    etl::fast_vector<Z, 5> a = {1.0, 2.0, 3.0, 4.0, 5.0};
+    etl::fast_vector<Z, 3> k = {1.0, 2.0, 3.0};
+
+    etl::fast_matrix<Z, 7> c;
+    c = conv_1d_full(a, k);
+
+    REQUIRE(c(0) == Z(1.0));
+    REQUIRE(c(1) == Z(4.0));
+    REQUIRE(c(2) == Z(10.0));
+    REQUIRE(c(3) == Z(16.0));
+    REQUIRE(c(4) == Z(22.0));
+    REQUIRE(c(5) == Z(22.0));
+    REQUIRE(c(6) == Z(15.0));
+}
+
 //}}}
 
 //{{{ convolution_2d_same
@@ -1199,21 +1215,13 @@ TEMPLATE_TEST_CASE_2( "convmtx/convmtx_2", "convmtx", Z, float, double ) {
     REQUIRE(c(3, 5) == 3);
 }
 
-TEMPLATE_TEST_CASE_2( "convmtx/convmtx_3", "convmtx conv", Z, float, double ) {
+TEMPLATE_TEST_CASE_2( "convmtx/convmtx_3", "convmtx conv", Z, double, float ) {
     etl::fast_vector<Z, 5> a = {1.0, 2.0, 3.0, 4.0, 5.0};
     etl::fast_vector<Z, 3> k = {1.0, 2.0, 3.0};
-    etl::fast_matrix<Z, 3, 7> c;
+    etl::fast_matrix<Z, 3, 7> c(convmtx(a, 3));
 
-    etl::fast_matrix<Z, 1, 7> c1;
-    etl::fast_matrix<Z, 7> c2;
-
-    c = convmtx(a, 3);
-
-    c1 = auto_vmmul(k, c);
-    c2 = conv_1d_full(a, k);
-
-    std::cout << to_string(c1) << std::endl;
-    std::cout << to_string(c2) << std::endl;
+    etl::fast_matrix<Z, 1, 7> c1(auto_vmmul(k, c));
+    etl::fast_matrix<Z, 7> c2(conv_1d_full(a, k));
 
     for(std::size_t i = 0; i < c1.size(); ++i){
         REQUIRE(c1[i] == c2[i]);
