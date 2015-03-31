db00b81772e12e679501ac337959aa546f013311
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-31 16:37:12 +0200

    Enable avx sconv2_full

--- include/etl/impl/conv.hpp
+++ include/etl/impl/conv.hpp
@@ -184,6 +184,13 @@ struct conv2_valid_impl<I, K, C, std::enable_if_t<c_is_double_precision<I,K,C>::
 };
 
 template<typename I, typename K, typename C>
+struct conv2_full_impl<I, K, C, std::enable_if_t<c_is_single_precision<I,K,C>::value && c_is_dma<I,K,C>::value>> {
+    static void apply(const I& input, const K& kernel, C&& conv){
+        impl::avx::sconv2_full(input, kernel, conv);
+    }
+};
+
+template<typename I, typename K, typename C>
 struct conv2_same_impl<I, K, C, std::enable_if_t<c_is_single_precision<I,K,C>::value && c_is_dma<I,K,C>::value>> {
     static void apply(const I& input, const K& kernel, C&& conv){
         impl::avx::sconv2_same(input, kernel, conv);
