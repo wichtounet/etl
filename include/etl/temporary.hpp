dbd589be44696211c8113c03df39e098e0e3732e
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-04-06 22:21:03 +0200

    Add system to create temporaries inside expressions

---
+++ include/etl/temporary.hpp
@@ -0,0 +1,43 @@
+//=======================================================================
+// Copyright (c) 2014-2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#ifndef ETL_TEMPORARY_HPP
+#define ETL_TEMPORARY_HPP
+
+#include "config.hpp"       //configuration of ETL
+#include "traits_lite.hpp"  //type traits
+#include "stop.hpp"
+
+namespace etl {
+
+template<typename E, cpp_enable_if(has_direct_access<E>::value || !create_temporary::value)>
+decltype(auto) make_temporary(E&& expr){
+    return std::forward<E>(expr);
+}
+
+template<typename E, cpp_enable_if(!has_direct_access<E>::value && create_temporary::value && !decay_traits<E>::is_fast)>
+decltype(auto) make_temporary(E&& expr){
+    //Sizes will be directly propagated
+    return dyn_matrix<value_t<E>, decay_traits<E>::dimensions()>{std::forward<E>(expr)};
+}
+
+template<typename E, typename Sequence>
+struct build_fast_dyn_matrix_type;
+
+template<typename E, std::size_t... I>
+struct build_fast_dyn_matrix_type<E, std::index_sequence<I...>> {
+    using type = fast_dyn_matrix<value_t<E>, decay_traits<E>::template dim<I>()...>;
+};
+
+template<typename E, cpp_enable_if(!has_direct_access<E>::value && create_temporary::value && decay_traits<E>::is_fast)>
+decltype(auto) make_temporary(E&& expr){
+    return typename build_fast_dyn_matrix_type<E, std::make_index_sequence<decay_traits<E>::dimensions()>>::type{std::forward<E>(expr)};
+}
+
+} //end of namespace etl
+
+#endif
