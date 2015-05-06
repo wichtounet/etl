95d6f20a338d0380123a8945a006143b1f290996
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-06 15:07:50 +0200

    Tests with higher dimensions

--- test/column_major.cpp
+++ test/column_major.cpp
@@ -56,3 +56,69 @@ TEMPLATE_TEST_CASE_2( "column_major/2", "[dyn][cm]", Z, int, long ) {
     REQUIRE(test_matrix(1, 1) == 4);
     REQUIRE(test_matrix(1, 2) == 6);
 }
+
+TEMPLATE_TEST_CASE_2( "column_major/3", "[fast][cm]", Z, int, long ) {
+    etl::fast_matrix_cm<Z, 2, 3, 4> test_matrix;
+
+    for(std::size_t i = 0; i < test_matrix.size(); ++i){
+        test_matrix[i] = i+1;
+    }
+
+    REQUIRE(test_matrix(0, 0, 0) == 1);
+    REQUIRE(test_matrix(0, 0, 1) == 7);
+    REQUIRE(test_matrix(0, 0, 2) == 13);
+    REQUIRE(test_matrix(0, 0, 3) == 19);
+    REQUIRE(test_matrix(0, 1, 0) == 3);
+    REQUIRE(test_matrix(0, 1, 1) == 9);
+    REQUIRE(test_matrix(0, 1, 2) == 15);
+    REQUIRE(test_matrix(0, 1, 3) == 21);
+    REQUIRE(test_matrix(0, 2, 0) == 5);
+    REQUIRE(test_matrix(0, 2, 1) == 11);
+    REQUIRE(test_matrix(0, 2, 2) == 17);
+    REQUIRE(test_matrix(0, 2, 3) == 23);
+    REQUIRE(test_matrix(1, 0, 0) == 2);
+    REQUIRE(test_matrix(1, 0, 1) == 8);
+    REQUIRE(test_matrix(1, 0, 2) == 14);
+    REQUIRE(test_matrix(1, 0, 3) == 20);
+    REQUIRE(test_matrix(1, 1, 0) == 4);
+    REQUIRE(test_matrix(1, 1, 1) == 10);
+    REQUIRE(test_matrix(1, 1, 2) == 16);
+    REQUIRE(test_matrix(1, 1, 3) == 22);
+    REQUIRE(test_matrix(1, 2, 0) == 6);
+    REQUIRE(test_matrix(1, 2, 1) == 12);
+    REQUIRE(test_matrix(1, 2, 2) == 18);
+    REQUIRE(test_matrix(1, 2, 3) == 24);
+}
+
+TEMPLATE_TEST_CASE_2( "column_major/4", "[dyn][cm]", Z, int, long ) {
+    etl::dyn_matrix_cm<Z, 3> test_matrix(2, 3, 4);
+
+    for(std::size_t i = 0; i < test_matrix.size(); ++i){
+        test_matrix[i] = i+1;
+    }
+
+    REQUIRE(test_matrix(0, 0, 0) == 1);
+    REQUIRE(test_matrix(0, 0, 1) == 7);
+    REQUIRE(test_matrix(0, 0, 2) == 13);
+    REQUIRE(test_matrix(0, 0, 3) == 19);
+    REQUIRE(test_matrix(0, 1, 0) == 3);
+    REQUIRE(test_matrix(0, 1, 1) == 9);
+    REQUIRE(test_matrix(0, 1, 2) == 15);
+    REQUIRE(test_matrix(0, 1, 3) == 21);
+    REQUIRE(test_matrix(0, 2, 0) == 5);
+    REQUIRE(test_matrix(0, 2, 1) == 11);
+    REQUIRE(test_matrix(0, 2, 2) == 17);
+    REQUIRE(test_matrix(0, 2, 3) == 23);
+    REQUIRE(test_matrix(1, 0, 0) == 2);
+    REQUIRE(test_matrix(1, 0, 1) == 8);
+    REQUIRE(test_matrix(1, 0, 2) == 14);
+    REQUIRE(test_matrix(1, 0, 3) == 20);
+    REQUIRE(test_matrix(1, 1, 0) == 4);
+    REQUIRE(test_matrix(1, 1, 1) == 10);
+    REQUIRE(test_matrix(1, 1, 2) == 16);
+    REQUIRE(test_matrix(1, 1, 3) == 22);
+    REQUIRE(test_matrix(1, 2, 0) == 6);
+    REQUIRE(test_matrix(1, 2, 1) == 12);
+    REQUIRE(test_matrix(1, 2, 2) == 18);
+    REQUIRE(test_matrix(1, 2, 3) == 24);
+}
