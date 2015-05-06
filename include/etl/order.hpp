07b0137beb093c5c395d52d03d5b85259cffcd05
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-06 10:54:43 +0200

    Prepare order

---
+++ include/etl/order.hpp
@@ -0,0 +1,20 @@
+//=======================================================================
+// Copyright (c) 2014-2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#ifndef ETL_ORDER_HPP
+#define ETL_ORDER_HPP
+
+namespace etl {
+
+enum class order {
+    ROW_MAJOR,
+    COL_MAJOR
+};
+
+} //end of namespace etl
+
+#endif
