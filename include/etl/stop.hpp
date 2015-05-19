29c0c0d11aaa54a8246c68c338a0112f71867d09
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-05-19 12:39:56 +0200

    Formatting

--- include/etl/stop.hpp
+++ include/etl/stop.hpp
@@ -12,25 +12,25 @@
 
 namespace etl {
 
-template<typename T, cpp_enable_if(is_etl_expr<T>::value && !etl_traits<T>::is_value && !etl_traits<T>::is_fast)>
-auto s(T&& value){
-    //Sizes will be directly propagated
+template <typename T, cpp_enable_if(is_etl_expr<T>::value && !etl_traits<T>::is_value && !etl_traits<T>::is_fast)>
+auto s(T&& value) {
+    // Sizes will be directly propagated
     return dyn_matrix<typename T::value_type, etl_traits<T>::dimensions()>(std::forward<T>(value));
 }
 
-template<typename M, typename Sequence>
+template <typename M, typename Sequence>
 struct build_matrix_type;
 
-template<typename M, std::size_t... I>
+template <typename M, std::size_t... I>
 struct build_matrix_type<M, std::index_sequence<I...>> {
     using type = fast_matrix<typename M::value_type, etl_traits<M>::template dim<I>()...>;
 };
 
-template<typename T, cpp_enable_if(is_etl_expr<T>::value && !etl_traits<T>::is_value && etl_traits<T>::is_fast)>
-auto s(T&& value){
+template <typename T, cpp_enable_if(is_etl_expr<T>::value && !etl_traits<T>::is_value && etl_traits<T>::is_fast)>
+auto s(T&& value) {
     return typename build_matrix_type<T, std::make_index_sequence<etl_traits<T>::dimensions()>>::type(std::forward<T>(value));
 }
 
-} //end of namespace etl
+} // end of namespace etl
 
 #endif
