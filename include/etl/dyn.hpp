dd42ac4a36d95ef3a87d5348390ff4f514c82d9d
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-06-15 13:47:06 +0200

    Fix warning

--- include/etl/dyn.hpp
+++ include/etl/dyn.hpp
@@ -123,8 +123,10 @@ private:
         cpp_assert(_size == _data.size(), "Invalid container size");
         cpp_assert(_dimensions.size() == D, "Invalid dimensions");
 
+#ifndef NDEBUG
         auto computed = std::accumulate(_dimensions.begin(), _dimensions.end(), std::size_t(1), std::multiplies<std::size_t>());
         cpp_assert(computed == _size, "Incoherency in dimensions");
+#endif
     }
 
 public:
