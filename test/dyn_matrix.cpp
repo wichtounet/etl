b38eb553b2ab3946982a89dd6704b22b7eb0d75d
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-06-15 11:12:55 +0200

    Enhance the test case

--- test/dyn_matrix.cpp
+++ test/dyn_matrix.cpp
@@ -831,26 +831,40 @@ TEST_CASE( "dyn_matrix/default_constructor_2", "" ) {
     REQUIRE(etl::dim<1>(values[0]) == 2);
 }
 
+etl::dyn_matrix<double, 3> test_return(){
+    return etl::dyn_matrix<double, 3>(3, 8, 1);
+}
+
 TEST_CASE( "dyn_matrix/default_constructor_3", "" ) {
-    std::vector<etl::dyn_matrix<double>> values;
+    std::vector<etl::dyn_matrix<double, 3>> values;
 
     values.emplace_back();
 
     REQUIRE(values[0].size() == 0);
 
-    values.emplace_back(5, 5, 1.0);
+    values.emplace_back(5, 5, 1, 1.0);
 
     REQUIRE(values[0].size() == 0);
     REQUIRE(values[1].size() == 25);
     REQUIRE(values[1][0] == 1.0);
 
-    values.push_back(etl::dyn_matrix<double>(3,3,10.0));
+    values.push_back(etl::dyn_matrix<double, 3>(3,2,5,13.0));
 
     REQUIRE(values[0].size() == 0);
     REQUIRE(values[1].size() == 25);
     REQUIRE(values[1][0] == 1.0);
-    REQUIRE(values[2].size() == 9);
-    REQUIRE(values[2][0] == 10.0);
+    REQUIRE(values[2].size() == 30);
+    REQUIRE(values[2][0] == 13.0);
+
+    values.shrink_to_fit();
+    values.push_back(test_return());
+
+    REQUIRE(values[0].size() == 0);
+    REQUIRE(values[1].size() == 25);
+    REQUIRE(values[1][0] == 1.0);
+    REQUIRE(values[2].size() == 30);
+    REQUIRE(values[2][0] == 13.0);
+    REQUIRE(values[3].size() == 24);
 
     values.pop_back();
     values.shrink_to_fit();
@@ -858,11 +872,15 @@ TEST_CASE( "dyn_matrix/default_constructor_3", "" ) {
     REQUIRE(values[0].size() == 0);
     REQUIRE(values[1].size() == 25);
     REQUIRE(values[1][0] == 1.0);
+    REQUIRE(values[2].size() == 30);
+    REQUIRE(values[2][0] == 13.0);
 
-    std::vector<etl::dyn_matrix<double>> values_2;
+    std::vector<etl::dyn_matrix<double, 3>> values_2;
     values_2 = values;
 
     REQUIRE(values_2[0].size() == 0);
     REQUIRE(values_2[1].size() == 25);
     REQUIRE(values_2[1][0] == 1.0);
+    REQUIRE(values_2[2].size() == 30);
+    REQUIRE(values_2[2][0] == 13.0);
 }
