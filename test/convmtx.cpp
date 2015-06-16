511e88558237b89b149b8b0ae41e9363a194bce7
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-06-16 12:28:07 +0200

    Complete the tests

--- test/convmtx.cpp
+++ test/convmtx.cpp
@@ -108,63 +108,79 @@ TEMPLATE_TEST_CASE_2( "convmtx2/convmtx2_1", "convmtx conv", Z, double, float ) 
 TEMPLATE_TEST_CASE_2( "convmtx2/convmtx2_2", "convmtx conv", Z, double, float ) {
     etl::fast_matrix<Z, 3, 3> I(etl::magic<Z>(3));
     etl::fast_matrix<Z, 2, 2> K(etl::magic<Z>(2));
-    etl::fast_matrix<Z, 4*4, 4> C;
+    etl::fast_matrix<Z, 4*4, 4> C1;
+    etl::fast_matrix<Z, 4*4, 4> C2;
 
-    C = convmtx2(I, 2, 2);
+    C1 = etl::convmtx2(I, 2, 2);
+    C2 = etl::convmtx2_direct<2, 2>(I);
 
-    REQUIRE(C(0, 0) == 8);
-    REQUIRE(C(0, 1) == 0);
-    REQUIRE(C(0, 2) == 0);
-    REQUIRE(C(0, 3) == 0);
+    REQUIRE(C1(0, 0) == 8);
+    REQUIRE(C1(0, 1) == 0);
+    REQUIRE(C1(0, 2) == 0);
+    REQUIRE(C1(0, 3) == 0);
 
-    REQUIRE(C(1, 0) == 3);
-    REQUIRE(C(1, 1) == 8);
-    REQUIRE(C(1, 2) == 0);
-    REQUIRE(C(1, 3) == 0);
+    REQUIRE(C1(1, 0) == 3);
+    REQUIRE(C1(1, 1) == 8);
+    REQUIRE(C1(1, 2) == 0);
+    REQUIRE(C1(1, 3) == 0);
 
-    REQUIRE(C(2, 0) == 4);
-    REQUIRE(C(2, 1) == 3);
-    REQUIRE(C(2, 2) == 0);
-    REQUIRE(C(2, 3) == 0);
+    REQUIRE(C1(2, 0) == 4);
+    REQUIRE(C1(2, 1) == 3);
+    REQUIRE(C1(2, 2) == 0);
+    REQUIRE(C1(2, 3) == 0);
 
-    REQUIRE(C(3, 0) == 0);
-    REQUIRE(C(3, 1) == 4);
-    REQUIRE(C(3, 2) == 0);
-    REQUIRE(C(3, 3) == 0);
+    REQUIRE(C1(3, 0) == 0);
+    REQUIRE(C1(3, 1) == 4);
+    REQUIRE(C1(3, 2) == 0);
+    REQUIRE(C1(3, 3) == 0);
 
-    REQUIRE(C(10, 0) == 2);
-    REQUIRE(C(10, 1) == 7);
-    REQUIRE(C(10, 2) == 9);
-    REQUIRE(C(10, 3) == 5);
+    REQUIRE(C1(10, 0) == 2);
+    REQUIRE(C1(10, 1) == 7);
+    REQUIRE(C1(10, 2) == 9);
+    REQUIRE(C1(10, 3) == 5);
 
-    REQUIRE(C(15, 0) == 0);
-    REQUIRE(C(15, 1) == 0);
-    REQUIRE(C(15, 2) == 0);
-    REQUIRE(C(15, 3) == 2);
+    REQUIRE(C1(15, 0) == 0);
+    REQUIRE(C1(15, 1) == 0);
+    REQUIRE(C1(15, 2) == 0);
+    REQUIRE(C1(15, 3) == 2);
+
+    REQUIRE(C1 == C2);
 }
 
 TEMPLATE_TEST_CASE_2( "convmtx2/convmtx2_3", "convmtx conv", Z, double, float ) {
     etl::fast_matrix<Z, 3, 3> I(etl::magic<Z>(3));
     etl::fast_matrix<Z, 2, 2> K(etl::magic<Z>(2));
 
-    etl::fast_matrix<Z, 4*4, 4> C(convmtx2(I, 2, 2));
+    etl::fast_matrix<Z, 4*4, 4> C1(etl::convmtx2(I, 2, 2));
+    etl::fast_matrix<Z, 4*4, 4> C2(etl::convmtx2_direct<2, 2>(I));
 
-    etl::fast_matrix<Z, 4, 4> c1(etl::conv_2d_full(I, K));
-    etl::fast_matrix<Z, 16, 1> c2(etl::mul(C, etl::reshape<4, 1>(etl::transpose(K))));
-    etl::fast_matrix<Z, 4, 4> c3(etl::transpose(etl::reshape<4, 4>(c2)));
+    etl::fast_matrix<Z, 16, 1> a1(etl::mul(C1, etl::reshape<4, 1>(etl::transpose(K))));
+    etl::fast_matrix<Z, 4, 4> b1(etl::transpose(etl::reshape<4, 4>(a1)));
 
-    REQUIRE(c1 == c3);
+    etl::fast_matrix<Z, 16, 1> a2(etl::mul(C2, etl::reshape<4, 1>(etl::transpose(K))));
+    etl::fast_matrix<Z, 4, 4> b2(etl::transpose(etl::reshape<4, 4>(a2)));
+
+    etl::fast_matrix<Z, 4, 4> ref(etl::conv_2d_full(I, K));
+
+    REQUIRE(ref == b1);
+    REQUIRE(ref == b2);
 }
 
 TEMPLATE_TEST_CASE_2( "convmtx2/convmtx2_4", "convmtx conv", Z, double, float ) {
     etl::dyn_matrix<Z> I(etl::magic<Z>(7));
     etl::dyn_matrix<Z> K(etl::magic<Z>(5));
 
-    etl::dyn_matrix<Z> C(convmtx2(I, 5, 5));
+    etl::dyn_matrix<Z> C1(etl::convmtx2(I, 5, 5));
+    etl::dyn_matrix<Z> C2(etl::convmtx2_direct<5, 5>(I));
 
-    etl::dyn_matrix<Z> c1(etl::conv_2d_full(I, K));
-    etl::dyn_matrix<Z> c2(etl::mul(C, etl::reshape<25, 1>(etl::transpose(K))));
-    etl::dyn_matrix<Z> c3(etl::transpose(etl::reshape(c2, 11, 11)));
+    etl::dyn_matrix<Z> a1(etl::mul(C1, etl::reshape<25, 1>(etl::transpose(K))));
+    etl::dyn_matrix<Z> b1(etl::transpose(etl::reshape(a1, 11, 11)));
 
-    REQUIRE(c1 == c3);
+    etl::dyn_matrix<Z> a2(etl::mul(C2, etl::reshape<25, 1>(etl::transpose(K))));
+    etl::dyn_matrix<Z> b2(etl::transpose(etl::reshape(a2, 11, 11)));
+
+    etl::dyn_matrix<Z> ref(etl::conv_2d_full(I, K));
+
+    REQUIRE(ref == b1);
+    REQUIRE(ref == b2);
 }
