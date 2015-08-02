eb060c933d0c40a8a5487516a3f51cb5db26172c
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-08-02 13:48:43 +0200

    Complete the FFT implementations

--- include/etl/impl/std/fft.hpp
+++ include/etl/impl/std/fft.hpp
@@ -171,31 +171,32 @@ void ifft1_kernel(const std::complex<T>* a, std::size_t n, std::complex<T>* c){
     }
 }
 
+template<typename T1, typename T>
+void fft1_kernel(const T1* a, std::size_t n, std::complex<T>* c){
+    using complex_t = std::complex<T>;
+
+    std::size_t N = detail::next_power_of_two(n);
+
+    if(N == n){
+        std::copy(a, a + n, c);
+
+        detail::inplace_radix2_fft1(c, n);
+    } else {
+        auto a_complex = allocate<complex_t>(n);
+        auto x = a_complex.get();
+
+        std::copy(a, a + n, x);
+
+        detail::czt1(x, n, c);
+    }
+}
+
 } //end of namespace detail
 
 //(T or complex<T>) -> complex<T>
 template<typename A, typename C>
 void fft1(A&& a, C&& c){
-    using complex_t = std::conditional_t<
-        etl::is_complex<A>::value,
-        value_t<A>,
-        std::complex<value_t<A>>>;
-
-    std::size_t n = etl::size(a);
-    std::size_t N = detail::next_power_of_two(n);
-
-    if(N == n){
-        std::copy(a.begin(), a.end(), c.begin());
-
-        detail::inplace_radix2_fft1(c.memory_start(), n);
-    } else {
-        auto a_complex = allocate<complex_t>(n);
-        auto x = a_complex.get();
-
-        std::copy(a.begin(), a.end(), x);
-
-        detail::czt1(x, n, c.memory_start());
-    }
+    detail::fft1_kernel(a.memory_start(), etl::size(a), c.memory_start());
 }
 
 //complex<T> -> complex<T>
@@ -219,6 +220,122 @@ void ifft1_real(A&& a, C&& c){
     for(std::size_t i = 0; i < n; ++i){
         c[i] = cc[i].real();
     }
+}
+
+//(T or complex<T>) -> complex<T>
+template<typename A, typename C>
+void fft2(A&& a, C&& c){
+    auto w = etl::force_temporary_dyn(c);
+
+    //Perform FFT on each rows
+    for(std::size_t r = 0; r < etl::dim<0>(c); ++r){
+        detail::fft1_kernel(a(r).memory_start(), etl::dim<1>(a), w(r).memory_start());
+    }
+
+    w.transpose_inplace();
+
+    //Perform FFT on each columns
+    for(std::size_t r = 0; r < etl::dim<1>(c); ++r){
+        detail::fft1_kernel(w(r).memory_start(), etl::dim<0>(a), w(r).memory_start());
+    }
+
+    w.transpose_inplace();
+
+    c = w;
+}
+
+//complex<T> -> complex<T>
+template<typename A, typename C>
+void ifft2(A&& a, C&& c){
+    std::size_t n = etl::size(a);
+
+    //Conjugate the complex numbers
+    for(std::size_t i = 0; i < n; ++i){
+        c[i] = std::conj(a[i]);
+    }
+
+    fft2(c, c);
+
+    //Conjugate the complex numbers again
+    for(std::size_t i = 0; i < n; ++i){
+        c[i] = std::conj(c[i]);
+    }
+
+    //Scale the numbers
+    for(std::size_t i = 0; i < n; ++i){
+        c[i] /= double(n);
+    }
+}
+
+//complex<T> -> T
+template<typename A, typename C>
+void ifft2_real(A&& a, C&& c){
+    auto w = etl::force_temporary(a);
+
+    ifft2(a, w);
+
+    for(std::size_t i = 0; i < etl::size(a); ++i){
+        c[i] = w[i].real();
+    }
+}
+
+template<typename A, typename B, typename C>
+void fft1_convolve(A&& a, B&& b, C&& c){
+    const auto m = etl::size(a);
+    const auto n = etl::size(b);
+    const auto size = m + n - 1;
+
+    auto a_padded = allocate<std::complex<value_t<A>>>(size);
+    auto b_padded = allocate<std::complex<value_t<A>>>(size);
+
+    std::copy(a.begin(), a.end(), a_padded.get());
+    std::copy(b.begin(), b.end(), b_padded.get());
+
+    detail::fft1_kernel(a_padded.get(), size, a_padded.get());
+    detail::fft1_kernel(b_padded.get(), size, b_padded.get());
+
+    for(std::size_t i = 0; i < size; ++i){
+        a_padded[i] *= b_padded[i];
+    }
+
+    detail::ifft1_kernel(a_padded.get(), size, a_padded.get());
+
+    for(std::size_t i = 0; i < size; ++i){
+        c[i] = a_padded[i].real();
+    }
+}
+
+template<typename A, typename B, typename C>
+void fft2_convolve(A&& a, B&& b, C&& c){
+    const auto m1 = etl::dim<0>(a);
+    const auto n1= etl::dim<0>(b);
+    const auto s1 = m1 + n1 - 1;
+
+    const auto m2 = etl::dim<1>(a);
+    const auto n2= etl::dim<1>(b);
+    const auto s2 = m2 + n2 - 1;
+
+    dyn_matrix<std::complex<value_t<A>>, 2> a_padded(s1, s2);
+    dyn_matrix<std::complex<value_t<A>>, 2> b_padded(s1, s2);
+
+    for(std::size_t i = 0; i < m1; ++i){
+        for(std::size_t j = 0; j < m2; ++j){
+            a_padded(i, j) = a(i,j);
+        }
+    }
+
+    for(std::size_t i = 0; i < n1; ++i){
+        for(std::size_t j = 0; j < n2; ++j){
+            b_padded(i, j) = b(i,j);
+        }
+    }
+
+    fft2(a_padded, a_padded);
+    fft2(b_padded, b_padded);
+
+    a_padded *= b_padded;
+
+    ifft2_real(a_padded, c);
 }
 
 } //end of namespace standard
