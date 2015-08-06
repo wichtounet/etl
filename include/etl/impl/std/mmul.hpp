e685da1da2816d1b4a0df607f06e331a8c951d5d
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-08-06 16:05:17 +0200

    Improve std::complex computation performances

--- include/etl/impl/std/mmul.hpp
+++ include/etl/impl/std/mmul.hpp
@@ -40,6 +40,29 @@ static void mm_mul(A&& a, B&& b, C&& c){
     }
 }
 
+inline void add_mul(float& c, float a, float b){
+    c += a * b;
+}
+
+inline void add_mul(double& c, double a, double b){
+    c += a * b;
+}
+
+//Note: For some reason, compilers have a real hard time
+//inlining/vectorizing std::complex operations
+//This helper improves performance by more than 50% on some cases
+
+template<typename T>
+inline void add_mul(std::complex<T>& c, std::complex<T> a, std::complex<T> b){
+    auto ac = a.real() * b.real();
+    auto bd = a.imag() * b.imag();
+
+    auto abcd = (a.real() + a.imag()) * (b.real() + b.imag());
+
+    c.real(c.real() + ac - bd);
+    c.imag(c.imag() + abcd - ac - bd);
+}
+
 template<typename A, typename B, typename C>
 static void vm_mul(A&& a, B&& b, C&& c){
     bool row_major = decay_traits<B>::storage_order == order::RowMajor;
@@ -49,13 +72,15 @@ static void vm_mul(A&& a, B&& b, C&& c){
     if(row_major){
         for(std::size_t k = 0; k < etl::dim<0>(a); k++){
             for(std::size_t j = 0; j < columns(b); j++){
-                c(j) += a(k) * b(k,j);
+                //c(j) += a(k) * b(k,j);
+                add_mul(c(j), a(k), b(k,j));
             }
         }
     } else {
         for(std::size_t j = 0; j < columns(b); j++){
             for(std::size_t k = 0; k < etl::dim<0>(a); k++){
-                c(j) += a(k) * b(k,j);
+                //c(j) += a(k) * b(k,j);
+                add_mul(c(j), a(k), b(k,j));
             }
         }
     }
@@ -70,13 +95,15 @@ static void mv_mul(A&& a, B&& b, C&& c){
     if(row_major){
         for(std::size_t i = 0; i < rows(a); i++){
             for(std::size_t k = 0; k < columns(a); k++){
-                c(i) += a(i,k) * b(k);
+                //c(i) += a(i,k) * b(k);
+                add_mul(c(i), a(i,k), b(k));
             }
         }
     } else {
         for(std::size_t k = 0; k < columns(a); k++){
             for(std::size_t i = 0; i < rows(a); i++){
-                c(i) += a(i,k) * b(k);
+                //c(i) += a(i,k) * b(k);
+                add_mul(c(i), a(i,k), b(k));
             }
         }
     }
