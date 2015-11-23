6f8137db4c786d4ec035c02077d49c6e4dfeff41
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-11-23 10:43:28 +0100

    Adds is_strictly_lower_triangular

--- include/etl/crtp/dim_testable.hpp
+++ include/etl/crtp/dim_testable.hpp
@@ -86,6 +86,14 @@ struct dim_testable {
     bool is_lower_triangular() const noexcept {
         return etl::is_lower_triangular(as_derived());
     }
+
+    /*!
+     * \brief Indicates if the given expression is a strictly lower triangular matrix or not.
+     * \return true if the given expression is a strictly lower triangular matrix, false otherwise.
+     */
+    bool is_strictly_lower_triangular() const noexcept {
+        return etl::is_strictly_lower_triangular(as_derived());
+    }
 };
 
 } //end of namespace etl
--- include/etl/globals.hpp
+++ include/etl/globals.hpp
@@ -98,4 +98,26 @@ bool is_lower_triangular(E&& expr) {
     return false;
 }
 
+/*!
+ * \brief Indicates if the given expression is a strictly lower triangular matrix or not.
+ * \param expr The expression to test
+ * \return true if the given expression is a strictly lower triangular matrix, false otherwise.
+ */
+template<typename E>
+bool is_strictly_lower_triangular(E&& expr) {
+    if(is_square(expr)){
+        for(std::size_t i = 0; i < etl::dim<0>(expr); ++i){
+            for(std::size_t j = i; j < etl::dim<0>(expr); ++j){
+                if(expr(i, j) != 0.0){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
 } //end of namespace etl
