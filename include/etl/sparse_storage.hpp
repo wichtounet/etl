2ee3c461fb1f6851c7fde16e80884ba6db967c44
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-11-24 14:23:28 +0100

    Add enum for sparse storage formats

---
+++ include/etl/sparse_storage.hpp
@@ -0,0 +1,24 @@
+//=======================================================================
+// Copyright (c) 2014-2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+/*!
+ * \file
+ * \brief Containers the sparse_storage enum
+ */
+
+#pragma once
+
+namespace etl {
+
+/*!
+ * \brief Enumeration for sparse storage formats
+ */
+enum class sparse_storage {
+    COO     ///< Coordinate Format (COO)
+};
+
+} //end of namespace etl
