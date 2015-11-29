5ffb9daceacdd7175fa35e00b4b9a88e997f17a5
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-11-29 12:14:42 +0100

    Complete the sparse matrix tests

--- test/src/sparse_matrix.cpp
+++ test/src/sparse_matrix.cpp
@@ -245,12 +245,62 @@ TEMPLATE_TEST_CASE_2("sparse_matrix/sequential/1", "[mat][erase][sparse]", Z, do
     REQUIRE(a[5] == 0.0);
 }
 
-TEMPLATE_TEST_CASE_2("sparse_matrix/add/1", "[mat][erase][sparse]", Z, double, float) {
+TEMPLATE_TEST_CASE_2("sparse_matrix/add/1", "[mat][add][sparse]", Z, double, float) {
     etl::sparse_matrix<Z> a(3, 2, std::initializer_list<Z>({1.0, 0.0, 0.0, 2.0, 3.0, 0.0}));
     etl::sparse_matrix<Z> b(3, 2, std::initializer_list<Z>({2.0, 1.0, 0.0, 3.0, 0.0, 0.0}));
     etl::sparse_matrix<Z> c(3, 2);
 
     c = a + b;
 
-    //TODO Continue the test
+    REQUIRE(c.get(0,0) == 3.0);
+    REQUIRE(c.get(0,1) == 1.0);
+    REQUIRE(c.get(1,0) == 0.0);
+    REQUIRE(c.get(1,1) == 5.0);
+    REQUIRE(c.get(2,0) == 3.0);
+    REQUIRE(c.get(2,1) == 0.0);
+}
+
+TEMPLATE_TEST_CASE_2("sparse_matrix/sub/1", "[mat][sub][sparse]", Z, double, float) {
+    etl::sparse_matrix<Z> a(3, 2, std::initializer_list<Z>({1.0, 0.0, 0.0, 2.0, 3.0, 1.0}));
+    etl::sparse_matrix<Z> b(3, 2, std::initializer_list<Z>({2.0, 1.0, 0.0, 3.0, 0.0, 0.0}));
+    etl::sparse_matrix<Z> c(3, 2);
+
+    c = a - b;
+
+    REQUIRE(c.get(0,0) == -1.0);
+    REQUIRE(c.get(0,1) == -1.0);
+    REQUIRE(c.get(1,0) == 0.0);
+    REQUIRE(c.get(1,1) == -1.0);
+    REQUIRE(c.get(2,0) == 3.0);
+    REQUIRE(c.get(2,1) == 1.0);
+}
+
+TEMPLATE_TEST_CASE_2("sparse_matrix/mul/1", "[mat][mul][sparse]", Z, double, float) {
+    etl::sparse_matrix<Z> a(3, 2, std::initializer_list<Z>({1.0, 0.0, 0.0, 2.0, 3.0, 1.0}));
+    etl::sparse_matrix<Z> b(3, 2, std::initializer_list<Z>({2.0, 1.0, 0.0, 3.0, 0.0, 0.0}));
+    etl::sparse_matrix<Z> c(3, 2);
+
+    c = a >> b;
+
+    REQUIRE(c.get(0,0) == 2.0);
+    REQUIRE(c.get(0,1) == 0.0);
+    REQUIRE(c.get(1,0) == 0.0);
+    REQUIRE(c.get(1,1) == 6.0);
+    REQUIRE(c.get(2,0) == 0.0);
+    REQUIRE(c.get(2,1) == 0.0);
+}
+
+TEMPLATE_TEST_CASE_2("sparse_matrix/div/1", "[mat][div][sparse]", Z, double, float) {
+    etl::sparse_matrix<Z> a(3, 2, std::initializer_list<Z>({1.0, 0.0, 0.1, 2.0, 3.0, 1.0}));
+    etl::sparse_matrix<Z> b(3, 2, std::initializer_list<Z>({2.0, 1.0, 0.1, 3.0, 1.0, 3.0}));
+    etl::sparse_matrix<Z> c(3, 2);
+
+    c = a / b;
+
+    REQUIRE(c.get(0,0) == Approx(Z(0.5)));
+    REQUIRE(c.get(0,1) == Approx(Z(0.0)));
+    REQUIRE(c.get(1,0) == Approx(Z(1.0)));
+    REQUIRE(c.get(1,1) == Approx(Z(0.666666)));
+    REQUIRE(c.get(2,0) == Approx(Z(3.0)));
+    REQUIRE(c.get(2,1) == Approx(Z(0.333333)));
 }
