6349d56cb0696f7640bc475e78776c3e8673155e
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-12-16 11:34:14 +0100

    Refactorings (clang-tidy)

--- include/etl/expr/unary_expr.hpp
+++ include/etl/expr/unary_expr.hpp
@@ -64,8 +64,8 @@ public:
         //Nothing else to init
     }
 
-    unary_expr(const unary_expr& rhs) = default;
-    unary_expr(unary_expr&& rhs) = default;
+    unary_expr(const unary_expr& rhs) noexcept = default;
+    unary_expr(unary_expr&& rhs) noexcept = default;
 
     //Expression are invariant
     unary_expr& operator=(const unary_expr& rhs) = delete;
@@ -303,8 +303,8 @@ public:
         //Nothing else to init
     }
 
-    unary_expr(const unary_expr& rhs) = default;
-    unary_expr(unary_expr&& rhs) = default;
+    unary_expr(const unary_expr& rhs) noexcept = default;
+    unary_expr(unary_expr&& rhs) noexcept = default;
 
     //Expression are invariant
     unary_expr& operator=(const unary_expr& e) = delete;
@@ -379,8 +379,8 @@ public:
         //Nothing else to init
     }
 
-    unary_expr(const unary_expr& rhs) = default;
-    unary_expr(unary_expr&& rhs) = default;
+    unary_expr(const unary_expr& rhs) noexcept = default;
+    unary_expr(unary_expr&& rhs) noexcept = default;
 
     //Expression are invariant
     unary_expr& operator=(const unary_expr& e) = delete;
