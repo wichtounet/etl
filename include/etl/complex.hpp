d8b585898e8d757f145c0ad6130b6bd5f0f6ccb4
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-12-30 12:15:02 +0100

    Complete doc

--- include/etl/complex.hpp
+++ include/etl/complex.hpp
@@ -29,17 +29,31 @@ namespace etl {
  */
 template <typename T>
 struct complex {
-    using value_type = T;
+    using value_type = T; ///< The value type
 
-    value_type real;
-    value_type imag;
+    value_type real; ///< The real part
+    value_type imag; ///< The imaginary part
 
+    /*!
+     * \brief Construct a complex number
+     * \param re The real part
+     * \param im The imaginary part
+     */
     constexpr complex(const T& re = T(), const T& im = T())
             : real(re), imag(im) {}
 
+    /*!
+     * \brief Construct a complex number by copy
+     * \param rhs The complex to copy from
+     */
     constexpr complex(const complex& rhs)
             : real(rhs.real), imag(rhs.imag) {}
 
+    /*!
+     * \brief Assign a real part to the complex number
+     * \param rhs The real part
+     * \return a reference to this
+     */
     complex& operator=(const T& rhs) noexcept {
         real         = rhs;
         imag         = 0.0;
@@ -47,6 +61,11 @@ struct complex {
         return *this;
     }
 
+    /*!
+     * \brief Assign a complex number by copy
+     * \param rhs The complex number to copy
+     * \return a reference to this
+     */
     complex& operator=(const complex& rhs) noexcept {
         real         = rhs.real;
         imag         = rhs.imag;
@@ -54,6 +73,11 @@ struct complex {
         return *this;
     }
 
+    /*!
+     * \brief Assign a complex number by copy
+     * \param rhs The complex number to copy
+     * \return a reference to this
+     */
     complex& operator=(const std::complex<T>& rhs) noexcept {
         real         = rhs.real();
         imag         = rhs.imag();
@@ -61,16 +85,31 @@ struct complex {
         return *this;
     }
 
+    /*!
+     * \brief Adds a complex number
+     * \param rhs The complex number to add
+     * \return a reference to this
+     */
     void operator+=(const complex& rhs) {
         real += rhs.real;
         imag += rhs.imag;
     }
 
+    /*!
+     * \brief Subtracts a complex number
+     * \param rhs The complex number to add
+     * \return a reference to this
+     */
     void operator-=(const complex& rhs) {
         real -= rhs.real;
         imag -= rhs.imag;
     }
 
+    /*!
+     * \brief Multipliies a complex number
+     * \param rhs The complex number to add
+     * \return a reference to this
+     */
     void operator*=(const complex& rhs) {
         T ac = real * rhs.real;
         T bd = imag * rhs.imag;
@@ -82,6 +121,11 @@ struct complex {
         imag = bc + ad;
     }
 
+    /*!
+     * \brief Divides a complex number
+     * \param rhs The complex number to add
+     * \return a reference to this
+     */
     void operator/=(const complex& rhs) {
         T ac = real * rhs.real;
         T bd = imag * rhs.imag;
@@ -96,31 +140,66 @@ struct complex {
     }
 };
 
+/*!
+ * \brief Test two complex numbers for equality
+ * \param lhs The left hand side complex
+ * \param rhs The right hand side complex
+ * \return true if the numbers are equals, false otherwise
+ */
 template <typename T>
 inline bool operator==(const complex<T>& lhs, const complex<T>& rhs) {
     return lhs.real == rhs.real && lhs.imag == rhs.imag;
 }
 
+/*!
+ * \brief Test two complex numbers for inequality
+ * \param lhs The left hand side complex
+ * \param rhs The right hand side complex
+ * \return true if the numbers are not equals, false otherwise
+ */
 template <typename T>
 inline bool operator!=(const complex<T>& lhs, const complex<T>& rhs) {
     return !(lhs == rhs);
 }
 
+/*!
+ * \brief Returns a complex number with the value of -rhs
+ * \param rhs The right hand side complex
+ * \return a complex number with the value of -rhs
+ */
 template <typename T>
 inline complex<T> operator-(const complex<T>& rhs) {
     return {-rhs.real, -rhs.imag};
 }
 
+/*!
+ * \brief Computes the addition of two complex numbers
+ * \param lhs The left hand side complex
+ * \param rhs The right hand side complex
+ * \return a new complex with the value of the addition of the two complex numbers
+ */
 template <typename T>
 inline complex<T> operator+(const complex<T>& lhs, const complex<T>& rhs) {
     return {lhs.real + rhs.real, lhs.imag + rhs.imag};
 }
 
+/*!
+ * \brief Computes the subtraction of two complex numbers
+ * \param lhs The left hand side complex
+ * \param rhs The right hand side complex
+ * \return a new complex with the value of the subtraction of the two complex numbers
+ */
 template <typename T>
 inline complex<T> operator-(const complex<T>& lhs, const complex<T>& rhs) {
     return {lhs.real - rhs.real, lhs.imag - rhs.imag};
 }
 
+/*!
+ * \brief Computes the multiplication of two complex numbers
+ * \param lhs The left hand side complex
+ * \param rhs The right hand side complex
+ * \return a new complex with the value of the multiplication of the two complex numbers
+ */
 template <typename T>
 inline complex<T> operator*(const complex<T>& lhs, const complex<T>& rhs) {
     T ac = lhs.real * rhs.real;
@@ -132,21 +211,45 @@ inline complex<T> operator*(const complex<T>& lhs, const complex<T>& rhs) {
     return {ac - bd, bc + ad};
 }
 
+/*!
+ * \brief Computes the multiplication of a complex number and a scalar
+ * \param lhs The left hand side complex
+ * \param rhs The right hand side complex
+ * \return a new complex with the value of the multiplication of a complex number and a scalar
+ */
 template <typename T>
 inline complex<T> operator*(const complex<T>& lhs, T rhs) {
     return {lhs.real * rhs, lhs.imag * rhs};
 }
 
+/*!
+ * \brief Computes the multiplication of a complex number and a scalar
+ * \param lhs The left hand side complex
+ * \param rhs The right hand side complex
+ * \return a new complex with the value of the multiplication of a complex number and a scalar
+ */
 template <typename T>
 inline complex<T> operator*(T lhs, const complex<T>& rhs) {
     return {lhs * rhs.real, lhs * rhs.imag};
 }
 
+/*!
+ * \brief Computes the division of a complex number and a scalar
+ * \param lhs The left hand side complex
+ * \param rhs The right hand side complex
+ * \return a new complex with the value of the multiplication of a complex number and a scalar
+ */
 template <typename T>
 inline complex<T> operator/(const complex<T>& lhs, T rhs) {
     return {lhs.real / rhs, lhs.imag / rhs};
 }
 
+/*!
+ * \brief Computes the division of two complex numbers
+ * \param lhs The left hand side complex
+ * \param rhs The right hand side complex
+ * \return a new complex with the value of the division of the two complex numbers
+ */
 template <typename T>
 inline complex<T> operator/(const complex<T>& lhs, const complex<T>& rhs) {
     T ac = lhs.real * rhs.real;
@@ -160,56 +263,101 @@ inline complex<T> operator/(const complex<T>& lhs, const complex<T>& rhs) {
     return {(ac + bd) / frac, (bc - ad) / frac};
 }
 
-//inverse
+/*!
+ * \brief Returns the inverse of the complex number
+ * \param c The complex number
+ * \return The inverse of the complex number
+ */
 template <typename T>
 inline complex<T> inverse(complex<T> x) {
     return {x.imag, x.real};
 }
 
-//inverse of the conj
+/*!
+ * \brief Returns the inverse of the conjugate of the complex number
+ * \param c The complex number
+ * \return The inverse of the conjugate of the complex number
+ */
 template <typename T>
 inline complex<T> inverse_conj(complex<T> x) {
     return {-x.imag, x.real};
 }
 
-//conj of the inverse
+/*!
+ * \brief Returns the conjugate of the inverse of the complex number
+ * \param c The complex number
+ * \return The conjugate of the inverse of the complex number
+ */
 template <typename T>
 inline complex<T> conj_inverse(complex<T> x) {
     return {x.imag, -x.real};
 }
 
+/*!
+ * \brief Returns the conjugate of the complex number
+ * \param c The complex number
+ * \return The conjugate of the complex number
+ */
 template <typename T>
-inline complex<T> conj(const complex<T>& rhs) {
-    return {rhs.real, -rhs.imag};
+inline complex<T> conj(const complex<T>& c) {
+    return {c.real, -c.imag};
 }
 
-//Common API to etl::complex and std::complex
-
+/*!
+ * \brief Returns the imaginary part of the given complex number
+ * \param c The complex number
+ * \return the imaginary part of the given complex number
+ */
 template<typename T>
 inline T get_imag(const std::complex<T>& c){
     return c.imag();
 }
 
+/*!
+ * \brief Returns the imaginary part of the given complex number
+ * \param c The complex number
+ * \return the imaginary part of the given complex number
+ */
 template<typename T>
 inline T get_imag(const etl::complex<T>& c){
     return c.imag;
 }
 
+/*!
+ * \brief Returns the real part of the given complex number
+ * \param c The complex number
+ * \return the real part of the given complex number
+ */
 template<typename T>
 inline T get_real(const std::complex<T>& c){
     return c.real();
 }
 
+/*!
+ * \brief Returns the real part of the given complex number
+ * \param c The complex number
+ * \return the real part of the given complex number
+ */
 template<typename T>
 inline T get_real(const etl::complex<T>& c){
     return c.real;
 }
 
+/*!
+ * \brief Returns the conjugate of the given complex number
+ * \param c The complex number
+ * \return the conjugate of the given complex number
+ */
 template<typename T>
 inline std::complex<T> get_conj(const std::complex<T>& c){
     return std::conj(c);
 }
 
+/*!
+ * \brief Returns the conjugate of the given complex number
+ * \param c The complex number
+ * \return the conjugate of the given complex number
+ */
 template<typename T>
 inline etl::complex<T> get_conj(const etl::complex<T>& c){
     return {c.real, -c.imag};
