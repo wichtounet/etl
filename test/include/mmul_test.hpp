fcd4c0b0f6a07d33aaeb1cb6ef3f595363cd94ce
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-01-09 13:53:42 +0100

    Revert "Fix doc issues"
    This reverts commit 783bb5947e36c2a8bda596486108aceb5e8eff1a.

--- test/include/mmul_test.hpp
+++ test/include/mmul_test.hpp
@@ -6,16 +6,7 @@
 //=======================================================================
 
 #define MUL_FUNCTOR(name, ...)                        \
-    /*!                                               \
-     * \brief name functor                            \
-     */                                               \
     struct name {                                     \
-        /*!                                           \
-         * \brief Apply the functor                   \
-         * \param a a                                 \
-         * \param b a                                 \
-         * \param c target                            \
-         */                                           \
         template <typename A, typename B, typename C> \
         static void apply(A&& a, B&& b, C& c) {       \
             __VA_ARGS__;                              \
