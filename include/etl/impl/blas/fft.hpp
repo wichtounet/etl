081af057310fd6c69da73323d76f336d36b1f6bd
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-01-18 07:35:47 +0100

    Fix function

--- include/etl/impl/blas/fft.hpp
+++ include/etl/impl/blas/fft.hpp
@@ -677,7 +677,7 @@ template <typename A, typename B, typename C>
 void fft1_convolve(A&& /*unused*/, C&& /*unused*/) {}
 
 template <typename A, typename B, typename C>
-void fft2_convolve(A&& /*unused*/, C&& /*unused*/) {}
+void fft2_convolve(A&& /*unused*/, B&& /*unused*/, C&& /*unused*/) {}
 
 #endif
 
