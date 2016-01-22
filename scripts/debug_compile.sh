73e8f21d4c57ddb5f55216245b0d2ed725d8131d
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-01-22 12:47:27 +0100

    Compute ms per lines

--- scripts/debug_compile.sh
+++ scripts/debug_compile.sh
@@ -11,9 +11,13 @@ for f in test/src/*.cpp
 do
     results=$(/usr/bin/time -v make debug/$f.o 2>&1)
     rss=$(echo "$results" | grep "Maximum resident set size" | rev | cut -d" " -f1 | rev)
+    user_time=$(echo "$results" | grep "User time" | rev | cut -d" " -f1 | rev)
     elapsed=$(echo "$results" | grep "Elapsed" | rev | cut -d" " -f1 | rev)
+    lines=$(cat $f | wc -l)
     memory=$(echo "scale=2; $rss/1024/4" | bc -l)
-    echo "$f => $elapsed => ${memory}MB"
+    relative=$(echo "1000.0 * ($user_time/$lines)" | bc -l)
+    relative_fixed=$(echo "scale=2; $relative/1.0" | bc -l)
+    echo "$f => $elapsed => ${memory}MB => ${relative_fixed} ms/l"
 
     if [ $(echo "$max_memory < $memory" | bc) -eq 1 ]
     then
