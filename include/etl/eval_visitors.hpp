fa9447f0ef81bc27614837948282a1f8f544c1ad
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-04 14:10:44 +0100

    Smarter evaluation visitor

--- include/etl/eval_visitors.hpp
+++ include/etl/eval_visitors.hpp
@@ -41,25 +41,131 @@ struct temporary_allocator_static_visitor : etl_visitor<temporary_allocator_stat
     }
 };
 
-struct evaluator_static_visitor : etl_visitor<evaluator_static_visitor, false, true> {
+struct evaluator_static_visitor {
     template <typename E>
     using enabled = cpp::bool_constant<decay_traits<E>::needs_evaluator_visitor>;
 
-    using etl_visitor<evaluator_static_visitor, false, true>::operator();
+    mutable bool need_value = false;
 
     template <typename T, typename AExpr, typename Op, typename Forced>
     void operator()(etl::temporary_unary_expr<T, AExpr, Op, Forced>& v) const {
+        bool old_need_value = need_value;
+
+        need_value = Op::is_gpu;
         (*this)(v.a());
 
         v.evaluate();
+
+        if(old_need_value){
+            v.gpu_copy_from_if_necessary();
+        }
+
+        need_value = old_need_value;
     }
 
     template <typename T, typename AExpr, typename BExpr, typename Op, typename Forced>
     void operator()(etl::temporary_binary_expr<T, AExpr, BExpr, Op, Forced>& v) const {
+        bool old_need_value = need_value;
+
+        need_value = Op::is_gpu;
         (*this)(v.a());
+
+        need_value = Op::is_gpu;
         (*this)(v.b());
 
         v.evaluate();
+
+        if(old_need_value){
+            v.gpu_copy_from_if_necessary();
+        }
+
+        need_value = old_need_value;
+    }
+
+    /*!
+     * \brief Visit the given unary expression
+     * \param v The unary expression
+     */
+    template <typename T, typename Expr, typename UnaryOp>
+    void operator()(etl::unary_expr<T, Expr, UnaryOp>& v) const {
+        bool old_need_value = need_value;
+        need_value = true;
+        (*this)(v.value());
+        need_value = old_need_value;
+    }
+
+    /*!
+     * \brief Visit the given binary expression
+     * \param v The binary expression
+     */
+    template <typename T, typename LeftExpr, typename BinaryOp, typename RightExpr>
+    void operator()(etl::binary_expr<T, LeftExpr, BinaryOp, RightExpr>& v) const {
+        bool old_need_value = need_value;
+        need_value = true;
+        (*this)(v.lhs());
+        need_value = true;
+        (*this)(v.rhs());
+        need_value = old_need_value;
+    }
+
+    /*!
+     * \brief Visit the given view
+     * \param view The view
+     */
+    template <typename T, cpp_enable_if(etl::is_view<T>::value)>
+    void operator()(T& view) const {
+        bool old_need_value = need_value;
+        need_value = true;
+        (*this)(view.value());
+        need_value = old_need_value;
+    }
+
+    /*!
+     * \brief Visit the given matrix-multiplication transformer
+     * \param transformer The matrix-multiplication transformer
+     */
+    template <typename L, typename R>
+    void operator()(mm_mul_transformer<L, R>& transformer) const {
+        bool old_need_value = need_value;
+        need_value = true;
+        (*this)(transformer.lhs());
+        need_value = true;
+        (*this)(transformer.rhs());
+        need_value = old_need_value;
+    }
+
+    /*!
+     * \brief Visit the given transformer
+     * \param transformer The transformer
+     */
+    template <typename T, cpp_enable_if(etl::is_transformer<T>::value)>
+    void operator()(T& transformer) const {
+        bool old_need_value = need_value;
+        need_value = true;
+        (*this)(transformer.value());
+        need_value = old_need_value;
+    }
+
+    //The leaves don't need any special handling
+
+    template <typename Generator>
+    void operator()(const generator_expr<Generator>& /*unused*/) const {
+        //Leaf
+    }
+
+    template <typename T, cpp_enable_if(etl::is_magic_view<T>::value)>
+    void operator()(const T& /*unused*/) const {
+        //Leaf
+    }
+
+    template <typename T, cpp_enable_if(etl::is_etl_value<T>::value)>
+    void operator()(const T& /*unused*/) const {
+        //Leaf
+    }
+
+    template <typename T>
+    void operator()(const etl::scalar<T>& /*unused*/) const {
+        //Leaf
     }
 };
 
