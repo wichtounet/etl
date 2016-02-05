9061b53a89b04f9082d03f9976273c10bbbaed66
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-05 08:22:28 +0100

    Introduce the notion of local context

---
+++ include/etl/context.hpp
@@ -0,0 +1,21 @@
+//=======================================================================
+// Copyright (c) 2014-2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#pragma once
+
+namespace etl {
+
+struct context {
+    bool serial = false;
+};
+
+inline context local_context(){
+    static thread_local context local_context;
+    return local_context;
+}
+
+} //end of namespace etl
