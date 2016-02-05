13a36e42ec4685d6661818ac9ad8abe844134d56
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-05 09:28:20 +0100

    Add tests for SERIAL_SECTION

--- test/src/serial.cpp
+++ test/src/serial.cpp
@@ -15,3 +15,41 @@ TEST_CASE("serial/1", "[fast][serial]") {
 
     REQUIRE(b[0] == 2.0);
 }
+
+TEST_CASE("serial_section/1", "[fast][serial]") {
+    etl::fast_vector<double, 3> a({1.0, -2.0, 3.0});
+    etl::fast_vector<double, 3> b;
+
+    REQUIRE(!etl::local_context().serial);
+
+    SERIAL_SECTION {
+        REQUIRE(etl::local_context().serial);
+        b = a + a;
+        b += a + a;
+    }
+
+    REQUIRE(!etl::local_context().serial);
+
+    REQUIRE(b[0] == 4.0);
+}
+
+TEST_CASE("serial_section/2", "[fast][serial]") {
+    etl::fast_vector<double, 3> a({1.0, -2.0, 3.0});
+    etl::fast_vector<double, 3> b;
+
+    REQUIRE(!etl::local_context().serial);
+
+    etl::local_context().serial = true;
+
+    SERIAL_SECTION {
+        REQUIRE(etl::local_context().serial);
+        b = a + a;
+        b += a + a;
+
+        etl::local_context().serial = false;
+    }
+
+    REQUIRE(etl::local_context().serial);
+
+    REQUIRE(b[0] == 4.0);
+}
