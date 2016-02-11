ff2b4ec35a6b6eb09925446e61d5034f4018c58b
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-11 09:50:43 +0100

    Add tags

--- benchmark/src/benchmark_cdbn.cpp
+++ benchmark/src/benchmark_cdbn.cpp
@@ -10,14 +10,14 @@
 
 CPM_DIRECT_BENCH_TWO_PASS_NS_P(
     NARY_POLICY(VALUES_POLICY(100, 500, 550, 600, 1000, 2000, 2500), VALUES_POLICY(100, 120, 500, 1000, 1200, 2000, 5000)),
-    "rbm_hidden",
+    "rbm_hidden [rbm]",
     [](std::size_t d1, std::size_t d2){ return std::make_tuple(dvec(d1), dvec(d2), dvec(d2), dvec(d2), dmat(d1, d2));},
     [](dvec& v, dvec& h, dvec& b, dvec& t, dmat& w){ h = etl::sigmoid(b + etl::mul(v, w, t)); }
 )
 
 CPM_DIRECT_BENCH_TWO_PASS_NS_P(
     NARY_POLICY(VALUES_POLICY(100, 500, 550, 600, 1000, 2000, 2500), VALUES_POLICY(100, 120, 500, 1000, 1200, 2000, 5000)),
-    "rbm_visible",
+    "rbm_visible [rbm]",
     [](std::size_t d1, std::size_t d2){ return std::make_tuple(dvec(d1), dvec(d2), dvec(d1), dvec(d1), dmat(d1, d2));},
     [](dvec& v, dvec& h, dvec& c, dvec& t, dmat& w){ v = etl::sigmoid(c + etl::mul(w, h, t)); }
 )
@@ -29,7 +29,7 @@ CPM_DIRECT_BENCH_TWO_PASS_NS_P(
 
 CPM_DIRECT_BENCH_TWO_PASS_NS_P(
     NARY_POLICY(VALUES_POLICY(1, 1, 1, 3, 3, 3, 30, 40), VALUES_POLICY(10, 10, 30, 30, 30, 40, 40, 40), VALUES_POLICY(28, 28, 28, 28, 36, 36, 36, 36), VALUES_POLICY(5, 11, 19, 19, 19, 19, 19, 19)),
-    "conv_rbm_hidden",
+    "conv_rbm_hidden [crbm]",
     [](std::size_t nc, std::size_t k, std::size_t nv, std::size_t nh){
         auto nw = nv - nh + 1;
         return std::make_tuple(dmat4(nc,k,nw,nw), dmat4(nc,k,nw,nw), dvec(k), dmat3(nc,nv,nv), dmat3(k, nh, nh), dmat4(2ul, k, nh, nh));},
@@ -56,7 +56,7 @@ CPM_DIRECT_BENCH_TWO_PASS_NS_P(
 
 CPM_DIRECT_BENCH_TWO_PASS_NS_P(
     NARY_POLICY(VALUES_POLICY(1, 1, 1, 3, 3, 3, 30, 40), VALUES_POLICY(10, 10, 30, 30, 30, 40, 40, 40), VALUES_POLICY(28, 28, 28, 28, 36, 36, 36, 36), VALUES_POLICY(5, 11, 19, 19, 19, 19, 19, 19)),
-    "conv_rbm_visible",
+    "conv_rbm_visible [crbm]",
     [](std::size_t nc, std::size_t k, std::size_t nv, std::size_t nh){
         auto nw = nv - nh + 1;
         return std::make_tuple(dmat4(nc,k,nw,nw), dvec(k), dvec(nc), dmat3(nc,nv,nv), dmat3(k,nh,nh), dmat3(k,nv,nv));},
