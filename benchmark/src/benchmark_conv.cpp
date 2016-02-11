0d0ab819a2ba7d572f13234cbfda70716994e3f4
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-11 10:51:30 +0100

    Review conv benchmarks

--- benchmark/src/benchmark_conv.cpp
+++ benchmark/src/benchmark_conv.cpp
@@ -11,24 +11,24 @@
 //1D-Convolution benchmarks with large-kernel
 CPM_BENCH() {
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000), VALUES_POLICY(500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000)),
-        "r = conv_1d_full(a,b)(large) [conv]",
+        large_kernel_policy,
+        "r = conv_1d_full(a,b)(large) [conv][conv1]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dvec(d1), dvec(d2), dvec(d1 + d2 - 1)); },
         [](dvec& a, dvec& b, dvec& r){ r = etl::conv_1d_full(a, b); },
         [](std::size_t d1, std::size_t d2){ return 2 * d1 * d2; }
         );
 
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000), VALUES_POLICY(500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000)),
-        "r = conv_1d_same(a,b)(large) [conv]",
+        large_kernel_policy,
+        "r = conv_1d_same(a,b)(large) [conv][conv1]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dvec(d1), dvec(d2), dvec(d1)); },
         [](dvec& a, dvec& b, dvec& r){ r = etl::conv_1d_same(a, b); },
         [](std::size_t d1, std::size_t d2){ return 2 * d1 * d2; }
         );
 
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000), VALUES_POLICY(500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000)),
-        "r = conv_1d_valid(a,b)(large) [conv]",
+        large_kernel_policy,
+        "r = conv_1d_valid(a,b)(large) [conv][conv1]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dvec(d1), dvec(d2), dvec(d1 - d2 + 1)); },
         [](dvec& a, dvec& b, dvec& r){ r = etl::conv_1d_valid(a, b); },
         [](std::size_t d1, std::size_t d2){ return 2 * d1 * d2; }
@@ -38,24 +38,24 @@ CPM_BENCH() {
 //1D-Convolution benchmarks with small-kernel
 CPM_BENCH() {
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000), VALUES_POLICY(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)),
-        "r = conv_1d_full(a,b)(small) [conv]",
+        small_kernel_policy,
+        "r = conv_1d_full(a,b)(small) [conv][conv1]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dvec(d1), dvec(d2), dvec(d1 + d2 - 1)); },
         [](dvec& a, dvec& b, dvec& r){ r = etl::conv_1d_full(a, b); }
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d2; }
         );
 
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000), VALUES_POLICY(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)),
-        "r = conv_1d_same(a,b)(small) [conv]",
+        small_kernel_policy,
+        "r = conv_1d_same(a,b)(small) [conv][conv1]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dvec(d1), dvec(d2), dvec(d1)); },
         [](dvec& a, dvec& b, dvec& r){ r = etl::conv_1d_same(a, b); }
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d2; }
         );
 
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000), VALUES_POLICY(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)),
-        "r = conv_1d_valid(a,b)(small) [conv]",
+        small_kernel_policy,
+        "r = conv_1d_valid(a,b)(small) [conv][conv1]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dvec(d1), dvec(d2), dvec(d1 - d2 + 1)); },
         [](dvec& a, dvec& b, dvec& r){ r = etl::conv_1d_valid(a, b); }
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d2; }
@@ -65,24 +65,24 @@ CPM_BENCH() {
 //2D-Convolution benchmarks with large-kernel
 CPM_BENCH() {
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(100, 110, 120, 130, 140, 150, 160, 170, 180), VALUES_POLICY(50, 55, 60, 65, 70, 75, 80, 85, 90)),
-        "r = conv_2d_full(a,b)(large) [conv]",
+        large_kernel_policy_2d,
+        "r = conv_2d_full(a,b)(large) [conv][conv2]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat(d2, d2), dmat(d1 + d2 - 1, d1 + d2 - 1)); },
         [](dmat& a, dmat& b, dmat& r){ r = etl::conv_2d_full(a, b); }
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }
         );
 
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(100, 110, 120, 130, 140, 150, 160, 170, 180), VALUES_POLICY(50, 55, 60, 65, 70, 75, 80, 85, 90)),
-        "r = conv_2d_same(a,b)(large) [conv]",
+        large_kernel_policy_2d,
+        "r = conv_2d_same(a,b)(large) [conv][conv2]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat(d2, d2), dmat(d1, d1)); },
         [](dmat& a, dmat& b, dmat& r){ r = etl::conv_2d_same(a, b); }
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }
         );
 
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(100, 110, 120, 130, 140, 150, 160, 170, 180), VALUES_POLICY(50, 55, 60, 65, 70, 75, 80, 85, 90)),
-        "r = conv_2d_valid(a,b)(large) [conv]",
+        large_kernel_policy_2d,
+        "r = conv_2d_valid(a,b)(large) [conv][conv2]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat(d2, d2), dmat(d1 - d2 + 1, d1 - d2 + 1)); },
         [](dmat& a, dmat& b, dmat& r){ r = etl::conv_2d_valid(a, b); }
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }
@@ -92,24 +92,24 @@ CPM_BENCH() {
 //2D-Convolution benchmarks with small-kernel
 CPM_BENCH() {
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(100, 150, 200, 250, 300, 350, 400), VALUES_POLICY(10, 15, 20, 25, 30, 35, 40)),
-        "r = conv_2d_full(a,b)(small) [conv]",
+        small_kernel_policy_2d,
+        "r = conv_2d_full(a,b)(small) [conv][conv2]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat(d2, d2), dmat(d1 + d2 - 1, d1 + d2 - 1)); },
         [](dmat& a, dmat& b, dmat& r){ r = etl::conv_2d_full(a, b); }
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }
         );
 
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(100, 150, 200, 250, 300, 350, 400), VALUES_POLICY(10, 15, 20, 25, 30, 35, 40)),
-        "r = conv_2d_same(a,b)(small) [conv]",
+        small_kernel_policy_2d,
+        "r = conv_2d_same(a,b)(small) [conv][conv2]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat(d2, d2), dmat(d1, d1)); },
         [](dmat& a, dmat& b, dmat& r){ r = etl::conv_2d_same(a, b); }
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }
         );
 
     CPM_TWO_PASS_NS_P(
-        NARY_POLICY(VALUES_POLICY(100, 150, 200, 250, 300, 350, 400), VALUES_POLICY(10, 15, 20, 25, 30, 35, 40)),
-        "r = conv_2d_valid(a,b)(small) [conv]",
+        small_kernel_policy_2d,
+        "r = conv_2d_valid(a,b)(small) [conv][conv2]",
         [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat(d2, d2), dmat(d1 - d2 + 1, d1 - d2 + 1)); },
         [](dmat& a, dmat& b, dmat& r){ r = etl::conv_2d_valid(a, b); }
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }
@@ -118,20 +118,21 @@ CPM_BENCH() {
 
 CPM_DIRECT_BENCH_TWO_PASS_P(
     NARY_POLICY(VALUES_POLICY(16, 16, 32, 32, 64, 64), VALUES_POLICY(4, 8, 8, 16, 16, 24)),
-    "convmtx2",
-    [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat((d1 + d2 - 1)*(d1 + d2 - 1), d2 * d2)); }, [](std::size_t /*d1*/, std::size_t d2, dmat& a, dmat& b){ b = etl::convmtx2(a, d2, d2); }
+    "convmtx2 [conv][convmtx]",
+    [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat((d1 + d2 - 1)*(d1 + d2 - 1), d2 * d2)); },
+    [](std::size_t /*d1*/, std::size_t d2, dmat& a, dmat& b){ b = etl::convmtx2(a, d2, d2); }
 )
 
 CPM_DIRECT_BENCH_TWO_PASS_P(
     NARY_POLICY(VALUES_POLICY(16, 16, 32, 32, 64, 64, 128), VALUES_POLICY(4, 8, 8, 16, 16, 32, 32)),
-    "convmtx2_t",
+    "convmtx2_t [conv][convmtx]",
     [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat((d1 + d2 - 1)*(d1 + d2 - 1), d2 * d2)); },
     [](std::size_t /*d1*/, std::size_t d2, dmat& a, dmat& b){ etl::convmtx2_direct_t(b, a, d2, d2); }
 )
 
 CPM_DIRECT_BENCH_TWO_PASS_P(
     NARY_POLICY(VALUES_POLICY(16, 16, 32, 32, 64, 64, 128), VALUES_POLICY(4, 8, 8, 16, 16, 32, 32)),
-    "im2col_direct",
+    "im2col_direct [conv][im2col]",
     [](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1, d1), dmat(d2 * d2, (d1 - d2 + 1)*(d1 - d2 + 1))); },
     [](std::size_t /*d1*/, std::size_t d2, dmat& a, dmat& b){ etl::im2col_direct(b, a, d2, d2); }
 )
@@ -154,6 +155,7 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv1_valid [conv][conv1]", conv_1d_large_po
     SSE_SECTION_FUNCTOR("avx", [](dvec& a, dvec& b, dvec& r){ etl::detail::conv1_valid_direct(a, b, r, etl::detail::conv_impl::AVX); })
 )
 
+#ifdef ETL_EXTENDED_BENCH
 CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv1_same [conv][conv1]", conv_1d_large_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d2; }),
     CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(svec(d1), svec(d2), svec(d1)); }),
@@ -171,6 +173,7 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv1_same [conv][conv1]", conv_1d_large_pol
     SSE_SECTION_FUNCTOR("sse", [](dvec& a, dvec& b, dvec& r){ etl::detail::conv1_same_direct(a, b, r, etl::detail::conv_impl::SSE); })
     SSE_SECTION_FUNCTOR("avx", [](dvec& a, dvec& b, dvec& r){ etl::detail::conv1_same_direct(a, b, r, etl::detail::conv_impl::AVX); })
 )
+#endif
 
 CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv1_full [conv][conv1]", conv_1d_large_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d2; }),
@@ -194,7 +197,7 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv1_full [conv][conv1]", conv_1d_large_pol
      MC_SECTION_FUNCTOR("mmul", [](dvec& a, dvec& b, dvec& r){ etl::impl::reduc::conv1_full(a, b, r); })
 )
 
-CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv2_valid [conv]", conv_2d_large_policy,
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv2_valid [conv][conv2]", conv_2d_large_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }),
     CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(smat(d1,d1), smat(d2,d2), smat(d1 - d2 + 1, d1 - d2 + 1)); }),
     CPM_SECTION_FUNCTOR("default", [](smat& a, smat& b, smat& r){ r = etl::conv_2d_valid(a, b); }),
@@ -203,7 +206,7 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv2_valid [conv]", conv_2d_large_policy,
     AVX_SECTION_FUNCTOR("avx", [](smat& a, smat& b, smat& r){ etl::impl::avx::conv2_valid(a, b, r); })
 )
 
-CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv2_valid [conv]", conv_2d_large_policy,
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv2_valid [conv][conv2]", conv_2d_large_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }),
     CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1,d1), dmat(d2,d2), dmat(d1 - d2 + 1, d1 - d2 + 1)); }),
     CPM_SECTION_FUNCTOR("default", [](dmat& a, dmat& b, dmat& r){ r = etl::conv_2d_valid(a, b); }),
@@ -212,7 +215,8 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv2_valid [conv]", conv_2d_large_policy,
     AVX_SECTION_FUNCTOR("avx", [](dmat& a, dmat& b, dmat& r){ etl::impl::avx::conv2_valid(a, b, r); })
 )
 
-CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv2_same [conv]", conv_2d_large_policy,
+#ifdef ETL_EXTENDED_BENCH
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv2_same [conv][conv2]", conv_2d_large_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }),
     CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(smat(d1,d1), smat(d2,d2), smat(d1,d1)); }),
     CPM_SECTION_FUNCTOR("default", [](smat& a, smat& b, smat& r){ r = etl::conv_2d_same(a, b); }),
@@ -221,7 +225,7 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv2_same [conv]", conv_2d_large_policy,
     AVX_SECTION_FUNCTOR("avx", [](smat& a, smat& b, smat& r){ etl::impl::avx::conv2_same(a, b, r); })
 )
 
-CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv2_same [conv]", conv_2d_large_policy,
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv2_same [conv][conv2]", conv_2d_large_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }),
     CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1,d1), dmat(d2,d2), dmat(d1,d1)); }),
     CPM_SECTION_FUNCTOR("default", [](dmat& a, dmat& b, dmat& r){ r = etl::conv_2d_same(a, b); }),
@@ -229,8 +233,9 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv2_same [conv]", conv_2d_large_policy,
     SSE_SECTION_FUNCTOR("sse", [](dmat& a, dmat& b, dmat& r){ etl::impl::sse::conv2_same(a, b, r); })
     AVX_SECTION_FUNCTOR("avx", [](dmat& a, dmat& b, dmat& r){ etl::impl::avx::conv2_same(a, b, r); })
 )
+#endif
 
-CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv2_full [conv]", conv_2d_large_policy,
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv2_full [conv][conv2]", conv_2d_large_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }),
     CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(smat(d1,d1), smat(d2,d2), smat(d1 + d2 - 1, d1 + d2 - 1)); }),
     CPM_SECTION_FUNCTOR("default", [](smat& a, smat& b, smat& r){ r = etl::conv_2d_full(a, b); }),
@@ -241,7 +246,7 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("sconv2_full [conv]", conv_2d_large_policy,
      MC_SECTION_FUNCTOR("mmul", [](smat& a, smat& b, smat& r){ etl::impl::reduc::conv2_full(a, b, r); })
 )
 
-CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv2_full [conv]", conv_2d_large_policy,
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("dconv2_full [conv][conv2]", conv_2d_large_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d1 * d2 * d2; }),
     CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(dmat(d1,d1), dmat(d2,d2), dmat(d1 + d2 - 1, d1 + d2 - 1)); }),
     CPM_SECTION_FUNCTOR("default", [](dmat& a, dmat& b, dmat& r){ r = etl::conv_2d_full(a, b); }),
