68513c67e6a4d39418e5f3ceaa4288821d09485e
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-17 12:16:46 +0100

    New im2col tests for non-square inputs

--- test/src/im2col.cpp
+++ test/src/im2col.cpp
@@ -114,3 +114,46 @@ TEMPLATE_TEST_CASE_2("im2col/im2col_4", "im2col", Z, double, float) {
     REQUIRE(C(0, 5) == 8);
     REQUIRE(C(1, 5) == 9);
 }
+
+TEMPLATE_TEST_CASE_2("im2col/im2col_5", "im2col", Z, double, float) {
+    etl::dyn_matrix<Z> I(3, 4, etl::values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
+    etl::dyn_matrix<Z> C(4, 6);
+
+    etl::im2col_direct(C, I, 2, 2);
+
+    REQUIRE(C(0, 0) == 1);
+    REQUIRE(C(1, 0) == 5);
+    REQUIRE(C(2, 0) == 2);
+    REQUIRE(C(3, 0) == 6);
+
+    REQUIRE(C(0, 1) == 5);
+    REQUIRE(C(1, 1) == 9);
+    REQUIRE(C(2, 1) == 6);
+    REQUIRE(C(3, 1) == 10);
+
+    REQUIRE(C(0, 3) == 6);
+    REQUIRE(C(1, 3) == 10);
+    REQUIRE(C(2, 3) == 7);
+    REQUIRE(C(3, 3) == 11);
+
+    REQUIRE(C(0, 5) == 7);
+    REQUIRE(C(1, 5) == 11);
+    REQUIRE(C(2, 5) == 8);
+    REQUIRE(C(3, 5) == 12);
+}
+
+TEMPLATE_TEST_CASE_2("im2col/im2col_6", "im2col", Z, double, float) {
+    etl::dyn_matrix<Z> I(2, 3, etl::values(1, 2, 3, 4, 5, 6));
+    etl::dyn_matrix<Z> C(2, 3);
+
+    etl::im2col_direct(C, I, 2, 1);
+
+    REQUIRE(C(0, 0) == 1);
+    REQUIRE(C(1, 0) == 4);
+
+    REQUIRE(C(0, 1) == 2);
+    REQUIRE(C(1, 1) == 5);
+
+    REQUIRE(C(0, 2) == 3);
+    REQUIRE(C(1, 2) == 6);
+}
