af2f54abbb68d3c220f8c2edb6b331c792cd95c6
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-22 13:39:50 +0100

    New inplace transpositions tests

--- test/src/transpose.cpp
+++ test/src/transpose.cpp
@@ -257,3 +257,80 @@ TEMPLATE_TEST_CASE_2("deep_transpose/2", "[dyn][trans]", Z, float, double) {
     REQUIRE(a(1, 1, 2, 1) == 6.0);
     REQUIRE(a(1, 1, 2, 2) == 9.0);
 }
+
+TEMPLATE_TEST_CASE_2("deep_transpose/3", "[dyn][trans]", Z, float, double) {
+    etl::dyn_matrix<Z, 4> a(2, 2, 3, 3,
+        etl::values<Z>(1, 2, 3, 4, 5, 6, 7, 8, 9,
+                       1, 2, 3, 4, 5, 6, 7, 8, 9,
+                       1, 2, 3, 4, 5, 6, 7, 8, 9,
+                       1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+    a(0).deep_transpose_inplace();
+    a(1).deep_transpose_inplace();
+
+    REQUIRE(a.dim(0) == 2);
+    REQUIRE(a.dim(1) == 2);
+    REQUIRE(a.dim(2) == 3);
+    REQUIRE(a.dim(3) == 3);
+
+    REQUIRE(a(0, 0, 0, 0) == 1.0);
+    REQUIRE(a(0, 0, 0, 1) == 4.0);
+    REQUIRE(a(0, 0, 0, 2) == 7.0);
+    REQUIRE(a(0, 0, 1, 0) == 2.0);
+    REQUIRE(a(0, 0, 1, 1) == 5.0);
+    REQUIRE(a(0, 0, 1, 2) == 8.0);
+    REQUIRE(a(0, 0, 2, 0) == 3.0);
+    REQUIRE(a(0, 0, 2, 1) == 6.0);
+    REQUIRE(a(0, 0, 2, 2) == 9.0);
+
+    REQUIRE(a(1, 1, 0, 0) == 1.0);
+    REQUIRE(a(1, 1, 0, 1) == 4.0);
+    REQUIRE(a(1, 1, 0, 2) == 7.0);
+    REQUIRE(a(1, 1, 1, 0) == 2.0);
+    REQUIRE(a(1, 1, 1, 1) == 5.0);
+    REQUIRE(a(1, 1, 1, 2) == 8.0);
+    REQUIRE(a(1, 1, 2, 0) == 3.0);
+    REQUIRE(a(1, 1, 2, 1) == 6.0);
+    REQUIRE(a(1, 1, 2, 2) == 9.0);
+}
+
+TEMPLATE_TEST_CASE_2("deep_transpose/4", "[dyn][trans]", Z, float, double) {
+    etl::dyn_matrix<Z, 4> a(2, 2, 4, 3,
+        etl::values<Z>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
+
+    a.deep_transpose_inplace();
+
+    REQUIRE(a.dim(0) == 2);
+    REQUIRE(a.dim(1) == 2);
+    REQUIRE(a.dim(2) == 3);
+    REQUIRE(a.dim(3) == 4);
+
+    REQUIRE(a(0, 0, 0, 0) == 1.0);
+    REQUIRE(a(0, 0, 0, 1) == 4.0);
+    REQUIRE(a(0, 0, 0, 2) == 7.0);
+    REQUIRE(a(0, 0, 0, 3) == 10.0);
+    REQUIRE(a(0, 0, 1, 0) == 2.0);
+    REQUIRE(a(0, 0, 1, 1) == 5.0);
+    REQUIRE(a(0, 0, 1, 2) == 8.0);
+    REQUIRE(a(0, 0, 1, 3) == 11.0);
+    REQUIRE(a(0, 0, 2, 0) == 3.0);
+    REQUIRE(a(0, 0, 2, 1) == 6.0);
+    REQUIRE(a(0, 0, 2, 2) == 9.0);
+    REQUIRE(a(0, 0, 2, 3) == 12.0);
+
+    REQUIRE(a(1, 1, 0, 0) == 1.0);
+    REQUIRE(a(1, 1, 0, 1) == 4.0);
+    REQUIRE(a(1, 1, 0, 2) == 7.0);
+    REQUIRE(a(1, 1, 0, 3) == 10.0);
+    REQUIRE(a(1, 1, 1, 0) == 2.0);
+    REQUIRE(a(1, 1, 1, 1) == 5.0);
+    REQUIRE(a(1, 1, 1, 2) == 8.0);
+    REQUIRE(a(1, 1, 1, 3) == 11.0);
+    REQUIRE(a(1, 1, 2, 0) == 3.0);
+    REQUIRE(a(1, 1, 2, 1) == 6.0);
+    REQUIRE(a(1, 1, 2, 2) == 9.0);
+    REQUIRE(a(1, 1, 2, 3) == 12.0);
+}
