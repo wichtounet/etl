953ec9e243b8c27dda481a8997a0805f59112228
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-25 16:09:29 +0100

    New test

--- test/src/slice.cpp
+++ test/src/slice.cpp
@@ -128,3 +128,26 @@ TEMPLATE_TEST_CASE_2("slice/5", "[slice]", Z, float, double) {
     REQUIRE(a(1, 0, 1) == 3.0);
     REQUIRE(a(1, 0, 2) == 3.0);
 }
+
+TEMPLATE_TEST_CASE_2("slice/6", "[slice]", Z, float, double) {
+    etl::fast_matrix<Z, 2, 1, 3> a;
+
+    a.slice(0, 1) = 1.0;
+    a.slice(1, 2) = 2.0;
+
+    REQUIRE(a(0, 0, 0) == 1.0);
+    REQUIRE(a(0, 0, 1) == 1.0);
+    REQUIRE(a(0, 0, 2) == 1.0);
+    REQUIRE(a(1, 0, 0) == 2.0);
+    REQUIRE(a(1, 0, 1) == 2.0);
+    REQUIRE(a(1, 0, 2) == 2.0);
+
+    a.slice(0, 2) = 3.0;
+
+    REQUIRE(a(0, 0, 0) == 3.0);
+    REQUIRE(a(0, 0, 1) == 3.0);
+    REQUIRE(a(0, 0, 2) == 3.0);
+    REQUIRE(a(1, 0, 0) == 3.0);
+    REQUIRE(a(1, 0, 1) == 3.0);
+    REQUIRE(a(1, 0, 2) == 3.0);
+}
