186fd278b7999ef112ad46740589a878699994ae
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-25 15:44:59 +0100

    More tests

--- test/src/slice.cpp
+++ test/src/slice.cpp
@@ -1,19 +1,16 @@
 //=======================================================================
-// Copyright (c) 2014-2016 Baptiste Wicht // Distributed under the terms of the MIT License.
+// Copyright (c) 2014-2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
 // (See accompanying file LICENSE or copy at
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
 
 #include "test_light.hpp"
 
-#include <cmath>
-
-// Init tests
-
 TEMPLATE_TEST_CASE_2("slice/1", "[slice]", Z, float, double) {
-    etl::fast_matrix<Z, 2, 3> test_matrix = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+    etl::fast_matrix<Z, 2, 3> a = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
 
-    auto s1 = slice(test_matrix, 0, 1);
+    auto s1 = slice(a, 0, 1);
 
     REQUIRE(etl::size(s1) == 3);
 
@@ -25,7 +22,7 @@ TEMPLATE_TEST_CASE_2("slice/1", "[slice]", Z, float, double) {
     REQUIRE(s1(0, 1) == 2);
     REQUIRE(s1(0, 2) == 3);
 
-    auto s2 = slice(test_matrix, 1, 2);
+    auto s2 = slice(a, 1, 2);
 
     REQUIRE(etl::size(s2) == 3);
 
@@ -37,7 +34,7 @@ TEMPLATE_TEST_CASE_2("slice/1", "[slice]", Z, float, double) {
     REQUIRE(s2(0, 1) == 5);
     REQUIRE(s2(0, 2) == 6);
 
-    auto s3 = slice(test_matrix, 0, 2);
+    auto s3 = slice(a, 0, 2);
 
     REQUIRE(etl::size(s3) == 6);
 
@@ -51,4 +48,83 @@ TEMPLATE_TEST_CASE_2("slice/1", "[slice]", Z, float, double) {
     REQUIRE(s3(1, 0) == 4);
     REQUIRE(s3(1, 1) == 5);
     REQUIRE(s3(1, 2) == 6);
+}
+
+TEMPLATE_TEST_CASE_2("slice/2", "[slice]", Z, float, double) {
+    etl::fast_matrix<Z, 5, 8, 2, 3> a;
+
+    auto s1 = slice(a, 3, 5);
+
+    REQUIRE(etl::size(s1) == 2 * 8 * 2 * 3);
+
+    REQUIRE(etl::dim<0>(s1) == 2);
+    REQUIRE(etl::dim<1>(s1) == 8);
+    REQUIRE(etl::dim<2>(s1) == 2);
+    REQUIRE(etl::dim<3>(s1) == 3);
+    REQUIRE(etl::dimensions(s1) == 4);
+
+    auto s2 = slice(a, 1, 2);
+
+    REQUIRE(etl::size(s2) == 1 * 8 * 2 * 3);
+
+    REQUIRE(etl::dim<0>(s2) == 1);
+    REQUIRE(etl::dim<1>(s2) == 8);
+    REQUIRE(etl::dim<2>(s2) == 2);
+    REQUIRE(etl::dim<3>(s2) == 3);
+    REQUIRE(etl::dimensions(s2) == 4);
+
+    auto s3 = slice(a, 0, 5);
+
+    REQUIRE(etl::size(s3) == 5 * 8 * 2 * 3);
+
+    REQUIRE(etl::dim<0>(s3) == 5);
+    REQUIRE(etl::dim<1>(s3) == 8);
+    REQUIRE(etl::dim<2>(s3) == 2);
+    REQUIRE(etl::dim<3>(s3) == 3);
+    REQUIRE(etl::dimensions(s3) == 4);
+}
+
+TEMPLATE_TEST_CASE_2("slice/3", "[slice]", Z, float, double) {
+    etl::fast_matrix<Z, 2, 3> a = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+    etl::fast_matrix<Z, 1, 3> b;
+
+    b = etl::slice(a, 0, 1) + etl::slice(a, 1, 2);
+
+    REQUIRE(b(0, 0) == 5.0);
+    REQUIRE(b(0, 1) == 7.0);
+    REQUIRE(b(0, 2) == 9.0);
+}
+
+TEMPLATE_TEST_CASE_2("slice/4", "[slice]", Z, float, double) {
+    etl::fast_matrix<Z, 2, 1, 3> a = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+    etl::fast_matrix<Z, 1, 1, 3> b;
+
+    b = etl::slice(a, 0, 1) + etl::slice(a, 1, 2);
+
+    REQUIRE(b(0, 0, 0) == 5.0);
+    REQUIRE(b(0, 0, 1) == 7.0);
+    REQUIRE(b(0, 0, 2) == 9.0);
+}
+
+TEMPLATE_TEST_CASE_2("slice/5", "[slice]", Z, float, double) {
+    etl::fast_matrix<Z, 2, 1, 3> a;
+
+    etl::slice(a, 0, 1) = 1.0;
+    etl::slice(a, 1, 2) = 2.0;
+
+    REQUIRE(a(0, 0, 0) == 1.0);
+    REQUIRE(a(0, 0, 1) == 1.0);
+    REQUIRE(a(0, 0, 2) == 1.0);
+    REQUIRE(a(1, 0, 0) == 2.0);
+    REQUIRE(a(1, 0, 1) == 2.0);
+    REQUIRE(a(1, 0, 2) == 2.0);
+
+    etl::slice(a, 0, 2) = 3.0;
+
+    REQUIRE(a(0, 0, 0) == 3.0);
+    REQUIRE(a(0, 0, 1) == 3.0);
+    REQUIRE(a(0, 0, 2) == 3.0);
+    REQUIRE(a(1, 0, 0) == 3.0);
+    REQUIRE(a(1, 0, 1) == 3.0);
+    REQUIRE(a(1, 0, 2) == 3.0);
 }
