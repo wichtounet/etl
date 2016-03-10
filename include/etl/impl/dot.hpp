5b323667b4814b21353e160f1ea43bac76a3e997
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-03-10 08:56:12 +0100

    Implement selection for dot

--- include/etl/impl/dot.hpp
+++ include/etl/impl/dot.hpp
@@ -22,12 +22,15 @@ namespace detail {
 
 /*!
  * \brief Select the dot implementation for an expression of type A and B
+ *
+ * This does not take the local context into account
+ *
  * \tparam A The type of lhs expression
  * \tparam B The type of rhs expression
  * \return The implementation to use
  */
 template <typename A, typename B>
-cpp14_constexpr etl::dot_impl select_dot_impl() {
+cpp14_constexpr etl::dot_impl select_default_dot_impl() {
     if(all_dma<A, B>::value){
         if (is_cblas_enabled) {
             return etl::dot_impl::BLAS;
@@ -37,6 +40,36 @@ cpp14_constexpr etl::dot_impl select_dot_impl() {
     }
 
     return etl::dot_impl::STD;
+}
+
+/*!
+ * \brief Select the dot implementation for an expression of type A and B
+ * \tparam A The type of lhs expression
+ * \tparam B The type of rhs expression
+ * \return The implementation to use
+ */
+template <typename A, typename B>
+cpp14_constexpr etl::dot_impl select_dot_impl() {
+    if(local_context().dot_selector.forced){
+        auto forced = local_context().dot_selector.impl;
+
+        switch (forced) {
+            //AVX cannot always be used
+            case dot_impl::BLAS:
+                if(!is_cblas_enabled || !all_dma<A, B>::value){
+                    std::cerr << "Forced selection to BLAS dot implementation, but not possible for this expression" << std::endl;
+                    return select_default_dot_impl<A, B>();
+                }
+
+                return forced;
+
+            //In other cases, simply use the forced impl
+            default:
+                return forced;
+        }
+    }
+
+    return select_default_dot_impl<A, B>();
 }
 
 /*!
