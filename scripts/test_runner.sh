2dfdcfe4cf28968d3bd9738d6388282e4a5cfeea
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-05-13 16:03:43 +0200

    New options for testing: lcov
    The goal was to be able to support line exclusions with lcov
    Unfortunately, this is way too slow to be used and does not seem
    to improve the result in any way. Indeed, combining the coverage
    files is slower than the complete test suite :(

--- scripts/test_runner.sh
+++ scripts/test_runner.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+function etl_run {
+    make clean
+    make $ETL_THREADS debug/bin/etl_test
+    ./debug/bin/etl_test
+
+if [ "$ETL_LCOV" == "" ]
+then
+    gcovr -x -b -r . --object-directory=debug/test > coverage_${1}.xml
+else
+    lcov --directory debug --rc lcov_branch_coverage=1 --capture --output-file coverage_${1}.dat
+    lcov_cobertura.py -b debug -o coverage_${1}.xml coverage_${1}.dat
+    sed -i 's/filename="..\//filename="/' coverage_${1}.xml
+fi
+}
+
 # Disable default options
 export ETL_NO_DEFAULT=true
 unset ETL_DEFAULTS
@@ -17,58 +32,40 @@ echo "Test 1. GCC (debug default)"
 
 export ETL_DEFAULTS="-DCPP_UTILS_ASSERT_EXCEPTION"
 
-make clean
-make $ETL_THREADS debug/bin/etl_test
-./debug/bin/etl_test
-gcovr -x -b -r . --object-directory=debug/test > coverage_1.xml
+etl_run 1
 
 echo "Test 2. GCC (debug vectorize avx)"
 
 export ETL_DEFAULTS="-DETL_VECTORIZE_FULL -mavx"
 
-make clean
-make $ETL_THREADS debug/bin/etl_test
-./debug/bin/etl_test
-gcovr -x -b -r . --object-directory=debug/test > coverage_2.xml
+etl_run 2
 
 echo "Test 3. GCC (debug vectorize sse)"
 
 export ETL_DEFAULTS="-DETL_VECTORIZE_FULL -msse3 -msse4"
 
-make clean
-make $ETL_THREADS debug/bin/etl_test
-./debug/bin/etl_test
-gcovr -x -b -r . --object-directory=debug/test > coverage_3.xml
+etl_run 3
 
 echo "Test 4. GCC (debug mkl)"
 
 unset ETL_DEFAULTS
 export ETL_MKL=true
 
-make clean
-make $ETL_THREADS debug/bin/etl_test
-./debug/bin/etl_test
-gcovr -x -b -r . --object-directory=debug/test > coverage_4.xml
+etl_run 4
 
 echo "Test 5. GCC (debug parallel)"
 
 unset ETL_MKL
 export ETL_DEFAULTS="-DETL_PARALLEL"
 
-make clean
-make $ETL_THREADS debug/bin/etl_test
-./debug/bin/etl_test
-gcovr -x -b -r . --object-directory=debug/test > coverage_5.xml
+etl_run 5
 
 echo "Test 6. GCC (debug vectorize sse avx parallel)"
 
 unset ETL_MKL
 export ETL_DEFAULTS="-DETL_PARALLEL -DETL_VECTORIZE_FULL -msse3 -msse4 -mavx"
 
-make clean
-make $ETL_THREADS debug/bin/etl_test
-./debug/bin/etl_test
-gcovr -x -b -r . --object-directory=debug/test > coverage_6.xml
+etl_run 6
 
 if [ "$ETL_NO_GPU" == "" ]
 then
@@ -79,16 +76,27 @@ then
     export ETL_CUBLAS=true
     export ETL_CUFFT=true
 
-    make clean
-    make $ETL_THREADS debug/bin/etl_test
-    ./debug/bin/etl_test
-    gcovr -x -b -r . --object-directory=debug/test > coverage_7.xml
+    etl_run 7
 
     echo "Merge the coverage reports"
 
-    merge-xml-coverage.py -o coverage_report.xml coverage_1.xml coverage_2.xml coverage_3.xml coverage_4.xml coverage_5.xml coverage_6.xml coverage_7.xml
+    if [ "$ETL_LCOV_MERGE" == "" ]
+    then
+        merge-xml-coverage.py -o coverage_report.xml coverage_1.xml coverage_2.xml coverage_3.xml coverage_4.xml coverage_5.xml coverage_6.xml coverage_7.xml
+    else
+        lcov --rc lcov_branch_coverage=1 -a coverage_1.dat -a coverage_2.dat -a coverage_3.dat -a coverage_4.dat -a coverage_5.dat -a coverage_6.dat -a coverage_7.dat -o coverage_full.dat
+        lcov_cobertura.py -b debug -o coverage_report.xml coverage_full.dat
+        sed -i 's/filename="..\//filename="/' coverage_report.xml
+    fi
 else
     echo "Merge the coverage reports"
 
-    merge-xml-coverage.py -o coverage_report.xml coverage_1.xml coverage_2.xml coverage_3.xml coverage_4.xml coverage_5.xml coverage_6.xml
+    if [ "$ETL_LCOV_MERGE" == "" ]
+    then
+        merge-xml-coverage.py -o coverage_report.xml coverage_1.xml coverage_2.xml coverage_3.xml coverage_4.xml coverage_5.xml coverage_6.xml
+    else
+        lcov --rc lcov_branch_coverage=1 -a coverage_1.dat -a coverage_2.dat -a coverage_3.dat -a coverage_4.dat -a coverage_5.dat -a coverage_6.dat -o coverage_full.dat
+        lcov_cobertura.py -b debug -o coverage_report.xml coverage_full.dat
+        sed -i 's/filename="..\//filename="/' coverage_report.xml
+    fi
 fi
