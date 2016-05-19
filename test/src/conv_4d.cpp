42e8650f83793255c65a90c1422cfd16df285f7d
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-05-19 16:47:11 +0200

    Tests for conv_4d_valid

---
+++ test/src/conv_4d.cpp
@@ -0,0 +1,32 @@
+//=======================================================================
+// Copyright (c) 2014-2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include "test.hpp"
+#include "conv_test.hpp"
+
+// conv_4d_valid
+
+CONV4_VALID_TEST_CASE("conv_4d/valid_1", "[conv][conv4][valid]") {
+    etl::fast_matrix<T, 10, 2, 5, 5> I(etl::sequence_generator(10.0) * 4.0);
+    etl::fast_matrix<T, 12, 2, 3, 3> K(etl::sequence_generator(2.0) * 0.3);
+
+    etl::fast_matrix<T, 10, 12, 3, 3> ref;
+    etl::fast_matrix<T, 10, 12, 3, 3> c;
+
+    ref = 0.0;
+    for(std::size_t i = 0; i < etl::dim<0>(I); ++i){
+        for(std::size_t c = 0; c < etl::dim<1>(K); ++c){
+            for(std::size_t k = 0; k < etl::dim<0>(K); ++k){
+                ref(i)(k) += conv_2d_valid(I(i)(c), K(k)(c));
+            }
+        }
+    }
+
+    Impl::apply(I, K, c);
+
+    REQUIRE(ref == c);
+}
