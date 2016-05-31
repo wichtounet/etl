f0c64dbf10bd074e3667d73584ecc0402b249252
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-05-31 16:26:44 +0200

    Reduce even more test case overhead

--- test/include/fast_catch.hpp
+++ test/include/fast_catch.hpp
@@ -7,42 +7,45 @@
 
 constexpr const auto base_eps = std::numeric_limits<float>::epsilon()*100;
 
-inline void evaluate_result_direct(Catch::ResultBuilder&& __result, bool value){
-    __result.setResultType(value);
-    __result.setLhs(value ? "true" : "false");
-    __result.setOp("");
-    __result.endExpression();
-    __result.react();
+inline void evaluate_result_direct(const char* file, std::size_t line, const char* exp, bool value){
+    Catch::ResultBuilder result("REQUIRE", {file, line}, exp, Catch::ResultDisposition::Flags::Normal);
+    result.setResultType(value);
+    result.setLhs(value ? "true" : "false");
+    result.setOp("");
+    result.endExpression();
+    result.react();
 }
 
 template<typename L, typename R>
-void evaluate_result(Catch::ResultBuilder&& __result, L lhs, R rhs){
-    __result.setResultType(lhs == rhs);
-    __result.setLhs(Catch::toString(lhs));
-    __result.setRhs(Catch::toString(rhs));
-    __result.setOp("==");
-    __result.endExpression();
-    __result.react();
+void evaluate_result(const char* file, std::size_t line, const char* exp, L lhs, R rhs){
+    Catch::ResultBuilder result("REQUIRE", {file, line}, exp, Catch::ResultDisposition::Flags::Normal);
+    result.setResultType(lhs == rhs);
+    result.setLhs(Catch::toString(lhs));
+    result.setRhs(Catch::toString(rhs));
+    result.setOp("==");
+    result.endExpression();
+    result.react();
 }
 
 template<typename L, typename R>
-void evaluate_result_approx(Catch::ResultBuilder&& __result, L lhs, R rhs, double eps = base_eps){
-    __result.setResultType(fabs(lhs - rhs) < eps * (1.0 + std::max(fabs(lhs), fabs(rhs))));
-    __result.setLhs(Catch::toString(lhs));
-    __result.setRhs("Approx(" + Catch::toString(rhs) + ")");
-    __result.setOp("==");
-    __result.endExpression();
-    __result.react();
+void evaluate_result_approx(const char* file, std::size_t line, const char* exp, L lhs, R rhs, double eps = base_eps){
+    Catch::ResultBuilder result("REQUIRE", {file, line}, exp, Catch::ResultDisposition::Flags::Normal);
+    result.setResultType(fabs(lhs - rhs) < eps * (1.0 + std::max(fabs(lhs), fabs(rhs))));
+    result.setLhs(Catch::toString(lhs));
+    result.setRhs("Approx(" + Catch::toString(rhs) + ")");
+    result.setOp("==");
+    result.endExpression();
+    result.react();
 }
 
 #define REQUIRE_DIRECT(value) \
-    evaluate_result_direct(Catch::ResultBuilder( "REQUIRE", CATCH_INTERNAL_LINEINFO, #value, Catch::ResultDisposition::Normal ), value);
+    evaluate_result_direct(__FILE__, __LINE__, #value, value);
 
 #define REQUIRE_EQUALS(lhs, rhs) \
-    evaluate_result(Catch::ResultBuilder( "REQUIRE", CATCH_INTERNAL_LINEINFO, #lhs " == " #rhs, Catch::ResultDisposition::Normal ), lhs, rhs);
+    evaluate_result(__FILE__, __LINE__, #lhs " == " #rhs, lhs, rhs);
 
 #define REQUIRE_EQUALS_APPROX(lhs, rhs) \
-    evaluate_result_approx(Catch::ResultBuilder( "REQUIRE", CATCH_INTERNAL_LINEINFO, #lhs " == " #rhs, Catch::ResultDisposition::Normal ), lhs, rhs);
+    evaluate_result_approx(__FILE__, __LINE__, #lhs " == " #rhs, lhs, rhs);
 
 #define REQUIRE_EQUALS_APPROX_E(lhs, rhs, eps) \
-    evaluate_result_approx(Catch::ResultBuilder( "REQUIRE", CATCH_INTERNAL_LINEINFO, #lhs " == " #rhs, Catch::ResultDisposition::Normal ), lhs, rhs, eps);
+    evaluate_result_approx(__FILE__, __LINE__, #lhs " == " #rhs, lhs, rhs, eps);
