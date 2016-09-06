4ff0d159cc24c4d2899352529beff4637f1889ab
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-06 19:10:40 +0200

    Tester for deep pooling

--- test/src/pooling_deep.cpp
+++ test/src/pooling_deep.cpp
@@ -29,3 +29,34 @@ TEMPLATE_TEST_CASE_2("pooling/deep/max2/1", "[pooling]", Z, float, double) {
     REQUIRE_EQUALS(b(1, 1, 0), 14.0);
     REQUIRE_EQUALS(b(1, 1, 1), 16.0);
 }
+
+TEMPLATE_TEST_CASE_2("pooling/deep/max2/2", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 4, 4> aa({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<Z, 2, 2, 4, 4> a;
+    a(0)(0) = aa;
+    a(0)(1) = aa;
+    a(1)(0) = aa;
+    a(1)(1) = aa;
+
+    etl::fast_matrix<Z, 2, 2, 2, 2> b(etl::max_pool_2d<2, 2>(a));
+
+    REQUIRE_EQUALS(b(0, 0, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(0, 0, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(0, 0, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(0, 0, 1, 1), 16.0);
+
+    REQUIRE_EQUALS(b(0, 1, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(0, 1, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(0, 1, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(0, 1, 1, 1), 16.0);
+
+    REQUIRE_EQUALS(b(1, 0, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(1, 0, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(1, 0, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(1, 0, 1, 1), 16.0);
+
+    REQUIRE_EQUALS(b(1, 1, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(1, 1, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(1, 1, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(1, 1, 1, 1), 16.0);
+}
