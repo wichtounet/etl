e60b4fc704cbf5127c2dc6ed565ea7801ebf9237
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-06 16:20:08 +0200

    Add todo note

--- test/src/pooling_stride.cpp
+++ test/src/pooling_stride.cpp
@@ -9,6 +9,8 @@
 
 #include <vector>
 
+//TODO The 3D tests are really poor
+
 TEMPLATE_TEST_CASE_2("pooling/stride/max2/1", "[pooling]", Z, float, double) {
     etl::fast_matrix<Z, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
     etl::fast_matrix<Z, 3, 3> b(etl::max_pool_2d<2, 2, 1, 1>(a));
