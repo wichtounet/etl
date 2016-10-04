758ad035798fc8914e910c71fa359f8b4c862f73
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-10-04 16:38:03 +0200

    Prepare the implementation of BLAS conv4

--- include/etl/conv_impl.hpp
+++ include/etl/conv_impl.hpp
@@ -31,13 +31,14 @@ enum class conv_impl {
  * \brief Enumeration describing the different convolution implementations
  */
 enum class conv4_impl {
-    STD,      ///< Standard implementation
-    SSE,      ///< SSE implementation
-    AVX,      ///< AVX implementation
-    CUDNN,    ///< CUDNN implementation
-    FFT_STD,  ///< FFT reduction (with STD impl)
-    FFT_MKL,  ///< FFT reduction (with MKL impl)
-    FFT_CUFFT ///< FFT reduction (with CUFFT impl)
+    STD,       ///< Standard implementation
+    SSE,       ///< SSE implementation
+    AVX,       ///< AVX implementation
+    CUDNN,     ///< CUDNN implementation
+    FFT_STD,   ///< FFT reduction (with STD impl)
+    FFT_MKL,   ///< FFT reduction (with MKL impl)
+    FFT_CUFFT, ///< FFT reduction (with CUFFT impl)
+    BLAS       ///< BLAS reduction
 };
 
 /*!
