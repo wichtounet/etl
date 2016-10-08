03a9fea52ca030d4ae9245b0870a14deafdd7be5
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-10-08 22:57:29 +0200

    ETL_STRONG_INLINE macro

--- include/etl/vectorization.hpp
+++ include/etl/vectorization.hpp
@@ -18,11 +18,13 @@
 
 #ifdef __clang__
 #define ETL_INLINE(RRRR) inline RRRR __attribute__((__always_inline__, __nodebug__))
+#define ETL_STRONG_INLINE(RRRR) inline RRRR __attribute__((__always_inline__))
 #define ETL_STATIC_INLINE(RRRR) static ETL_INLINE(RRRR)
 #define ETL_TMP_INLINE(RRRR)    static inline RRRR __attribute__((__always_inline__, __nodebug__))
 #define ETL_OUT_INLINE(RRRR)    inline RRRR __attribute__((__always_inline__, __nodebug__))
 #else
 #define ETL_INLINE(RRRR) inline RRRR __attribute__((__always_inline__, __gnu_inline__, __artificial__))
+#define ETL_STRONG_INLINE(RRRR) inline RRRR __attribute__((__always_inline__, __gnu_inline__))
 #define ETL_STATIC_INLINE(RRRR) static ETL_INLINE(RRRR)
 #define ETL_TMP_INLINE(RRRR) static inline RRRR __attribute__((__always_inline__, __artificial__))
 #define ETL_OUT_INLINE(RRRR)    inline RRRR __attribute__((__always_inline__, __artificial__))
