96666ec89a88255e3c9f6b996da02417182ba7de
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-10-11 20:41:29 +0200

    Rename the detail namespace

--- include/etl/impl/blas/fft.hpp
+++ include/etl/impl/blas/fft.hpp
@@ -19,7 +19,7 @@ namespace blas {
 
 #ifdef ETL_MKL_MODE
 
-namespace detail {
+namespace mkl_detail {
 
 inline void fft_kernel(const std::complex<float>* in, std::size_t s, std::complex<float>* out) {
     DFTI_DESCRIPTOR_HANDLE descriptor;
@@ -412,14 +412,14 @@ void conv2_full_kernel(const T* a, std::size_t m1, std::size_t m2, const T* b, s
     }
 }
 
-} //End of namespace detail
+} //End of namespace mkl_detail
 
 inline void fft1(const opaque_memory<float,1>& a, const opaque_memory<std::complex<float>,1>& c) {
     auto a_complex = allocate<std::complex<float>>(a.size());
 
     direct_copy(a.memory_start(), a.memory_end(), a_complex.get());
 
-    detail::fft_kernel(a_complex.get(), a.size(), c.memory_start());
+    mkl_detail::fft_kernel(a_complex.get(), a.size(), c.memory_start());
 }
 
 inline void fft1(const opaque_memory<double,1>& a, const opaque_memory<std::complex<double>,1>& c) {
@@ -427,24 +427,24 @@ inline void fft1(const opaque_memory<double,1>& a, const opaque_memory<std::comp
 
     direct_copy(a.memory_start(), a.memory_end(), a_complex.get());
 
-    detail::fft_kernel(a_complex.get(), a.size(), c.memory_start());
+    mkl_detail::fft_kernel(a_complex.get(), a.size(), c.memory_start());
 }
 
 template <typename T>
 void fft1(const opaque_memory<std::complex<T>,1>& a, const opaque_memory<std::complex<T>,1>& c) {
-    detail::fft_kernel(a.memory_start(), a.size(), c.memory_start());
+    mkl_detail::fft_kernel(a.memory_start(), a.size(), c.memory_start());
 }
 
 template <typename A, typename C>
 void ifft1(A&& a, C&& c) {
-    detail::ifft_kernel(a.memory_start(), etl::size(a), c.memory_start());
+    mkl_detail::ifft_kernel(a.memory_start(), etl::size(a), c.memory_start());
 }
 
 template <typename A, typename C, cpp_enable_if(all_complex_single_precision<A>::value)>
 void ifft1_real(A&& a, C&& c) {
     auto c_complex = allocate<std::complex<float>>(etl::size(a));
 
-    detail::ifft_kernel(a.memory_start(), etl::size(a), c_complex.get());
+    mkl_detail::ifft_kernel(a.memory_start(), etl::size(a), c_complex.get());
 
     for (std::size_t i = 0; i < etl::size(a); ++i) {
         c[i] = c_complex[i].real();
@@ -455,7 +455,7 @@ template <typename A, typename C, cpp_enable_if(all_complex_double_precision<A>:
 void ifft1_real(A&& a, C&& c) {
     auto c_complex = allocate<std::complex<double>>(etl::size(a));
 
-    detail::ifft_kernel(a.memory_start(), etl::size(a), c_complex.get());
+    mkl_detail::ifft_kernel(a.memory_start(), etl::size(a), c_complex.get());
 
     for (std::size_t i = 0; i < etl::size(a); ++i) {
         c[i] = c_complex[i].real();
@@ -471,7 +471,7 @@ void fft1_many(const opaque_memory<T, N>& a, const opaque_memory<std::complex<T>
 
     direct_copy(a.memory_start(), a.memory_end(), a_complex.get());
 
-    detail::fft_many_kernel(a_complex.get(), batch, n, c.memory_start());
+    mkl_detail::fft_many_kernel(a_complex.get(), batch, n, c.memory_start());
 }
 
 template<std::size_t N, typename T>
@@ -479,7 +479,7 @@ void fft1_many(const opaque_memory<std::complex<T>, N>& a, const opaque_memory<s
     std::size_t n     = a.template dim<N - 1>(); //Size of the transform
     std::size_t batch = a.size() / n;            //Number of batch
 
-    detail::fft_many_kernel(a.memory_start(), batch, n, c.memory_start());
+    mkl_detail::fft_many_kernel(a.memory_start(), batch, n, c.memory_start());
 }
 
 template <typename A, typename C>
@@ -489,7 +489,7 @@ void ifft1_many(A&& a, C&& c) {
     std::size_t n     = etl::dim<N - 1>(a); //Size of the transform
     std::size_t batch = etl::size(a) / n;   //Number of batch
 
-    detail::ifft_many_kernel(a.memory_start(), batch, n, c.memory_start());
+    mkl_detail::ifft_many_kernel(a.memory_start(), batch, n, c.memory_start());
 }
 
 template <typename A, typename B, typename C>
@@ -507,12 +507,12 @@ void conv1_full(A&& a, B&& b, C&& c) {
     direct_copy(a.memory_start(), a.memory_end(), a_padded.memory_start());
     direct_copy(b.memory_start(), b.memory_end(), b_padded.memory_start());
 
-    detail::inplace_fft_kernel(reinterpret_cast<std::complex<type>*>(a_padded.memory_start()), size);
-    detail::inplace_fft_kernel(reinterpret_cast<std::complex<type>*>(b_padded.memory_start()), size);
+    mkl_detail::inplace_fft_kernel(reinterpret_cast<std::complex<type>*>(a_padded.memory_start()), size);
+    mkl_detail::inplace_fft_kernel(reinterpret_cast<std::complex<type>*>(b_padded.memory_start()), size);
 
     a_padded *= b_padded;
 
-    detail::inplace_ifft_kernel(reinterpret_cast<std::complex<type>*>(a_padded.memory_start()), size);
+    mkl_detail::inplace_ifft_kernel(reinterpret_cast<std::complex<type>*>(a_padded.memory_start()), size);
 
     for (std::size_t i = 0; i < size; ++i) {
         c[i] = a_padded[i].real;
@@ -525,7 +525,7 @@ void fft2(A&& a, C&& c) {
 
     direct_copy(a.memory_start(), a.memory_end(), a_complex.get());
 
-    detail::fft2_kernel(a_complex.get(), etl::dim<0>(a), etl::dim<1>(a), c.memory_start());
+    mkl_detail::fft2_kernel(a_complex.get(), etl::dim<0>(a), etl::dim<1>(a), c.memory_start());
 }
 
 template <typename A, typename C, cpp_enable_if(all_double_precision<A>::value)>
@@ -534,12 +534,12 @@ void fft2(A&& a, C&& c) {
 
     direct_copy(a.memory_start(), a.memory_end(), a_complex.get());
 
-    detail::fft2_kernel(a_complex.get(), etl::dim<0>(a), etl::dim<1>(a), c.memory_start());
+    mkl_detail::fft2_kernel(a_complex.get(), etl::dim<0>(a), etl::dim<1>(a), c.memory_start());
 }
 
 template <typename A, typename C, cpp_enable_if(all_complex<A>::value)>
 void fft2(A&& a, C&& c) {
-    detail::fft2_kernel(a.memory_start(), etl::dim<0>(a), etl::dim<1>(a), c.memory_start());
+    mkl_detail::fft2_kernel(a.memory_start(), etl::dim<0>(a), etl::dim<1>(a), c.memory_start());
 }
 
 template <typename A, typename C, cpp_enable_if(all_single_precision<A>::value)>
@@ -554,7 +554,7 @@ void fft2_many(A&& a, C&& c) {
 
     direct_copy(a.memory_start(), a.memory_end(), a_complex.get());
 
-    detail::fft2_many_kernel(a_complex.get(), batch, n1, n2, c.memory_start());
+    mkl_detail::fft2_many_kernel(a_complex.get(), batch, n1, n2, c.memory_start());
 }
 
 template <typename A, typename C, cpp_enable_if(all_double_precision<A>::value)>
@@ -569,7 +569,7 @@ void fft2_many(A&& a, C&& c) {
 
     direct_copy(a.memory_start(), a.memory_end(), a_complex.get());
 
-    detail::fft2_many_kernel(a_complex.get(), batch, n1, n2, c.memory_start());
+    mkl_detail::fft2_many_kernel(a_complex.get(), batch, n1, n2, c.memory_start());
 }
 
 template <typename A, typename C, cpp_enable_if(all_complex<A>::value)>
@@ -580,7 +580,7 @@ void fft2_many(A&& a, C&& c) {
     std::size_t n2    = etl::dim<N - 1>(a);       //Size of the transform
     std::size_t batch = etl::size(a) / (n1 * n2); //Number of batch
 
-    detail::fft2_many_kernel(a.memory_start(), batch, n1, n2, c.memory_start());
+    mkl_detail::fft2_many_kernel(a.memory_start(), batch, n1, n2, c.memory_start());
 }
 
 template <typename A, typename C>
@@ -591,19 +591,19 @@ void ifft2_many(A&& a, C&& c) {
     std::size_t n2    = etl::dim<N - 1>(a);       //Size of the transform
     std::size_t batch = etl::size(a) / (n1 * n2); //Number of batch
 
-    detail::ifft2_many_kernel(a.memory_start(), batch, n1, n2, c.memory_start());
+    mkl_detail::ifft2_many_kernel(a.memory_start(), batch, n1, n2, c.memory_start());
 }
 
 template <typename A, typename C, cpp_enable_if(all_complex<A>::value)>
 void ifft2(A&& a, C&& c) {
-    detail::ifft2_kernel(a.memory_start(), etl::dim<0>(a), etl::dim<1>(a), c.memory_start());
+    mkl_detail::ifft2_kernel(a.memory_start(), etl::dim<0>(a), etl::dim<1>(a), c.memory_start());
 }
 
 template <typename A, typename C>
 void ifft2_real(A&& a, C&& c) {
     auto c_complex = allocate<std::complex<value_t<C>>>(etl::size(a));
 
-    detail::ifft2_kernel(a.memory_start(), etl::dim<0>(a), etl::dim<1>(a), c_complex.get());
+    mkl_detail::ifft2_kernel(a.memory_start(), etl::dim<0>(a), etl::dim<1>(a), c_complex.get());
 
     for (std::size_t i = 0; i < etl::size(a); ++i) {
         c[i] = c_complex[i].real();
@@ -612,7 +612,7 @@ void ifft2_real(A&& a, C&& c) {
 
 template <typename T>
 void conv2_full(const opaque_memory<T, 2>& a, const opaque_memory<T, 2>& b, const opaque_memory<T, 2>& c) {
-    detail::conv2_full_kernel(a.memory_start(), a.dim(0), a.dim(1), b.memory_start(), b.dim(0), b.dim(1), c.memory_start(), T(0.0));
+    mkl_detail::conv2_full_kernel(a.memory_start(), a.dim(0), a.dim(1), b.memory_start(), b.dim(0), b.dim(1), c.memory_start(), T(0.0));
 }
 
 template <typename T>
@@ -623,7 +623,7 @@ void conv2_full_flipped(const opaque_memory<T, 2>& a, const opaque_memory<T, 2>&
 
     prepared_b.fflip_inplace();
 
-    detail::conv2_full_kernel(a.memory_start(), a.dim(0), a.dim(1), prepared_b.memory_start(), b.dim(0), b.dim(1), c.memory_start(), T(0.0));
+    mkl_detail::conv2_full_kernel(a.memory_start(), a.dim(0), a.dim(1), prepared_b.memory_start(), b.dim(0), b.dim(1), c.memory_start(), T(0.0));
 }
 
 template <typename T>
@@ -641,7 +641,7 @@ void conv4_full(const opaque_memory<T, 4>& input, const opaque_memory<T, 4>& ker
         for (std::size_t i = 0; i < input.dim(0); ++i) {
             //k = 0
             for (std::size_t c = 0; c < kernel.dim(1); ++c) {
-                detail::conv2_full_kernel(
+                mkl_detail::conv2_full_kernel(
                     input.memory_start() + i * input_i_inc + 0 * input_k_inc, input.dim(2), input.dim(3),
                     kernel.memory_start() + 0 * kernel_k_inc + c * kernel_c_inc, kernel.dim(2), kernel.dim(3),
                     conv.memory_start() + i * conv_i_inc + c * conv_c_inc, T(0.0));
@@ -649,7 +649,7 @@ void conv4_full(const opaque_memory<T, 4>& input, const opaque_memory<T, 4>& ker
 
             for (std::size_t k = 1; k < kernel.dim(0); ++k) {
                 for (std::size_t c = 0; c < kernel.dim(1); ++c) {
-                    detail::conv2_full_kernel(
+                    mkl_detail::conv2_full_kernel(
                         input.memory_start() + i * input_i_inc + k * input_k_inc, input.dim(2), input.dim(3),
                         kernel.memory_start() + k * kernel_k_inc + c * kernel_c_inc, kernel.dim(2), kernel.dim(3),
                         conv.memory_start() + i * conv_i_inc + c * conv_c_inc, T(1.0));
