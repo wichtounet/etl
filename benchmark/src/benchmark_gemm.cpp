e518b75d774d971aeb43aeaf8eb7dfedf3cddbf7
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-10-20 13:20:31 +0200

    Benchmark for outer product

--- benchmark/src/benchmark_gemm.cpp
+++ benchmark/src/benchmark_gemm.cpp
@@ -213,3 +213,11 @@ CPM_BENCH(){
         , [](std::size_t d1, std::size_t d2){ return 2 * d1 * d2 + 2 * d1 * d2; }
         );
 }
+
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("r = a *o b (s)", outer_policy,
+    FLOPS([](std::size_t d1, std::size_t d2){ return d1 * d2; }),
+    CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(svec(d1), svec(d2), smat(d1, d2)); }),
+    CPM_SECTION_FUNCTOR("default", [](svec& a, svec& b, smat& c){ c = etl::outer(a, b); }),
+    CPM_SECTION_FUNCTOR("std", [](svec& a, svec& b, smat& c){ c = selected_helper(etl::outer_impl::STD, etl::outer(a, b)); })
+    BLAS_SECTION_FUNCTOR("blas", [](svec& a, svec& b, smat& c){ c = selected_helper(etl::outer_impl::BLAS, etl::outer(a, b)); })
+)
