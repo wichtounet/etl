36244e5528f9bb100ae6942a71c5d5115704fd9c
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-10-24 12:07:50 +0200

    Add unroll

--- include/etl/impl/vec/dot.hpp
+++ include/etl/impl/vec/dot.hpp
@@ -30,6 +30,35 @@ value_t<L> selected_dot(const L& lhs, const R& rhs) {
     size_t i = 0;
 
     auto r1 = vec_type::template zero<T>();
+    auto r2 = vec_type::template zero<T>();
+    auto r3 = vec_type::template zero<T>();
+    auto r4 = vec_type::template zero<T>();
+
+    if (n < 1000000) {
+        for(; i + (vec_size * 4) - 1 < n; i += 4 * vec_size){
+            auto a1 = lhs.load(i + 0 * vec_size);
+            auto a2 = lhs.load(i + 1 * vec_size);
+            auto a3 = lhs.load(i + 2 * vec_size);
+            auto a4 = lhs.load(i + 3 * vec_size);
+
+            auto b1 = rhs.load(i + 0 * vec_size);
+            auto b2 = rhs.load(i + 1 * vec_size);
+            auto b3 = rhs.load(i + 2 * vec_size);
+            auto b4 = rhs.load(i + 3 * vec_size);
+
+            auto t1 = vec_type::template mul<false>(a1, b1);
+            r1 = vec_type::add(r1, t1);
+
+            auto t2 = vec_type::template mul<false>(a2, b2);
+            r2 = vec_type::add(r2, t2);
+
+            auto t3 = vec_type::template mul<false>(a3, b3);
+            r3 = vec_type::add(r3, t3);
+
+            auto t4 = vec_type::template mul<false>(a4, b4);
+            r4 = vec_type::add(r4, t4);
+        }
+    }
 
     for(; i + vec_size - 1 < n; i += vec_size){
         auto a1 = lhs.load(i);
@@ -39,7 +68,7 @@ value_t<L> selected_dot(const L& lhs, const R& rhs) {
         r1 = vec_type::add(r1, t1);
     }
 
-    auto product = vec_type::hadd(r1);
+    auto product = vec_type::hadd(r1) + vec_type::hadd(r2) + vec_type::hadd(r3) + vec_type::hadd(r4);
 
     for(; i < n; ++i){
         product += lhs[i] * rhs[i];
