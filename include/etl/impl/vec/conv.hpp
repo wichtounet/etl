f914c47c106a6289fb845d1b8cfadf080a381b22
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-10-26 17:44:54 +0200

    Review stride

--- include/etl/impl/vec/conv.hpp
+++ include/etl/impl/vec/conv.hpp
@@ -26,7 +26,13 @@ void conv2_valid_flipped(const I& input, const K& kernel, C&& conv, size_t s1, s
     const auto n1 = etl::dim<0>(input);
     const auto n2 = etl::dim<1>(input);
 
-    if(p1 || p2){
+    const auto k1 = etl::dim<0>(kernel);
+    const auto k2 = etl::dim<1>(kernel);
+
+    const auto c1 = etl::dim<0>(conv);
+    const auto c2 = etl::dim<1>(conv);
+
+    if(cpp_unlikely(p1 || p2)){
         const auto o1 = n1 + 2 * p1;
         const auto o2 = n2 + 2 * p2;
 
@@ -43,15 +49,24 @@ void conv2_valid_flipped(const I& input, const K& kernel, C&& conv, size_t s1, s
         return;
     }
 
-    const auto k1 = etl::dim<0>(kernel);
-    const auto k2 = etl::dim<1>(kernel);
+    if(cpp_unlikely(s1 > 1 || s2 > 1)){
+        etl::dyn_matrix<T> tmp_result(n1 - k1 + 1, n2 - k2 + 1);
 
-    const auto c1 = etl::dim<0>(conv);
-    const auto c2 = etl::dim<1>(conv);
+        conv2_valid_flipped(input, kernel, tmp_result, 1, 1, 0, 0);
+
+        // Strided copy of the large result into the small result
+        for (std::size_t i = 0; i < c1; ++i) {
+            for (std::size_t j = 0; j < c2; ++j) {
+                conv(i, j) = tmp_result(i * s1, j * s2);
+            }
+        }
+
+        return;
+    }
 
     const auto R = std::min(k1, c1); // Max number of kernels per line of input
 
-    conv = 0;
+    conv = T(0);
 
     // Primary steps
     for(size_t i = 0; i < k1 - 1; ++i){
@@ -64,7 +79,7 @@ void conv2_valid_flipped(const I& input, const K& kernel, C&& conv, size_t s1, s
                 T value = 0;
 
                 for(size_t k = 0; k < k2; ++k){
-                    value += input(i * s1, j * s1 + k) * kernel(k_i, k);
+                    value += input(i, j + k) * kernel(k_i, k);
                 }
 
                 conv(m,j) += value;
@@ -83,7 +98,7 @@ void conv2_valid_flipped(const I& input, const K& kernel, C&& conv, size_t s1, s
                 T value = 0;
 
                 for(size_t k = 0; k < k2; ++k){
-                    value += input(i * s1, j * s1 + k) * kernel(m, k);
+                    value += input(i, j + k) * kernel(m, k);
                 }
 
                 conv(c_i, j) += value;
@@ -103,7 +118,7 @@ void conv2_valid_flipped(const I& input, const K& kernel, C&& conv, size_t s1, s
                 T value = 0;
 
                 for(size_t k = 0; k < k2; ++k){
-                    value += input(i * s1, j * s1 + k) * kernel(k_i, k);
+                    value += input(i, j + k) * kernel(k_i, k);
                 }
 
                 conv(c_i, j) += value;
