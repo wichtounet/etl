6682f1089d6c838567528b8f28aa732e53a87278
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-10-28 17:08:48 +0200

    More realistic machine-learning benchmark

--- benchmark/include/benchmark.hpp
+++ benchmark/include/benchmark.hpp
@@ -95,6 +95,11 @@ using large_vector_policy = VALUES_POLICY(10, 100, 1000, 10000, 1000000, 1000000
 using conv_1d_large_policy = NARY_POLICY(VALUES_POLICY(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000), VALUES_POLICY(500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000));
 using conv_2d_large_policy = NARY_POLICY(VALUES_POLICY(100, 105, 110, 115, 120, 125, 130, 135, 140), VALUES_POLICY(50, 50, 55, 55, 60, 60, 65, 65, 70));
 
+// Use some common kernels in machine learning
+using conv_2d_real_policy = NARY_POLICY(
+    VALUES_POLICY(28, 28, 28, 28, 32, 32, 32, 32, 227, 227, 227, 227),
+    VALUES_POLICY(3, 5, 7, 9, 3, 5, 7, 9, 3, 5, 11, 13));
+
 using conv_2d_small_policy = NARY_POLICY(
     VALUES_POLICY(10, 10, 12, 12, 16, 16, 20, 20, 28, 28, 28, 32, 100),
     VALUES_POLICY(5,  9,  5,  9,  5,  9,  5,  9,  5,  9,  17, 16, 24));
