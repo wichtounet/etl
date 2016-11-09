7670547d20243f5c27eb2a407f13b492ca031142
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-11-09 20:22:33 +0100

    First vectorized version of gevm
    Not very good for now

--- include/etl/impl/vec/gemm.hpp
+++ include/etl/impl/vec/gemm.hpp
@@ -104,11 +104,11 @@ void gemv(A&& a, B&& b, C&& c) {
 
 template <typename V, typename A, typename B, typename C, cpp_enable_if((all_row_major<A, B, C>::value))>
 void gevm(const A& a, const B& b, C& c) {
-    //using vec_type = V;
-    //using T        = value_t<A>;
+    using vec_type = V;
+    using T        = value_t<A>;
 
-    //static constexpr size_t vec_size = vec_type::template traits<T>::size;
-    //static constexpr bool Cx         = is_complex_t<T>::value;
+    static constexpr size_t vec_size = vec_type::template traits<T>::size;
+    static constexpr bool Cx         = is_complex_t<T>::value;
 
     const auto m = rows(b);
     const auto n = columns(b);
@@ -116,8 +116,46 @@ void gevm(const A& a, const B& b, C& c) {
     c = 0;
 
     for (size_t k = 0; k < m; k++) {
-        for (size_t j = 0; j < n; j++) {
-            c(j) += a(k) * b(k, j);
+        auto factor = a(k);
+
+        size_t j = 0;
+
+        auto f = vec_type::set(factor);
+
+        for (; j + (4 * vec_size) - 1 < n; j += 4 * vec_size) {
+            auto b1 = b.template loadu<vec_type>(k * n + j + 0 * vec_size);
+            auto b2 = b.template loadu<vec_type>(k * n + j + 1 * vec_size);
+            auto b3 = b.template loadu<vec_type>(k * n + j + 2 * vec_size);
+            auto b4 = b.template loadu<vec_type>(k * n + j + 3 * vec_size);
+
+            auto c1 = c.template loadu<vec_type>(j + 0 * vec_size);
+            auto c2 = c.template loadu<vec_type>(j + 1 * vec_size);
+            auto c3 = c.template loadu<vec_type>(j + 2 * vec_size);
+            auto c4 = c.template loadu<vec_type>(j + 3 * vec_size);
+
+            auto t1 = vec_type::template mul<Cx>(f, b1);
+            auto t2 = vec_type::template mul<Cx>(f, b2);
+            auto t3 = vec_type::template mul<Cx>(f, b3);
+            auto t4 = vec_type::template mul<Cx>(f, b4);
+
+            c.template storeu<vec_type>(vec_type::add(c1, t1), j + 0 * vec_size);
+            c.template storeu<vec_type>(vec_type::add(c2, t2), j + 1 * vec_size);
+            c.template storeu<vec_type>(vec_type::add(c3, t3), j + 2 * vec_size);
+            c.template storeu<vec_type>(vec_type::add(c4, t4), j + 3 * vec_size);
+        }
+
+        for (; j + vec_size - 1 < n; j += vec_size) {
+            auto b1 = b.template loadu<vec_type>(k * n + j);
+            auto c1 = c.template loadu<vec_type>(j);
+
+            auto t1 = vec_type::template mul<Cx>(f, b1);
+            c1 = vec_type::add(c1, t1);
+
+            c.template storeu<vec_type>(c1, j);
+        }
+
+        for (; j < n; j++) {
+            c[i] += factor * b(k, j);
         }
     }
 }
