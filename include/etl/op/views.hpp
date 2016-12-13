21e6e54de5340b90cce27685d8bc45d0fbc353b2
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-12-13 22:59:43 +0100

    Reduce the overhead of sub_view

--- include/etl/op/views.hpp
+++ include/etl/op/views.hpp
@@ -195,220 +195,6 @@ struct dim_view {
 };
 
 /*!
- * \brief View that shows a sub matrix of an expression
- * \tparam T The type of expression on which the view is made
- */
-template <typename T>
-struct sub_view {
-    T sub_expr;                 ///< The Sub expression
-    const size_t i;          ///< The index
-    const size_t sub_offset; ///< The sub size
-
-    using sub_type          = T;                                               ///< The sub type
-    using value_type        = value_t<sub_type>;                               ///< The value contained in the expression
-    using memory_type       = memory_t<sub_type>;                              ///< The memory acess type
-    using const_memory_type = const_memory_t<sub_type>;                        ///< The const memory access type
-    using return_type       = return_helper<sub_type, decltype(sub_expr[0])>;       ///< The type returned by the view
-    using const_return_type = const_return_helper<sub_type, decltype(sub_expr[0])>; ///< The const type return by the view
-
-    /*!
-     * \brief The vectorization type for V
-     */
-    template<typename V = default_vec>
-    using vec_type               = typename V::template vec_type<value_type>;
-
-    /*!
-     * \brief Construct a new sub_view over the given sub expression
-     * \param sub_expr The sub expression
-     * \param i The sub index
-     */
-    sub_view(sub_type sub_expr, std::size_t i)
-            : sub_expr(sub_expr), i(i), sub_offset(i * subsize(sub_expr)) {}
-
-    /*!
-     * \brief Returns the element at the given index
-     * \param j The index
-     * \return a reference to the element at the given index.
-     */
-    const_return_type operator[](std::size_t j) const {
-        return decay_traits<sub_type>::storage_order == order::RowMajor
-                   ? sub_expr[sub_offset + j]
-                   : sub_expr[i + dim<0>(sub_expr) * j];
-    }
-
-    /*!
-     * \brief Returns the element at the given index
-     * \param j The index
-     * \return a reference to the element at the given index.
-     */
-    return_type operator[](std::size_t j) {
-        return decay_traits<sub_type>::storage_order == order::RowMajor
-                   ? sub_expr[sub_offset + j]
-                   : sub_expr[i + dim<0>(sub_expr) * j];
-    }
-
-    /*!
-     * \brief Returns the value at the given index
-     * This function never has side effects.
-     * \param j The index
-     * \return the value at the given index.
-     */
-    value_type read_flat(std::size_t j) const noexcept {
-        return decay_traits<sub_type>::storage_order == order::RowMajor
-                   ? sub_expr.read_flat(sub_offset + j)
-                   : sub_expr.read_flat(i + dim<0>(sub_expr) * j);
-    }
-
-    /*!
-     * \brief Access to the element at the given (args...) position
-     * \param args The indices
-     * \return a reference to the element at the given position.
-     */
-    template <typename... S, cpp_enable_if((sizeof...(S) + 1 == decay_traits<sub_type>::dimensions()))>
-    ETL_STRONG_INLINE(const_return_type) operator()(S... args) const {
-        return sub_expr(i, static_cast<std::size_t>(args)...);
-    }
-
-    /*!
-     * \brief Access to the element at the given (args...) position
-     * \param args The indices
-     * \return a reference to the element at the given position.
-     */
-    template <typename... S, cpp_enable_if((sizeof...(S) + 1 == decay_traits<sub_type>::dimensions()))>
-    ETL_STRONG_INLINE(return_type) operator()(S... args) {
-        return sub_expr(i, static_cast<std::size_t>(args)...);
-    }
-
-    /*!
-     * \brief Returns the value on which the transformer is working.
-     * \return A reference  to the value on which the transformer is working.
-     */
-    sub_type& value() {
-        return sub_expr;
-    }
-
-    /*!
-     * \brief Returns the value on which the transformer is working.
-     * \return A reference  to the value on which the transformer is working.
-     */
-    const sub_type& value() const {
-        return sub_expr;
-    }
-
-    /*!
-     * \brief Store several elements in the matrix at once
-     * \param in The several elements to store
-     * \param x The position at which to start. This will be aligned from the beginning (multiple of the vector size).
-     * \tparam V The vectorization mode to use
-     */
-    template <typename V = default_vec>
-    void store(vec_type<V> in, std::size_t x) noexcept {
-        return value().template storeu<V>(in, x + sub_offset);
-    }
-
-    /*!
-     * \brief Store several elements in the matrix at once
-     * \param in The several elements to store
-     * \param x The position at which to start. This will be aligned from the beginning (multiple of the vector size).
-     * \tparam V The vectorization mode to use
-     */
-    template <typename V = default_vec>
-    void storeu(vec_type<V> in, std::size_t x) noexcept {
-        return value().template storeu<V>(in, x + sub_offset);
-    }
-
-    /*!
-     * \brief Store several elements in the matrix at once, using non-temporal store
-     * \param in The several elements to store
-     * \param x The position at which to start. This will be aligned from the beginning (multiple of the vector size).
-     * \tparam V The vectorization mode to use
-     */
-    template <typename V = default_vec>
-    void stream(vec_type<V> in, std::size_t x) noexcept {
-        return value().template storeu<V>(in, x + sub_offset);
-    }
-
-    /*!
-     * \brief Load several elements of the expression at once
-     * \param x The position at which to start. This will be aligned from the beginning (multiple of the vector size).
-     * \tparam V The vectorization mode to use
-     * \return a vector containing several elements of the expression
-     */
-    template <typename V = default_vec>
-    ETL_STRONG_INLINE(vec_type<V>) load(std::size_t x) const noexcept {
-        return sub_expr.template loadu<V>(x + sub_offset);
-    }
-
-    /*!
-     * \brief Load several elements of the expression at once
-     * \param x The position at which to start. This will be aligned from the beginning (multiple of the vector size).
-     * \tparam V The vectorization mode to use
-     * \return a vector containing several elements of the expression
-     */
-    template <typename V = default_vec>
-    ETL_STRONG_INLINE(vec_type<V>) loadu(std::size_t x) const noexcept {
-        return sub_expr.template loadu<V>(x + sub_offset);
-    }
-
-    /*!
-     * \brief Test if this expression aliases with the given expression
-     * \param rhs The other expression to test
-     * \return true if the two expressions aliases, false otherwise
-     */
-    template <typename E>
-    bool alias(const E& rhs) const noexcept {
-        return sub_expr.alias(rhs);
-    }
-
-    /*!
-     * \brief Returns a pointer to the first element in memory.
-     * \return a pointer tot the first element in memory.
-     */
-    ETL_STRONG_INLINE(memory_type) memory_start() noexcept {
-        static_assert(has_direct_access<T>::value && decay_traits<sub_type>::storage_order == order::RowMajor, "This expression does not have direct memory access");
-        return sub_expr.memory_start() + sub_offset;
-    }
-
-    /*!
-     * \brief Returns a pointer to the first element in memory.
-     * \return a pointer tot the first element in memory.
-     */
-    ETL_STRONG_INLINE(const_memory_type) memory_start() const noexcept {
-        static_assert(has_direct_access<T>::value && decay_traits<sub_type>::storage_order == order::RowMajor, "This expression does not have direct memory access");
-        return sub_expr.memory_start() + sub_offset;
-    }
-
-    /*!
-     * \brief Returns a pointer to the past-the-end element in memory.
-     * \return a pointer tot the past-the-end element in memory.
-     */
-    ETL_STRONG_INLINE(memory_type) memory_end() noexcept {
-        static_assert(has_direct_access<T>::value && decay_traits<sub_type>::storage_order == order::RowMajor, "This expression does not have direct memory access");
-        return sub_expr.memory_start() + (i + 1) * subsize(sub_expr);
-    }
-
-    /*!
-     * \brief Returns a pointer to the past-the-end element in memory.
-     * \return a pointer tot the past-the-end element in memory.
-     */
-    ETL_STRONG_INLINE(const_memory_type) memory_end() const noexcept {
-        static_assert(has_direct_access<T>::value && decay_traits<sub_type>::storage_order == order::RowMajor, "This expression does not have direct memory access");
-        return sub_expr.memory_start() + (i + 1) * subsize(sub_expr);
-    }
-
-    /*!
-     * \brief Returns a reference to the ith dimension value.
-     *
-     * This should only be used internally and with care
-     *
-     * \return a refernece to the ith dimension value.
-     */
-    std::size_t& unsafe_dimension_access(std::size_t i) {
-        return sub_expr.unsafe_dimension_access(i + 1);
-    }
-};
-
-/*!
  * \brief View that shows a slice of an expression
  * \tparam T The type of expression on which the view is made
  */
@@ -426,7 +212,7 @@ struct slice_view {
     using const_return_type = const_return_helper<sub_type, decltype(sub[0])>; ///< The const type return by the view
 
     /*!
-     * \brief Construct a new sub_view over the given sub expression
+     * \brief Construct a new slice_view over the given sub expression
      * \param sub The sub expression
      * \param first The first index
      * \param last The last index
@@ -609,7 +395,7 @@ struct memory_slice_view {
     using vec_type               = typename V::template vec_type<value_type>;
 
     /*!
-     * \brief Construct a new sub_view over the given sub expression
+     * \brief Construct a new memory_slice_view over the given sub expression
      * \param sub The sub expression
      * \param first The first index
      * \param last The last index
@@ -1364,84 +1150,6 @@ struct etl_traits<etl::dim_view<T, D>> {
 };
 
 /*!
- * \brief Specialization for sub_view
- */
-template <typename T>
-struct etl_traits<etl::sub_view<T>> {
-    using expr_t     = etl::sub_view<T>; ///< The expression type
-    using sub_expr_t = std::decay_t<T>;  ///< The sub expression type
-
-    static constexpr bool is_etl                  = true;                                            ///< Indicates if the type is an ETL expression
-    static constexpr bool is_transformer          = false;                                           ///< Indicates if the type is a transformer
-    static constexpr bool is_view                 = true;                                            ///< Indicates if the type is a view
-    static constexpr bool is_magic_view           = false;                                           ///< Indicates if the type is a magic view
-    static constexpr bool is_fast                 = etl_traits<sub_expr_t>::is_fast;                 ///< Indicates if the expression is fast
-    static constexpr bool is_linear               = etl_traits<sub_expr_t>::is_linear;               ///< Indicates if the expression is linear
-    static constexpr bool is_thread_safe          = etl_traits<sub_expr_t>::is_thread_safe;          ///< Indicates if the expression is thread safe
-    static constexpr bool is_value                = false;                                           ///< Indicates if the expression is of value type
-    static constexpr bool is_direct               = etl_traits<sub_expr_t>::is_direct && etl_traits<sub_expr_t>::storage_order == order::RowMajor;               ///< Indicates if the expression has direct memory access
-    static constexpr bool is_generator            = false;                                           ///< Indicates if the expression is a generator
-    static constexpr bool is_padded               = false;                          ///< Indicates if the expression is padded
-    static constexpr bool is_aligned               = false;                          ///< Indicates if the expression is padded
-    static constexpr bool needs_temporary_visitor = etl_traits<sub_expr_t>::needs_temporary_visitor; ///< Indicates if the expression needs a temporary visitor
-    static constexpr bool needs_evaluator_visitor = etl_traits<sub_expr_t>::needs_evaluator_visitor; ///< Indicates if the exxpression needs a evaluator visitor
-    static constexpr order storage_order          = etl_traits<sub_expr_t>::storage_order;           ///< The expression's storage order
-
-    /*!
-     * \brief Indicates if the expression is vectorizable using the
-     * given vector mode
-     * \tparam V The vector mode
-     */
-    template <vector_mode_t V>
-    using vectorizable = cpp::bool_constant<decay_traits<sub_expr_t>::template vectorizable<V>::value && storage_order == order::RowMajor>;
-
-    /*!
-     * \brief Returns the size of the given expression
-     * \param v The expression to get the size for
-     * \returns the size of the given expression
-     */
-    static std::size_t size(const expr_t& v) {
-        return etl_traits<sub_expr_t>::size(v.sub_expr) / etl_traits<sub_expr_t>::dim(v.sub_expr, 0);
-    }
-
-    /*!
-     * \brief Returns the dth dimension of the given expression
-     * \param v The expression
-     * \param d The dimension to get
-     * \return The dth dimension of the given expression
-     */
-    static std::size_t dim(const expr_t& v, std::size_t d) {
-        return etl_traits<sub_expr_t>::dim(v.sub_expr, d + 1);
-    }
-
-    /*!
-     * \brief Returns the size of an expression of this fast type.
-     * \returns the size of an expression of this fast type.
-     */
-    static constexpr std::size_t size() {
-        return etl_traits<sub_expr_t>::size() / etl_traits<sub_expr_t>::template dim<0>();
-    }
-
-    /*!
-     * \brief Returns the Dth dimension of an expression of this type
-     * \tparam D The dimension to get
-     * \return the Dth dimension of an expression of this type
-     */
-    template <std::size_t D>
-    static constexpr std::size_t dim() {
-        return etl_traits<sub_expr_t>::template dim<D + 1>();
-    }
-
-    /*!
-     * \brief Returns the number of expressions for this type
-     * \return the number of dimensions of this type
-     */
-    static constexpr std::size_t dimensions() {
-        return etl_traits<sub_expr_t>::dimensions() - 1;
-    }
-};
-
-/*!
  * \brief Specialization for slice_view
  */
 template <typename T>
@@ -1716,17 +1424,6 @@ struct etl_traits<etl::dyn_matrix_view<T, D>> {
 template <typename T, std::size_t D>
 std::ostream& operator<<(std::ostream& os, const dim_view<T, D>& v) {
     return os << "dim[" << D << "](" << v.sub << ", " << v.i << ")";
-}
-
-/*!
- * \brief Print a representation of the view on the given stream
- * \param os The output stream
- * \param v The view to print
- * \return the output stream
- */
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const sub_view<T>& v) {
-    return os << "sub(" << v.sub_expr << ", " << v.i << ")";
 }
 
 /*!
