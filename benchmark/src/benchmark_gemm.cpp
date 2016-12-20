7708ca7deab5ff14be7bec1743e1f6298912be80
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-12-20 20:50:51 +0100

    Fix the benchmkar

--- benchmark/src/benchmark_gemm.cpp
+++ benchmark/src/benchmark_gemm.cpp
@@ -62,7 +62,7 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("A * B (z) [gemm]", small_square_policy,
 CPM_DIRECT_SECTION_TWO_PASS_NS_PF("A * trans(B) (s) [gemm]", gemm_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d2 * d2; }),
     CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(smat(d1,d2), smat(d1,d2), smat(d1, d2)); }),
-    CPM_SECTION_FUNCTOR("default", [](smat& a, smat& b, smat& c){ c = a * b; }),
+    CPM_SECTION_FUNCTOR("default", [](smat& a, smat& b, smat& c){ c = a * transpose(b); }),
     CPM_SECTION_FUNCTOR("std", [](smat& a, smat& b, smat& c){ c = selected_helper(etl::gemm_impl::STD, a * transpose(b)); })
     VEC_SECTION_FUNCTOR("vec", [](smat& a, smat& b, smat& c){ c = selected_helper(etl::gemm_impl::VEC, a * transpose(b)); })
     BLAS_SECTION_FUNCTOR("blas", [](smat& a, smat& b, smat& c){ c = selected_helper(etl::gemm_impl::BLAS, a * transpose(b)); })
