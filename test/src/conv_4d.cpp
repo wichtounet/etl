af7af649b9429c9dbed0deded500be4d72ae545b
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-02-10 07:58:51 +0100

    New test

--- test/src/conv_4d.cpp
+++ test/src/conv_4d.cpp
@@ -127,12 +127,35 @@ CONV4_FULL_TEST_CASE("conv_4d/full_2", "[conv][conv4][valid]") {
     }
 }
 
-CONV4_FULL_FLIPPED_TEST_CASE("conv_4d/full_3", "[conv][conv4][valid]") {
+CONV4_FULL_FLIPPED_TEST_CASE("conv/4d/full/flipped/1", "[conv][conv4][valid]") {
     etl::fast_matrix<T, 8, 9, 6, 6> I(etl::sequence_generator(15.0) * -3.0);
     etl::fast_matrix<T, 9, 3, 3, 3> K(etl::sequence_generator(-4.0) * 1.6);
 
     etl::fast_matrix<T, 8, 3, 8, 8> ref;
     etl::fast_matrix<T, 8, 3, 8, 8> c;
+
+    ref = 0.0;
+    for(std::size_t i = 0; i < etl::dim<0>(I); ++i){
+        for(std::size_t c = 0; c < etl::dim<1>(K); ++c){
+            for(std::size_t k = 0; k < etl::dim<0>(K); ++k){
+                ref(i)(c) += conv_2d_full(I(i)(k), fflip(K(k)(c)));
+            }
+        }
+    }
+
+    Impl::apply(I, K, c);
+
+    for(std::size_t i = 0; i < ref.size(); ++i){
+        REQUIRE_EQUALS_APPROX(c[i], ref[i]);
+    }
+}
+
+CONV4_FULL_FLIPPED_TEST_CASE("conv/4d/full/flipped/2", "[conv][conv4][valid]") {
+    etl::fast_matrix<T, 5, 3, 6, 6> I(etl::sequence_generator(1.1) * -3.0);
+    etl::fast_matrix<T, 3, 2, 3, 3> K(etl::sequence_generator(-1.3) * 1.6);
+
+    etl::fast_matrix<T, 5, 2, 8, 8> ref;
+    etl::fast_matrix<T, 5, 2, 8, 8> c;
 
     ref = 0.0;
     for(std::size_t i = 0; i < etl::dim<0>(I); ++i){
