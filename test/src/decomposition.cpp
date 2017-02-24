35ec0c2b7016c266f5ddebca73cd62fc38116b67
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-02-24 11:22:43 +0100

    Test for QR decomposition

--- test/src/decomposition.cpp
+++ test/src/decomposition.cpp
@@ -7,8 +7,7 @@
 
 #include "test.hpp"
 
-// Note: The ugliness of the some comparisons is due to Catch being
-// a douche
+/* LU */
 
 TEMPLATE_TEST_CASE_2("globals/lu/1", "[globals][LU]", Z, float, double) {
     etl::fast_matrix<Z, 3, 3> A{1, 3, 5, 2, 4, 7, 1, 1, 0};
@@ -18,37 +17,12 @@ TEMPLATE_TEST_CASE_2("globals/lu/1", "[globals][LU]", Z, float, double) {
 
     etl::lu(A, L, U, P);
 
-    REQUIRE_EQUALS((P * A == L * U), true);
+    etl::fast_matrix<Z, 3, 3> PA;
+    etl::fast_matrix<Z, 3, 3> LU;
+    PA = P * A;
+    LU = L * U;
 
-    REQUIRE_EQUALS(L(0, 0), 1.0);
-    REQUIRE_EQUALS(L(0, 1), 0.0);
-    REQUIRE_EQUALS(L(0, 2), 0.0);
-    REQUIRE_EQUALS(L(1, 0), 0.5);
-    REQUIRE_EQUALS(L(1, 1), 1.0);
-    REQUIRE_EQUALS(L(1, 2), 0.0);
-    REQUIRE_EQUALS(L(2, 0), 0.5);
-    REQUIRE_EQUALS(L(2, 1), -1.0);
-    REQUIRE_EQUALS(L(2, 2), 1.0);
-
-    REQUIRE_EQUALS(U(0, 0), 2.0);
-    REQUIRE_EQUALS(U(0, 1), 4.0);
-    REQUIRE_EQUALS(U(0, 2), 7.0);
-    REQUIRE_EQUALS(U(1, 0), 0.0);
-    REQUIRE_EQUALS(U(1, 1), 1.0);
-    REQUIRE_EQUALS(U(1, 2), 1.5);
-    REQUIRE_EQUALS(U(2, 0), 0.0);
-    REQUIRE_EQUALS(U(2, 1), 0.0);
-    REQUIRE_EQUALS(U(2, 2), -2.0);
-
-    REQUIRE_EQUALS(P(0, 0), 0.0);
-    REQUIRE_EQUALS(P(0, 1), 1.0);
-    REQUIRE_EQUALS(P(0, 2), 0.0);
-    REQUIRE_EQUALS(P(1, 0), 1.0);
-    REQUIRE_EQUALS(P(1, 1), 0.0);
-    REQUIRE_EQUALS(P(1, 2), 0.0);
-    REQUIRE_EQUALS(P(2, 0), 0.0);
-    REQUIRE_EQUALS(P(2, 1), 0.0);
-    REQUIRE_EQUALS(P(2, 2), 1.0);
+    REQUIRE_DIRECT(approx_equals(PA, LU, base_eps));
 }
 
 TEMPLATE_TEST_CASE_2("globals/lu/2", "[globals][LU]", Z, float, double) {
@@ -59,57 +33,25 @@ TEMPLATE_TEST_CASE_2("globals/lu/2", "[globals][LU]", Z, float, double) {
 
     etl::lu(A, L, U, P);
 
-    //TODO Review this test
-    REQUIRE_EQUALS((P * A == L * U), true);
+    etl::fast_matrix<Z, 4, 4> PA;
+    etl::fast_matrix<Z, 4, 4> LU;
+    PA = P * A;
+    LU = L * U;
 
-    REQUIRE_EQUALS_APPROX(L(0, 0), Z(1.0));
-    REQUIRE_EQUALS_APPROX(L(0, 1), Z(0.0));
-    REQUIRE_EQUALS_APPROX(L(0, 2), Z(0.0));
-    REQUIRE_EQUALS_APPROX(L(0, 3), Z(0.0));
-    REQUIRE_EQUALS_APPROX(L(1, 0), Z(0.27273));
-    REQUIRE_EQUALS_APPROX(L(1, 1), Z(1.0));
-    REQUIRE_EQUALS_APPROX(L(1, 2), Z(0.0));
-    REQUIRE_EQUALS_APPROX(L(1, 3), Z(0.0));
-    REQUIRE_EQUALS_APPROX(L(2, 0), Z(0.09091));
-    REQUIRE_EQUALS_APPROX(L(2, 1), Z(0.28750));
-    REQUIRE_EQUALS_APPROX(L(2, 2), Z(1.0));
-    REQUIRE_EQUALS_APPROX(L(2, 3), Z(0.0));
-    REQUIRE_EQUALS_APPROX(L(3, 0), Z(0.18182));
-    REQUIRE_EQUALS_APPROX(L(3, 1), Z(0.23125));
-    REQUIRE_EQUALS_APPROX(L(3, 2), Z(0.00360));
-    REQUIRE_EQUALS_APPROX(L(3, 3), Z(1.0));
+    REQUIRE_DIRECT(approx_equals(PA, LU, base_eps));
+}
 
-    REQUIRE_EQUALS_APPROX(U(0, 0), Z(11.0));
-    REQUIRE_EQUALS_APPROX(U(0, 1), Z(9.0));
-    REQUIRE_EQUALS_APPROX(U(0, 2), Z(24.0));
-    REQUIRE_EQUALS_APPROX(U(0, 3), Z(2.0));
-    REQUIRE_EQUALS_APPROX(U(1, 0), Z(0.0));
-    REQUIRE_EQUALS_APPROX(U(1, 1), Z(14.54545));
-    REQUIRE_EQUALS_APPROX(U(1, 2), Z(11.45455));
-    REQUIRE_EQUALS_APPROX(U(1, 3), Z(0.45455));
-    REQUIRE_EQUALS_APPROX(U(2, 0), Z(0.0));
-    REQUIRE_EQUALS_APPROX(U(2, 1), Z(0.0));
-    REQUIRE_EQUALS_APPROX(U(2, 2), Z(-3.475));
-    REQUIRE_EQUALS_APPROX(U(2, 3), Z(5.6875));
-    REQUIRE_EQUALS_APPROX(U(3, 0), Z(0.0));
-    REQUIRE_EQUALS_APPROX(U(3, 1), Z(0.0));
-    REQUIRE_EQUALS_APPROX(U(3, 2), Z(0.0));
-    REQUIRE_EQUALS_APPROX(U(3, 3), Z(0.51079));
+/* QR */
 
-    REQUIRE_EQUALS(P(0, 0), 1.0);
-    REQUIRE_EQUALS(P(0, 1), 0.0);
-    REQUIRE_EQUALS(P(0, 2), 0.0);
-    REQUIRE_EQUALS(P(1, 0), 0.0);
-    REQUIRE_EQUALS(P(0, 3), 0.0);
-    REQUIRE_EQUALS(P(1, 1), 0.0);
-    REQUIRE_EQUALS(P(1, 2), 1.0);
-    REQUIRE_EQUALS(P(1, 3), 0.0);
-    REQUIRE_EQUALS(P(2, 0), 0.0);
-    REQUIRE_EQUALS(P(2, 1), 1.0);
-    REQUIRE_EQUALS(P(2, 2), 0.0);
-    REQUIRE_EQUALS(P(2, 3), 0.0);
-    REQUIRE_EQUALS(P(3, 0), 0.0);
-    REQUIRE_EQUALS(P(3, 1), 0.0);
-    REQUIRE_EQUALS(P(3, 2), 0.0);
-    REQUIRE_EQUALS(P(3, 3), 1.0);
+TEMPLATE_TEST_CASE_2("globals/qr/1", "[globals][QR]", Z, float, double) {
+    etl::fast_matrix<Z, 5, 3> A{12, -51, 4, 6, 167, -68, -4, 24, -41, -1, 1, 0, 2, 0, 3};
+    etl::fast_matrix<Z, 5, 5> Q;
+    etl::fast_matrix<Z, 5, 3> R;
+    etl::fast_matrix<Z, 5, 3> QR;
+
+    etl::qr(A, Q, R);
+
+    QR = Q * R;
+
+    REQUIRE_DIRECT(approx_equals(QR, A, base_eps));
 }
