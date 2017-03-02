d0e9a6ceb18f80fd191f2e20f895d0da7c454263
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-02 16:07:28 +0100

    Revert the const ref

--- include/etl/stop.hpp
+++ include/etl/stop.hpp
@@ -15,7 +15,7 @@ namespace etl {
  * \return A value class with the values of the given expression
  */
 template <typename T, cpp_enable_if(is_etl_expr<T>::value, !etl_traits<T>::is_fast)>
-auto s(const T& value) {
+auto s(T&& value) {
     // Sizes will be directly propagated
     dyn_matrix<value_t<T>, etl_traits<T>::dimensions()> mat;
     mat = value;
@@ -42,7 +42,7 @@ struct build_matrix_type<M, std::index_sequence<I...>> {
  * \return A value class with the values of the given expression
  */
 template <typename T, cpp_enable_if(is_etl_expr<T>::value, etl_traits<T>::is_fast)>
-auto s(const T& value) {
+auto s(T&& value) {
     typename build_matrix_type<T, std::make_index_sequence<etl_traits<T>::dimensions()>>::type mat;
     mat = value;
     return mat;
