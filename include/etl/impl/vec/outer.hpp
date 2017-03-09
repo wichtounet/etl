efb9c8a00ec95032a5633007b0d7bbb82ce1f930
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-09 11:25:02 +0100

    Doc

--- include/etl/impl/vec/outer.hpp
+++ include/etl/impl/vec/outer.hpp
@@ -264,6 +264,12 @@ void batch_outer(const L& lhs, const R& rhs, C&& result) {
     result.invalidate_gpu();
 }
 
+/*!
+ * \brief Compute the batch outer product of a and b and store the result in c
+ * \param lhs The a expression
+ * \param rhs The b expression
+ * \param result The c expression
+ */
 template <typename A, typename B, typename C>
 void batch_outer(const A& lhs, const B& rhs, C&& c) {
     batch_outer<default_vec>(lhs, rhs, c);
