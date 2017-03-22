36c9e2e9aee7b07796f23ac1e7068f47202b275e
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-22 10:27:15 +0100

    Optimize handling of parallel MKL
    This brings very high speedup for conv4_full
    Most of the time the convolutions are going to be using small
    kernels, therefore, the parallelization is more important around the
    FFT than inside

--- include/etl/impl/blas/fft.hpp
+++ include/etl/impl/blas/fft.hpp
@@ -1200,10 +1200,20 @@ void conv2_full_multi(I&& input, K&& kernel, C&& conv) {
             }
         };
 
-        if (etl::is_parallel && !is_blas_parallel) {
+        size_t mkl_threads = 0;
+        if(is_blas_parallel){
+            mkl_threads = mkl_get_max_threads();
+            mkl_set_num_threads(1);
+        }
+
+        if (etl::is_parallel) {
             dispatch_1d_any(select_parallel(KK, 2), batch_fun_k, 0, KK);
         } else {
             batch_fun_k(0, KK);
+        }
+
+        if(is_blas_parallel){
+            mkl_set_num_threads(mkl_threads);
         }
 
         conv.invalidate_gpu();
@@ -1330,12 +1340,22 @@ void conv4_full(I&& input, KK&& kernel, CC&& conv) {
             }
         };
 
-        if (etl::is_parallel && !is_blas_parallel) {
+        size_t mkl_threads = 0;
+        if(is_blas_parallel){
+            mkl_threads = mkl_get_max_threads();
+            mkl_set_num_threads(1);
+        }
+
+        if (etl::is_parallel) {
             dispatch_1d_any(select_parallel(K * C, 2), batch_fun_kc, 0, K * C);
             dispatch_1d_any(select_parallel(N, 2), batch_fun_n, 0, N);
         } else {
             batch_fun_kc(0, K * C);
             batch_fun_n(0, N);
+        }
+
+        if(is_blas_parallel){
+            mkl_set_num_threads(mkl_threads);
         }
 
         conv.invalidate_gpu();
