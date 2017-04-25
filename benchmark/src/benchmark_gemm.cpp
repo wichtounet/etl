5954148e92223401f3d4f5c851667a406b088170
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-04-25 20:57:50 +0200

    New benchmark for column major

--- benchmark/src/benchmark_gemm.cpp
+++ benchmark/src/benchmark_gemm.cpp
@@ -99,6 +99,16 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("A * x (d) [gemm]", gemv_policy,
     CUBLAS_SECTION_FUNCTOR("cublas", [](dmat& a, dvec& b, dvec& c){ c = selected_helper(etl::gemm_impl::CUBLAS, a * b); })
 )
 
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("A * x (cm/s) [gemm]", gemv_policy,
+    FLOPS([](std::size_t d1, std::size_t d2){ return 2 * d1 * d2; }),
+    CPM_SECTION_INIT([](std::size_t d1, std::size_t d2){ return std::make_tuple(smat(d1,d2), svec(d2), svec(d1)); }),
+    CPM_SECTION_FUNCTOR("default", [](smat& a, svec& b, svec& c){ c = a * b; }),
+    CPM_SECTION_FUNCTOR("std", [](smat& a, svec& b, svec& c){ c = selected_helper(etl::gemm_impl::STD, a * b); })
+    VEC_SECTION_FUNCTOR("vec", [](smat& a, svec& b, svec& c){ c = selected_helper(etl::gemm_impl::VEC, a * b); })
+    BLAS_SECTION_FUNCTOR("blas", [](smat& a, svec& b, svec& c){ c = selected_helper(etl::gemm_impl::BLAS, a * b); })
+    CUBLAS_SECTION_FUNCTOR("cublas", [](smat& a, svec& b, svec& c){ c = selected_helper(etl::gemm_impl::CUBLAS, a * b); })
+)
+
 #ifdef ETL_EXTENDED_BENCH
 CPM_DIRECT_SECTION_TWO_PASS_NS_PF("A * x (c) [gemm]", gemv_policy,
     FLOPS([](std::size_t d1, std::size_t d2){ return 6 * 2 * d1 * d2; }),
