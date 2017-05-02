fc4f226c00a84ee5fb3fc3ac9c23da0d28c6d180
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-05-02 20:53:33 +0200

    Prepare 2D dispatching code (untested)

--- include/etl/parallel.hpp
+++ include/etl/parallel.hpp
@@ -93,6 +93,86 @@ inline void engine_dispatch_1d(Functor&& functor, size_t first, size_t last, siz
     }
 }
 
+inline std::pair<size_t, size_t> thread_blocks(size_t M, size_t N) {
+    if (M >= N) {
+        size_t m = std::min(threads, std::max(1UL, size_t(round(std::sqrt(threads * double(M) / double(N))))));
+        size_t n = threads / m;
+
+        while (m * n != threads) {
+            ++m;
+            n = threads / m;
+        }
+
+        return {m, n};
+    } else {
+        size_t n = std::min(threads, std::max(1UL, size_t(round(std::sqrt(threads * double(N) / double(M))))));
+        size_t m = threads / n;
+
+        while (m * n != threads) {
+            ++n;
+            m = threads / n;
+        }
+
+        return {m, n};
+    }
+}
+
+/*!
+ * \brief Dispatch the elements of a 2D range to a functor in a parallel
+ * manner, using the global thread engine.
+ *
+ * The dispatching will be done in batch. That is to say that the
+ * functor will be called with a range of data.
+ *
+ * This will only be dispatched in parallel if etl is running in
+ * parallel mode and if the range is bigger than the treshold.
+ *
+ * \param functor The functor to execute
+ * \param last1 The size of the first range
+ * \param last2 The size of the first range
+ * \param threshold The threshold for parallelization
+ */
+template <typename Functor>
+inline void engine_dispatch_2d(Functor&& functor, size_t last1, size_t last2, size_t threshold) {
+    cpp_assert(last >= first, "Range must be valid");
+
+    if (last1 && last2) {
+        if (engine_select_parallel(last1 * last2, threshold)) {
+            thread_engine::acquire();
+
+            auto block = thread_blocks(last1, last2);
+
+            const size_t block_1 = last1 / block.first + (last1 % block.first > 0);
+            const size_t block_2 = last2 / block.second + (last2 % block.second > 0);
+
+            for(size_t i = 0; i < block.first; ++i){
+                const size_t row = block_1 * i;
+
+                if(cpp_unlikely(row >= last1)){
+                    continue;
+                }
+
+                for(size_t j = 0; j < block.second; ++j){
+                    const size_t column = block_2 * j;
+
+                    if(cpp_unlikely(column >= last2)){
+                        continue;
+                    }
+
+                    const size_t m = std::min(block_1, last1 - row);
+                    const size_t n = std::min(block_2, last2 - column);
+
+                    thread_engine::schedule(functor, row, row + m, column, column + n);
+                }
+            }
+
+            thread_engine::wait();
+        } else {
+            functor(0, last1, 0, last2);
+        }
+    }
+}
+
 /*!
  * \brief Dispatch the elements of a range to a functor in a parallel
  * manner, using the global thread engine.
