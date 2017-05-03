f5518ef81f416193078b3145fd76330d65294a91
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-05-03 14:29:48 +0200

    Full optimizations for bias batch mean

--- include/etl/expr/bias_batch_mean_expr.hpp
+++ include/etl/expr/bias_batch_mean_expr.hpp
@@ -70,21 +70,21 @@ struct bias_batch_mean_expr : base_temporary_expr_un<bias_batch_mean_expr<A>, A>
      * \brief Assign to a matrix of the same storage order
      * \param c The expression to which assign
      */
-    template<typename C>
-    void assign_to(C&& c)  const {
-        static_assert(all_etl_expr<A, C>::value, "bias_batch_mean only supported for ETL expressions");
+    template<typename L>
+    void assign_to(L&& lhs)  const {
+        static_assert(all_etl_expr<A, L>::value, "bias_batch_mean only supported for ETL expressions");
 
         auto& a = this->a();
 
         standard_evaluator::pre_assign_rhs(a);
-        standard_evaluator::pre_assign_lhs(c);
+        standard_evaluator::pre_assign_lhs(lhs);
 
-        const auto N = etl::size(a) / etl::size(c);
-        const auto K = etl::size(c);
+        const auto N = etl::size(a) / etl::size(lhs);
+        const auto K = etl::size(lhs);
 
         using T = value_t<A>;
 
-        check(a, c);
+        check(a, lhs);
 
         auto batch_fun_k = [&](const size_t first, const size_t last) {
             if (last - first) {
@@ -96,7 +96,7 @@ struct bias_batch_mean_expr : base_temporary_expr_un<bias_batch_mean_expr<A>, A>
                             mean += sum(a(b)(k));
                         }
 
-                        c(k) = mean / N;
+                        lhs(k) = mean / N;
                     }
                 }
             }
@@ -111,7 +111,37 @@ struct bias_batch_mean_expr : base_temporary_expr_un<bias_batch_mean_expr<A>, A>
      */
     template<typename L>
     void assign_add_to(L&& lhs)  const {
-        std_add_evaluate(*this, lhs);
+        static_assert(all_etl_expr<A, L>::value, "bias_batch_mean only supported for ETL expressions");
+
+        auto& a = this->a();
+
+        standard_evaluator::pre_assign_rhs(a);
+        standard_evaluator::pre_assign_lhs(lhs);
+
+        const auto N = etl::size(a) / etl::size(lhs);
+        const auto K = etl::size(lhs);
+
+        using T = value_t<A>;
+
+        check(a, lhs);
+
+        auto batch_fun_k = [&](const size_t first, const size_t last) {
+            if (last - first) {
+                SERIAL_SECTION {
+                    for (size_t k = first; k < last; ++k) {
+                        T mean(0);
+
+                        for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                            mean += sum(a(b)(k));
+                        }
+
+                        lhs(k) += mean / N;
+                    }
+                }
+            }
+        };
+
+        engine_dispatch_1d(batch_fun_k, 0, K, 2UL);
     }
 
     /*!
@@ -120,7 +150,37 @@ struct bias_batch_mean_expr : base_temporary_expr_un<bias_batch_mean_expr<A>, A>
      */
     template<typename L>
     void assign_sub_to(L&& lhs)  const {
-        std_sub_evaluate(*this, lhs);
+        static_assert(all_etl_expr<A, L>::value, "bias_batch_mean only supported for ETL expressions");
+
+        auto& a = this->a();
+
+        standard_evaluator::pre_assign_rhs(a);
+        standard_evaluator::pre_assign_lhs(lhs);
+
+        const auto N = etl::size(a) / etl::size(lhs);
+        const auto K = etl::size(lhs);
+
+        using T = value_t<A>;
+
+        check(a, lhs);
+
+        auto batch_fun_k = [&](const size_t first, const size_t last) {
+            if (last - first) {
+                SERIAL_SECTION {
+                    for (size_t k = first; k < last; ++k) {
+                        T mean(0);
+
+                        for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                            mean += sum(a(b)(k));
+                        }
+
+                        lhs(k) -= mean / N;
+                    }
+                }
+            }
+        };
+
+        engine_dispatch_1d(batch_fun_k, 0, K, 2UL);
     }
 
     /*!
@@ -129,7 +189,37 @@ struct bias_batch_mean_expr : base_temporary_expr_un<bias_batch_mean_expr<A>, A>
      */
     template<typename L>
     void assign_mul_to(L&& lhs)  const {
-        std_mul_evaluate(*this, lhs);
+        static_assert(all_etl_expr<A, L>::value, "bias_batch_mean only supported for ETL expressions");
+
+        auto& a = this->a();
+
+        standard_evaluator::pre_assign_rhs(a);
+        standard_evaluator::pre_assign_lhs(lhs);
+
+        const auto N = etl::size(a) / etl::size(lhs);
+        const auto K = etl::size(lhs);
+
+        using T = value_t<A>;
+
+        check(a, lhs);
+
+        auto batch_fun_k = [&](const size_t first, const size_t last) {
+            if (last - first) {
+                SERIAL_SECTION {
+                    for (size_t k = first; k < last; ++k) {
+                        T mean(0);
+
+                        for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                            mean += sum(a(b)(k));
+                        }
+
+                        lhs(k) *= mean / N;
+                    }
+                }
+            }
+        };
+
+        engine_dispatch_1d(batch_fun_k, 0, K, 2UL);
     }
 
     /*!
@@ -138,7 +228,37 @@ struct bias_batch_mean_expr : base_temporary_expr_un<bias_batch_mean_expr<A>, A>
      */
     template<typename L>
     void assign_div_to(L&& lhs)  const {
-        std_div_evaluate(*this, lhs);
+        static_assert(all_etl_expr<A, L>::value, "bias_batch_mean only supported for ETL expressions");
+
+        auto& a = this->a();
+
+        standard_evaluator::pre_assign_rhs(a);
+        standard_evaluator::pre_assign_lhs(lhs);
+
+        const auto N = etl::size(a) / etl::size(lhs);
+        const auto K = etl::size(lhs);
+
+        using T = value_t<A>;
+
+        check(a, lhs);
+
+        auto batch_fun_k = [&](const size_t first, const size_t last) {
+            if (last - first) {
+                SERIAL_SECTION {
+                    for (size_t k = first; k < last; ++k) {
+                        T mean(0);
+
+                        for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                            mean += sum(a(b)(k));
+                        }
+
+                        lhs(k) /= mean / N;
+                    }
+                }
+            }
+        };
+
+        engine_dispatch_1d(batch_fun_k, 0, K, 2UL);
     }
 
     /*!
@@ -147,7 +267,37 @@ struct bias_batch_mean_expr : base_temporary_expr_un<bias_batch_mean_expr<A>, A>
      */
     template<typename L>
     void assign_mod_to(L&& lhs)  const {
-        std_mod_evaluate(*this, lhs);
+        static_assert(all_etl_expr<A, L>::value, "bias_batch_mean only supported for ETL expressions");
+
+        auto& a = this->a();
+
+        standard_evaluator::pre_assign_rhs(a);
+        standard_evaluator::pre_assign_lhs(lhs);
+
+        const auto N = etl::size(a) / etl::size(lhs);
+        const auto K = etl::size(lhs);
+
+        using T = value_t<A>;
+
+        check(a, lhs);
+
+        auto batch_fun_k = [&](const size_t first, const size_t last) {
+            if (last - first) {
+                SERIAL_SECTION {
+                    for (size_t k = first; k < last; ++k) {
+                        T mean(0);
+
+                        for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                            mean += sum(a(b)(k));
+                        }
+
+                        lhs(k) %= mean / N;
+                    }
+                }
+            }
+        };
+
+        engine_dispatch_1d(batch_fun_k, 0, K, 2UL);
     }
 };
 
