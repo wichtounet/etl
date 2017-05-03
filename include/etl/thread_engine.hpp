09ec6740447f210a49bed0fee011922fc1f53cb4
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-05-03 12:56:49 +0200

    Cleanup and doc

--- include/etl/thread_engine.hpp
+++ include/etl/thread_engine.hpp
@@ -11,10 +11,22 @@ namespace etl {
 
 #ifdef ETL_PARALLEL_SUPPORT
 
+/*!
+ * \brief The default thread engine.
+ * \tparam Pool The thread pool implementation
+ *
+ * This should only be used by ETL internals such as the evaluator
+ * and the engine_dispatch functions.
+ */
 template<typename Pool>
 struct conf_thread_engine {
-    //static Pool pool;
-
+    /*!
+     * \brief Acquire the thread engine.
+     *
+     * This function must be called before tasks are being
+     * scheduled. It is mostly to ensure that selection is done
+     * correctly and that the thread engine is used correclty.
+     */
     static void acquire(){
         cpp_assert(etl::parallel_support, "thread_engine can only be used if paralle support is enabled");
         cpp_assert(!local_context().serial, "thread_engine cannot be used in serial context");
@@ -23,17 +35,29 @@ struct conf_thread_engine {
         etl::local_context().serial = true;
     }
 
+    /*!
+     * \brief Schedule a new task
+     * \param fun The functor to execute
+     * \param args The arguments to pass to the functor
+     */
     template <class Functor, typename... Args>
     static void schedule(Functor&& fun, Args&&... args) {
         get_pool().do_task(std::forward<Functor>(fun), std::forward<Args>(args)...);
     }
 
+    /*!
+     * \brief Wait for all the scheduled threads to finish their task
+     */
     static void wait(){
         get_pool().wait();
 
         etl::local_context().serial = false;
     }
 
+    /*!
+     * \brief Returns a reference to the thread pool
+     * \return The unique thread pool.
+     */
     static Pool& get_pool(){
         static Pool pool(etl::threads);
         return pool;
