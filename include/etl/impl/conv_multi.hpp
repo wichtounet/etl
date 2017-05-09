20a5f6570343061a86a49ee3a65150a9bfac3568
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-05-09 20:36:05 +0200

    Expression for conv_2d_same_multi

--- include/etl/impl/conv_multi.hpp
+++ include/etl/impl/conv_multi.hpp
@@ -806,80 +806,12 @@ struct conv2_same_multi_impl {
             cpp_unreachable("Invalid conv implementation selection");
         }
     }
-
-    /*!
-     * \brief Returns the description of the operation
-     */
-    static constexpr const char* desc(){
-        return "conv2_same_multi";
-    }
-
-    /*!
-     * \brief Assert that the convolution is done on correct dimensions
-     */
-    template <typename I, typename K, typename C>
-    static void check(I&& input, K&& kernel, const C& conv){
-        static_assert(etl::dimensions<I>() == 2, "Invalid number of dimensions for input of conv2_same_multi");
-        static_assert(etl::dimensions<K>() == 3, "Invalid number of dimensions for kernel of conv2_same_multi");
-        static_assert(etl::dimensions<C>() == 3, "Invalid number of dimensions for conv of conv2_same_multi");
-
-        cpp_assert(etl::dim(conv, 0) == etl::dim(kernel, 0), "Invalid dimensions for conv2_same_multi");
-        cpp_assert(etl::dim(conv, 1) == etl::dim(input, 0), "Invalid dimensions for conv2_same_multi");
-        cpp_assert(etl::dim(conv, 2) == etl::dim(input, 1), "Invalid dimensions for conv2_same_multi");
-        cpp_assert(etl::dim(input, 0) >= etl::dim(kernel, 1), "Invalid dimensions for conv2_same_multi");
-        cpp_assert(etl::dim(input, 1) >= etl::dim(kernel, 2), "Invalid dimensions for conv2_same_multi");
-
-        cpp_unused(input);
-        cpp_unused(kernel);
-        cpp_unused(conv);
-    }
-
-    /*!
-     * \brief Assert that the convolution is done on correct dimensions
-     */
-    template <typename I, typename K, typename C>
-    static void check(){
-        static_assert(etl::dimensions<I>() == 2, "Invalid number of dimensions for input of conv2_same_multi");
-        static_assert(etl::dimensions<K>() == 3, "Invalid number of dimensions for kernel of conv2_same_multi");
-        static_assert(etl::dimensions<C>() == 3, "Invalid number of dimensions for conv of conv2_same_multi");
-
-        static_assert(etl::dim<0,C>() == etl::dim<0,K>(), "Invalid dimensions for conv2_same_multi");
-        static_assert(etl::dim<1,C>() == etl::dim<0,I>(), "Invalid dimensions for conv2_same_multi");
-        static_assert(etl::dim<2,C>() == etl::dim<1,I>(), "Invalid dimensions for conv2_same_multi");
-        static_assert(etl::dim<0,I>() >= etl::dim<1,K>(), "Invalid dimensions for conv2_same_multi");
-        static_assert(etl::dim<1,I>() >= etl::dim<2,K>(), "Invalid dimensions for conv2_same_multi");
-    }
-
-    /*!
-     * \brief Returns the dth dimension of the result of the convolution
-     */
-    template <typename I, typename K>
-    static size_t dim(size_t d, I&& input, K&& kernel){
-        cpp_assert(d < 3, "Invalid dimensions access");
-
-        if(d == 0){
-            return etl::dim(kernel, 0);
-        } else {
-            return etl::dim(input, d - 1);
-        }
-    }
-
-    /*!
-     * \brief Returns the Dth dimension of the result of the convolution
-     */
-    template <size_t D, typename I, typename K>
-    static constexpr size_t dim(){
-        static_assert(D < 3, "Invalid dimension access");
-
-        return D == 0 ? etl::dim<0,K>()
-            : etl::safe_dim<D - 1,I>();
-    }
 };
 
 /*!
  * \brief The functor impl for 2D same conv, with multiple flipped kernels
  */
-struct conv2_same_multi_flipped_impl : conv2_same_multi_impl {
+struct conv2_same_multi_flipped_impl {
     /*!
      * \brief Apply the convolution
      * \param input The input expression
@@ -897,13 +829,6 @@ struct conv2_same_multi_flipped_impl : conv2_same_multi_impl {
         } else {
             cpp_unreachable("Invalid conv implementation selection");
         }
-    }
-
-    /*!
-     * \brief Returns the description of the operation
-     */
-    static constexpr const char* desc(){
-        return "conv2_same_multi";
     }
 };
 
