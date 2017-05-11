a59b764eb3845a60eb5e94f2deee410e428f1729
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-05-11 14:08:18 +0200

    Vectorized version of bias_add
    Very small gains compared to the compiled version

--- include/etl/impl/vec/bias_add.hpp
+++ include/etl/impl/vec/bias_add.hpp
@@ -26,12 +26,96 @@ namespace vec {
  */
 template <typename V, typename L, typename R, typename C>
 void bias_add(const L& x, const R& b, C&& y) {
+    using vec_type = V;
+    using T        = value_t<L>;
+
+    static constexpr size_t vec_size = vec_type::template traits<T>::size;
+
+    const auto M = etl::dim<2>(x);
+    const auto N = etl::dim<3>(x);
+    const auto MN = M * N;
+
+    x.ensure_cpu_up_to_date();
+    b.ensure_cpu_up_to_date();
+
+    // Note: This kernel is particularly adapted for large inner (MN) dimensions
+
     for (size_t i = 0; i < etl::dim<0>(x); ++i) {
         for (size_t j = 0; j < etl::dim<1>(x); ++j) {
-            for (size_t k = 0; k < etl::dim<2>(x); ++k) {
-                for (size_t l = 0; l < etl::dim<3>(x); ++l) {
-                    y(i, j, k, l) = x(i, j, k, l) + b(j);
-                }
+            auto x_s = x(i)(j).memory_start();
+            auto y_s = y(i)(j).memory_start();
+
+            auto b1 = vec_type::set(b[j]);
+
+            size_t m = 0;
+
+            for(; m + vec_size * 8 - 1 < MN; m += vec_size * 8 ){
+                auto x1 = vec_type::loadu(x_s + m + 0 * vec_size);
+                auto x2 = vec_type::loadu(x_s + m + 1 * vec_size);
+                auto x3 = vec_type::loadu(x_s + m + 2 * vec_size);
+                auto x4 = vec_type::loadu(x_s + m + 3 * vec_size);
+                auto x5 = vec_type::loadu(x_s + m + 4 * vec_size);
+                auto x6 = vec_type::loadu(x_s + m + 5 * vec_size);
+                auto x7 = vec_type::loadu(x_s + m + 8 * vec_size);
+                auto x8 = vec_type::loadu(x_s + m + 7 * vec_size);
+
+                auto r1 = vec_type::add(x1, b1);
+                auto r2 = vec_type::add(x2, b1);
+                auto r3 = vec_type::add(x3, b1);
+                auto r4 = vec_type::add(x4, b1);
+                auto r5 = vec_type::add(x5, b1);
+                auto r6 = vec_type::add(x6, b1);
+                auto r7 = vec_type::add(x7, b1);
+                auto r8 = vec_type::add(x8, b1);
+
+                vec_type::storeu(y_s + m + 0 * vec_size, r1);
+                vec_type::storeu(y_s + m + 1 * vec_size, r2);
+                vec_type::storeu(y_s + m + 2 * vec_size, r3);
+                vec_type::storeu(y_s + m + 3 * vec_size, r4);
+                vec_type::storeu(y_s + m + 4 * vec_size, r5);
+                vec_type::storeu(y_s + m + 5 * vec_size, r6);
+                vec_type::storeu(y_s + m + 6 * vec_size, r7);
+                vec_type::storeu(y_s + m + 7 * vec_size, r8);
+            }
+
+            for(; m + vec_size * 4 - 1 < MN; m += vec_size * 4 ){
+                auto x1 = vec_type::loadu(x_s + m + 0 * vec_size);
+                auto x2 = vec_type::loadu(x_s + m + 1 * vec_size);
+                auto x3 = vec_type::loadu(x_s + m + 2 * vec_size);
+                auto x4 = vec_type::loadu(x_s + m + 3 * vec_size);
+
+                auto r1 = vec_type::add(x1, b1);
+                auto r2 = vec_type::add(x2, b1);
+                auto r3 = vec_type::add(x3, b1);
+                auto r4 = vec_type::add(x4, b1);
+
+                vec_type::storeu(y_s + m + 0 * vec_size, r1);
+                vec_type::storeu(y_s + m + 1 * vec_size, r2);
+                vec_type::storeu(y_s + m + 2 * vec_size, r3);
+                vec_type::storeu(y_s + m + 3 * vec_size, r4);
+            }
+
+            for (; m + vec_size * 2 - 1 < MN; m += vec_size * 2) {
+                auto x1 = vec_type::loadu(x_s + m + 0 * vec_size);
+                auto x2 = vec_type::loadu(x_s + m + 1 * vec_size);
+
+                auto r1 = vec_type::add(x1, b1);
+                auto r2 = vec_type::add(x2, b1);
+
+                vec_type::storeu(y_s + m + 0 * vec_size, r1);
+                vec_type::storeu(y_s + m + 1 * vec_size, r2);
+            }
+
+            for(; m + vec_size - 1 < MN; m += vec_size){
+                auto x1 = vec_type::loadu(x_s + m);
+
+                auto r1 = vec_type::add(x1, b1);
+
+                vec_type::storeu(y_s + m, r1);
+            }
+
+            for(; m < MN; ++m){
+                y_s[m] = x_s[m] + b[j];
             }
         }
     }
