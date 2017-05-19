997ba73623911c41331ea60719dfa4644c92f539
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-05-19 14:25:55 +0200

    Dispatch stride tests

--- test/src/avg_pool_2d.cpp
+++ test/src/avg_pool_2d.cpp
@@ -98,6 +98,86 @@ TEMPLATE_TEST_CASE_2("pooling/avg2/6", "[pooling]", Z, float, double) {
     REQUIRE_EQUALS(b(1, 1, 1, 1), 13.5);
 }
 
+TEMPLATE_TEST_CASE_2("pooling/avg2/7", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<Z, 3, 3> b;
+    b = etl::avg_pool_2d<2, 2, 1, 1>(a);
+
+    REQUIRE_EQUALS(b(0, 0), 3.5);
+    REQUIRE_EQUALS(b(0, 1), 4.5);
+    REQUIRE_EQUALS(b(0, 2), 5.5);
+
+    REQUIRE_EQUALS(b(1, 0), 7.5);
+    REQUIRE_EQUALS(b(1, 1), 8.5);
+    REQUIRE_EQUALS(b(1, 2), 9.5);
+
+    REQUIRE_EQUALS(b(2, 0), 11.5);
+    REQUIRE_EQUALS(b(2, 1), 12.5);
+    REQUIRE_EQUALS(b(2, 2), 13.5);
+}
+
+TEMPLATE_TEST_CASE_2("pooling/avg2/8", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<Z, 2, 2> b;
+    b = etl::avg_pool_2d<3, 3, 1, 1>(a);
+
+    REQUIRE_EQUALS(b(0, 0), 6.0);
+    REQUIRE_EQUALS(b(0, 1), 7.0);
+
+    REQUIRE_EQUALS(b(1, 0), 10.0);
+    REQUIRE_EQUALS(b(1, 1), 11.0);
+}
+
+TEMPLATE_TEST_CASE_2("pooling/avg2/9", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<Z, 1, 1> b;
+    b = etl::avg_pool_2d<4, 4, 1, 1>(a);
+
+    REQUIRE_EQUALS(b(0, 0), 8.5);
+}
+
+TEMPLATE_TEST_CASE_2("pooling/avg2/10", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<Z, 2, 2> b;
+    b = etl::avg_pool_2d<1, 1, 2, 2>(a);
+
+    REQUIRE_EQUALS(b(0, 0), 1.0);
+    REQUIRE_EQUALS(b(0, 1), 3.0);
+
+    REQUIRE_EQUALS(b(1, 0), 9.0);
+    REQUIRE_EQUALS(b(1, 1), 11.0);
+}
+
+TEMPLATE_TEST_CASE_2("pooling/avg2/11", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 2, 2> a({1.0, 2.0, 3.0, 4.0});
+    etl::fast_matrix<Z, 2, 2> b;
+    b = etl::avg_pool_2d<2, 2, 2, 2, 1, 1>(a);
+
+    REQUIRE_EQUALS(b(0, 0), 0.25);
+    REQUIRE_EQUALS(b(0, 1), 0.5);
+
+    REQUIRE_EQUALS(b(1, 0), 0.75);
+    REQUIRE_EQUALS(b(1, 1), 1.0);
+}
+
+TEMPLATE_TEST_CASE_2("pooling/avg2/12", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 2, 2> a({1.0, 2.0, 3.0, 4.0});
+    etl::fast_matrix<Z, 3, 3> b;
+    b = etl::avg_pool_2d<2, 2, 1, 1, 1, 1>(a);
+
+    REQUIRE_EQUALS(b(0, 0), 0.25);
+    REQUIRE_EQUALS(b(0, 1), 0.75);
+    REQUIRE_EQUALS(b(0, 2), 0.5);
+
+    REQUIRE_EQUALS(b(1, 0), 1.0);
+    REQUIRE_EQUALS(b(1, 1), 2.5);
+    REQUIRE_EQUALS(b(1, 2), 1.5);
+
+    REQUIRE_EQUALS(b(2, 0), 0.75);
+    REQUIRE_EQUALS(b(2, 1), 1.75);
+    REQUIRE_EQUALS(b(2, 2), 1.0);
+}
+
 // Dynamic versions
 
 TEMPLATE_TEST_CASE_2("dyn_pooling/avg2/1", "[pooling]", Z, float, double) {
