9af9e7e80b776d99f7a43e9612726de56f23c8c6
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-05-19 14:15:41 +0200

    Complete max pooling tests

---
+++ test/include/pool_test.hpp
@@ -0,0 +1,83 @@
+//=======================================================================
+// Copyright (c) 2014-2017 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#ifdef ETL_CUDNN_MODE
+#define TEST_CUDNN
+#endif
+
+#define POOL_2D_FUNCTOR(name, ...)                                                                          \
+    struct name {                                                                                           \
+        template <size_t C1, size_t C2, size_t S1, size_t S2, size_t P1, size_t P2, typename X, typename Y> \
+        static void apply(X&& x, Y&& y) {                                                                   \
+            __VA_ARGS__;                                                                                    \
+        }                                                                                                   \
+    };
+
+#define DYN_POOL_2D_FUNCTOR(name, ...)                                                                      \
+    struct name {                                                                                           \
+        template <typename X, typename Y>                                                                   \
+        static void apply(X&& x, Y&& y, size_t c1, size_t c2, size_t s1, size_t s2, size_t p1, size_t p2) { \
+            __VA_ARGS__;                                                                                    \
+        }                                                                                                   \
+    };
+
+POOL_2D_FUNCTOR(default_mp2_valid, y = etl::max_pool_2d<C1, C2, S1, S2, P1, P2>(x))
+POOL_2D_FUNCTOR(std_mp2_valid, y = selected_helper(etl::conv_impl::STD, (etl::max_pool_2d<C1, C2, S1, S2, P1, P2>(x))))
+
+DYN_POOL_2D_FUNCTOR(default_dyn_mp2_valid, y = etl::max_pool_2d(x, c1, c2, s1, s2, p1, p2))
+DYN_POOL_2D_FUNCTOR(std_dyn_mp2_valid, y = selected_helper(etl::conv_impl::STD, (etl::max_pool_2d(x, c1, c2, s1, s2, p1, p2))))
+
+#define MP2_TEST_CASE_SECTION_DEFAULT POOL_TEST_CASE_SECTIONS(default_mp2_valid)
+#define MP2_TEST_CASE_SECTION_STD POOL_TEST_CASE_SECTIONS(std_mp2_valid)
+
+#define DYN_MP2_TEST_CASE_SECTION_DEFAULT POOL_TEST_CASE_SECTIONS(default_dyn_mp2_valid)
+#define DYN_MP2_TEST_CASE_SECTION_STD POOL_TEST_CASE_SECTIONS(std_dyn_mp2_valid)
+
+#ifdef TEST_CUDNN
+POOL_2D_FUNCTOR(cudnn_mp2_valid, y = selected_helper(etl::conv_impl::CUDNN, (etl::max_pool_2d<C1, C2, S1, S2, P1, P2>(x))))
+DYN_POOL_2D_FUNCTOR(cudnn_dyn_mp2_valid, y = selected_helper(etl::conv_impl::CUDNN, (etl::max_pool_2d(x, c1, c2, s1, s2, p1, p2))))
+
+#define MP2_TEST_CASE_SECTION_CUDNN POOL_TEST_CASE_SECTIONS(cudnn_mp2_valid)
+#define DYN_MP2_TEST_CASE_SECTION_CUDNN POOL_TEST_CASE_SECTIONS(cudnn_dyn_mp2_valid)
+#else
+#define MP2_TEST_CASE_SECTION_CUDNN
+#define DYN_MP2_TEST_CASE_SECTION_CUDNN
+#endif
+
+#define POOL_TEST_CASE_DECL(name, description)                                 \
+    template <typename T, typename Impl>                                       \
+    static void UNIQUE_NAME(____C_A_T_C_H____T_E_M_P_L_A_TE____T_E_S_T____)(); \
+    ETL_TEST_CASE(name, description)
+
+#define POOL_TEST_CASE_SECTION(Tn, Impln)                                         \
+    ETL_SECTION(#Tn "_" #Impln) {                                                 \
+        UNIQUE_NAME(____C_A_T_C_H____T_E_M_P_L_A_TE____T_E_S_T____)<Tn, Impln>(); \
+    }
+
+#define POOL_TEST_CASE_DEFN              \
+    template <typename T, typename Impl> \
+    static void UNIQUE_NAME(____C_A_T_C_H____T_E_M_P_L_A_TE____T_E_S_T____)()
+
+#define POOL_TEST_CASE_SECTIONS(S1) \
+    POOL_TEST_CASE_SECTION(float, S1)   \
+    POOL_TEST_CASE_SECTION(double, S1)
+
+#define MP2_TEST_CASE(name, description) \
+    POOL_TEST_CASE_DECL(name, description) {   \
+        MP2_TEST_CASE_SECTION_DEFAULT    \
+        MP2_TEST_CASE_SECTION_STD        \
+        MP2_TEST_CASE_SECTION_CUDNN      \
+    }                                          \
+    POOL_TEST_CASE_DEFN
+
+#define DYN_MP2_TEST_CASE(name, description) \
+    POOL_TEST_CASE_DECL(name, description) {       \
+        DYN_MP2_TEST_CASE_SECTION_DEFAULT    \
+        DYN_MP2_TEST_CASE_SECTION_STD        \
+        DYN_MP2_TEST_CASE_SECTION_CUDNN      \
+    }                                              \
+    POOL_TEST_CASE_DEFN
--- test/src/max_pool_2d.cpp
+++ test/src/max_pool_2d.cpp
@@ -6,13 +6,15 @@
 //=======================================================================
 
 #include "test.hpp"
+#include "pool_test.hpp"
 
 #include <vector>
 
-TEMPLATE_TEST_CASE_2("pooling/max2/1", "[pooling]", Z, float, double) {
-    etl::fast_matrix<Z, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
-    etl::fast_matrix<Z, 2, 2> b;
-    b = etl::max_pool_2d<2, 2>(a);
+MP2_TEST_CASE("pooling/max2/1", "[pooling]") {
+    etl::fast_matrix<T, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<T, 2, 2> b;
+
+    Impl::template apply<2, 2, 2, 2, 0, 0>(a, b);
 
     REQUIRE_EQUALS(b(0, 0), 6.0);
     REQUIRE_EQUALS(b(0, 1), 8.0);
@@ -20,35 +22,115 @@ TEMPLATE_TEST_CASE_2("pooling/max2/1", "[pooling]", Z, float, double) {
     REQUIRE_EQUALS(b(1, 1), 16.0);
 }
 
-TEMPLATE_TEST_CASE_2("pooling/max2/2", "[pooling]", Z, float, double) {
-    etl::fast_matrix<Z, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
-    etl::fast_matrix<Z, 1, 1> b;
-    b = etl::max_pool_2d<4, 4>(a);
+MP2_TEST_CASE("pooling/max2/2", "[pooling]") {
+    etl::fast_matrix<T, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<T, 1, 1> b;
+
+    Impl::template apply<4, 4, 4, 4, 0, 0>(a, b);
 
     REQUIRE_EQUALS(b(0, 0), 16.0);
 }
 
-TEMPLATE_TEST_CASE_2("pooling/max2/3", "[pooling]", Z, float, double) {
-    etl::fast_matrix<Z, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
-    etl::fast_matrix<Z, 1, 2> b;
-    b = etl::max_pool_2d<4, 2>(a);
+MP2_TEST_CASE("pooling/max2/3", "[pooling]") {
+    etl::fast_matrix<T, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<T, 1, 2> b;
+
+    Impl::template apply<4, 2, 4, 2, 0, 0>(a, b);
 
     REQUIRE_EQUALS(b(0, 0), 14.0);
     REQUIRE_EQUALS(b(0, 1), 16.0);
 }
 
-TEMPLATE_TEST_CASE_2("pooling/max2/4", "[pooling]", Z, float, double) {
-    etl::fast_matrix<Z, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
-    etl::fast_matrix<Z, 2, 1> b;
-    b = etl::max_pool_2d<2, 4>(a);
+MP2_TEST_CASE("pooling/max2/4", "[pooling]") {
+    etl::fast_matrix<T, 4, 4> a({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<T, 2, 1> b;
+
+    Impl::template apply<2, 4, 2, 4, 0, 0>(a, b);
 
     REQUIRE_EQUALS(b(0, 0), 8.0);
     REQUIRE_EQUALS(b(1, 0), 16.0);
 }
 
-TEMPLATE_TEST_CASE_2("dyn_pooling/max2/1", "[pooling]", Z, float, double) {
-    etl::dyn_matrix<Z, 2> a(4, 4, etl::values(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0));
-    etl::dyn_matrix<Z, 2> b(2, 2);
+MP2_TEST_CASE("pooling/max2/5", "[pooling]") {
+    etl::fast_matrix<T, 4, 4> A({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<T, 2, 4, 4> a;
+    etl::fast_matrix<T, 2, 2, 2> b;
+
+    a(0) = A;
+    a(1) = A;
+
+    Impl::template apply<2, 2, 2, 2, 0, 0>(a, b);
+
+    REQUIRE_EQUALS(b(0, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(0, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(0, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(0, 1, 1), 16.0);
+
+    REQUIRE_EQUALS(b(1, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(1, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(1, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(1, 1, 1), 16.0);
+}
+
+MP2_TEST_CASE("pooling/max2/6", "[pooling]") {
+    etl::fast_matrix<T, 4, 4> aa({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<T, 2, 4, 4> a;
+    a(0) = aa;
+    a(1) = aa;
+
+    etl::fast_matrix<T, 2, 2, 2> b;
+
+    Impl::template apply<2, 2, 2, 2, 0, 0>(a, b);
+
+    REQUIRE_EQUALS(b(0, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(0, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(0, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(0, 1, 1), 16.0);
+
+    REQUIRE_EQUALS(b(1, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(1, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(1, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(1, 1, 1), 16.0);
+}
+
+MP2_TEST_CASE("pooling/max2/7", "[pooling]") {
+    etl::fast_matrix<T, 4, 4> aa({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0});
+    etl::fast_matrix<T, 2, 2, 4, 4> a;
+    a(0)(0) = aa;
+    a(0)(1) = aa;
+    a(1)(0) = aa;
+    a(1)(1) = aa;
+
+    etl::fast_matrix<T, 2, 2, 2, 2> b;
+
+    Impl::template apply<2, 2, 2, 2, 0, 0>(a, b);
+
+    REQUIRE_EQUALS(b(0, 0, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(0, 0, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(0, 0, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(0, 0, 1, 1), 16.0);
+
+    REQUIRE_EQUALS(b(0, 1, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(0, 1, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(0, 1, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(0, 1, 1, 1), 16.0);
+
+    REQUIRE_EQUALS(b(1, 0, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(1, 0, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(1, 0, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(1, 0, 1, 1), 16.0);
+
+    REQUIRE_EQUALS(b(1, 1, 0, 0), 6.0);
+    REQUIRE_EQUALS(b(1, 1, 0, 1), 8.0);
+    REQUIRE_EQUALS(b(1, 1, 1, 0), 14.0);
+    REQUIRE_EQUALS(b(1, 1, 1, 1), 16.0);
+}
+
+// Dynamic version
+
+TEMPLATE_TEST_CASE_2("dyn_pooling/max2/1", "[pooling]", T, float, double) {
+    etl::dyn_matrix<T, 2> a(4, 4, etl::values(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0));
+    etl::dyn_matrix<T, 2> b(2, 2);
 
     b = etl::max_pool_2d(a, 2, 2);
 
@@ -58,18 +140,18 @@ TEMPLATE_TEST_CASE_2("dyn_pooling/max2/1", "[pooling]", Z, float, double) {
     REQUIRE_EQUALS(b(1, 1), 16.0);
 }
 
-TEMPLATE_TEST_CASE_2("dyn_pooling/max2/2", "[pooling]", Z, float, double) {
-    etl::dyn_matrix<Z, 2> a(4, 4, etl::values(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0));
-    etl::dyn_matrix<Z, 2> b(1, 1);
+TEMPLATE_TEST_CASE_2("dyn_pooling/max2/2", "[pooling]", T, float, double) {
+    etl::dyn_matrix<T, 2> a(4, 4, etl::values(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0));
+    etl::dyn_matrix<T, 2> b(1, 1);
 
     b = etl::max_pool_2d(a, 4, 4);
 
     REQUIRE_EQUALS(b(0, 0), 16.0);
 }
 
-TEMPLATE_TEST_CASE_2("dyn_pooling/max2/3", "[pooling]", Z, float, double) {
-    etl::dyn_matrix<Z, 2> a(4, 4, etl::values(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0));
-    etl::dyn_matrix<Z, 2> b(1, 2);
+TEMPLATE_TEST_CASE_2("dyn_pooling/max2/3", "[pooling]", T, float, double) {
+    etl::dyn_matrix<T, 2> a(4, 4, etl::values(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0));
+    etl::dyn_matrix<T, 2> b(1, 2);
 
     b = etl::max_pool_2d(a, 4, 2);
 
@@ -77,9 +159,9 @@ TEMPLATE_TEST_CASE_2("dyn_pooling/max2/3", "[pooling]", Z, float, double) {
     REQUIRE_EQUALS(b(0, 1), 16.0);
 }
 
-TEMPLATE_TEST_CASE_2("dyn_pooling/max2/4", "[pooling]", Z, float, double) {
-    etl::dyn_matrix<Z, 2> a(4, 4, etl::values(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0));
-    etl::dyn_matrix<Z, 2> b(2, 1);
+TEMPLATE_TEST_CASE_2("dyn_pooling/max2/4", "[pooling]", T, float, double) {
+    etl::dyn_matrix<T, 2> a(4, 4, etl::values(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0));
+    etl::dyn_matrix<T, 2> b(2, 1);
 
     b = etl::max_pool_2d(a, 2, 4);
 
