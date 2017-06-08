f8f2544c8b50863faed9ca493bc61ec330284733
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-06-08 14:47:56 +0200

    Cleanup the counters test
    This should ensure that all counters are taken into account

--- workbench/src/counters.cpp
+++ workbench/src/counters.cpp
@@ -31,90 +31,94 @@ float fake = 0;
  * ML: 139 / 10 / 19 (with activations)
  */
 
-void simple(){
-    etl::dyn_matrix<float, 2> A(4096, 4096);
-    etl::dyn_matrix<float, 2> B(4096, 4096);
-    etl::dyn_matrix<float, 2> C(4096, 4096);
-
-    A = 1e-4 >> etl::sequence_generator<float>(1.0);
-    B = 1e-4 >> etl::sequence_generator<float>(1.0);
-    C = 1e-4 >> etl::sequence_generator<float>(1.0);
-
+void simple() {
+    std::cout << "Simple" << std::endl;
     etl::reset_counters();
 
-    std::cout << "Simple" << std::endl;
+    {
+        etl::dyn_matrix<float, 2> A(4096, 4096);
+        etl::dyn_matrix<float, 2> B(4096, 4096);
+        etl::dyn_matrix<float, 2> C(4096, 4096);
 
-    for (size_t i = 0; i < 10; ++i) {
-        C = A * B;
-        fake += etl::mean(C);
+        A = 1e-4 >> etl::sequence_generator<float>(1.0);
+        B = 1e-4 >> etl::sequence_generator<float>(1.0);
+        C = 1e-4 >> etl::sequence_generator<float>(1.0);
+
+        for (size_t i = 0; i < 10; ++i) {
+            C = A * B;
+            fake += etl::mean(C);
+        }
+
+        std::cout << "   Result: " << fake << std::endl;
+        std::cout << "Should be: 2.8826e+10" << std::endl;
     }
 
     etl::dump_counters();
-
-    std::cout << "   Result: " << fake << std::endl;
-    std::cout << "Should be: 2.8826e+10" << std::endl;
 }
 
-void basic(){
-    etl::dyn_matrix<float, 2> A(4096, 4096);
-    etl::dyn_matrix<float, 2> B(4096, 4096);
-    etl::dyn_matrix<float, 2> C(4096, 4096);
-    etl::dyn_matrix<float, 2> D(4096, 4096);
-    etl::dyn_matrix<float, 2> E(4096, 4096);
-
-    A = 1e-4 >> etl::sequence_generator<float>(1.0);
-    B = 1e-4 >> etl::sequence_generator<float>(1.0);
-    C = 1e-4 >> etl::sequence_generator<float>(1.0);
-    D = 1e-4 >> etl::sequence_generator<float>(1.0);
-    E = 1e-4 >> etl::sequence_generator<float>(1.0);
-
-    etl::reset_counters();
-
+void basic() {
     std::cout << "Basic" << std::endl;
 
-    for (size_t i = 0; i < 10; ++i) {
-        IF_DEBUG std::cout << i << ":0 C = A * B * E" << std::endl;
-        C = A * B * E;
-        IF_DEBUG std::cout << i << ":1 D = A * trans(A)" << std::endl;
-        D = A * trans(A);
-        IF_DEBUG std::cout << i << ":2 D *= 1.1" << std::endl;
-        D *= 1.1;
-        IF_DEBUG std::cout << i << ":3 E = D" << std::endl;
-        E = D;
-        IF_DEBUG std::cout << i << ":4 D += C" << std::endl;
-        D += C;
-        IF_DEBUG std::cout << i << ":5 fake += etl::mean(D)" << std::endl;
-        fake += etl::mean(D);
-        IF_DEBUG std::cout << i << ":6 end" << std::endl;
+    etl::reset_counters();
+
+    {
+        etl::dyn_matrix<float, 2> A(4096, 4096);
+        etl::dyn_matrix<float, 2> B(4096, 4096);
+        etl::dyn_matrix<float, 2> C(4096, 4096);
+        etl::dyn_matrix<float, 2> D(4096, 4096);
+        etl::dyn_matrix<float, 2> E(4096, 4096);
+
+        A = 1e-4 >> etl::sequence_generator<float>(1.0);
+        B = 1e-4 >> etl::sequence_generator<float>(1.0);
+        C = 1e-4 >> etl::sequence_generator<float>(1.0);
+        D = 1e-4 >> etl::sequence_generator<float>(1.0);
+        E = 1e-4 >> etl::sequence_generator<float>(1.0);
+
+        for (size_t i = 0; i < 10; ++i) {
+            IF_DEBUG std::cout << i << ":0 C = A * B * E" << std::endl;
+            C = A * B * E;
+            IF_DEBUG std::cout << i << ":1 D = A * trans(A)" << std::endl;
+            D = A * trans(A);
+            IF_DEBUG std::cout << i << ":2 D *= 1.1" << std::endl;
+            D *= 1.1;
+            IF_DEBUG std::cout << i << ":3 E = D" << std::endl;
+            E = D;
+            IF_DEBUG std::cout << i << ":4 D += C" << std::endl;
+            D += C;
+            IF_DEBUG std::cout << i << ":5 fake += etl::mean(D)" << std::endl;
+            fake += etl::mean(D);
+            IF_DEBUG std::cout << i << ":6 end" << std::endl;
+        }
+
+        std::cout << "   Result: " << fake << std::endl;
+        std::cout << "Should be: 3.36933e+23" << std::endl;
     }
 
     etl::dump_counters();
-
-    std::cout << "   Result: " << fake << std::endl;
-    std::cout << "Should be: 3.36933e+23" << std::endl;
 }
 
-void sub(){
-    etl::dyn_matrix<float, 3> A(16, 2048, 2048);
-    etl::dyn_matrix<float, 3> B(16, 2048, 2048);
-    etl::dyn_matrix<float, 3> C(16, 2048, 2048);
-    etl::dyn_matrix<float, 3> D(16, 2048, 2048);
-
-    A = etl::normal_generator<float>(1.0, 0.0);
-    B = etl::normal_generator<float>(1.0, 0.0);
-    C = etl::normal_generator<float>(1.0, 0.0);
-    D = etl::normal_generator<float>(1.0, 0.0);
-
-    etl::reset_counters();
-
+void sub() {
     std::cout << "Sub" << std::endl;
 
-    for (size_t i = 0; i < 10; ++i) {
-        for (size_t k = 0; k < 16; ++k) {
-            C(k) = A(k) * B(k) * B(k);
-            D(k) += C(k);
-            D(k) *= 1.1;
-            fake += etl::mean(D(k));
+    etl::reset_counters();
+    {
+        etl::dyn_matrix<float, 3> A(16, 2048, 2048);
+        etl::dyn_matrix<float, 3> B(16, 2048, 2048);
+        etl::dyn_matrix<float, 3> C(16, 2048, 2048);
+        etl::dyn_matrix<float, 3> D(16, 2048, 2048);
+
+        A = etl::normal_generator<float>(1.0, 0.0);
+        B = etl::normal_generator<float>(1.0, 0.0);
+        C = etl::normal_generator<float>(1.0, 0.0);
+        D = etl::normal_generator<float>(1.0, 0.0);
+
+        for (size_t i = 0; i < 10; ++i) {
+            for (size_t k = 0; k < 16; ++k) {
+                C(k) = A(k) * B(k) * B(k);
+                D(k) += C(k);
+                D(k) *= 1.1;
+                fake += etl::mean(D(k));
+            }
         }
     }
 
@@ -122,92 +126,93 @@ void sub(){
 }
 
 // Simulate forward propagation in a neural network (with some ops as DLL)
-void ml(){
-    etl::dyn_matrix<float, 4> I(32, 3, 28, 28);
-    etl::dyn_matrix<float, 2> L(32, 10);
-
-    etl::dyn_matrix<float, 4> C1_W(16, 3, 3, 3);
-    etl::dyn_matrix<float, 1> C1_B(16);
-    etl::dyn_matrix<float, 4> C1_W_G(16, 3, 3, 3);
-    etl::dyn_matrix<float, 1> C1_B_G(16);
-    etl::dyn_matrix<float, 4> C1_O(32, 16, 28, 28);
-    etl::dyn_matrix<float, 4> C1_E(32, 16, 28, 28);
-
-    etl::dyn_matrix<float, 4> P1_O(32, 16, 14, 14);
-    etl::dyn_matrix<float, 4> P1_E(32, 16, 14, 14);
-
-    etl::dyn_matrix<float, 4> C2_W(16, 16, 3, 3);
-    etl::dyn_matrix<float, 1> C2_B(16);
-    etl::dyn_matrix<float, 4> C2_W_G(16, 16, 3, 3);
-    etl::dyn_matrix<float, 1> C2_B_G(16);
-    etl::dyn_matrix<float, 4> C2_O(32, 16, 14, 14);
-    etl::dyn_matrix<float, 4> C2_E(32, 16, 14, 14);
-
-    etl::dyn_matrix<float, 4> P2_O(32, 16, 7, 7);
-    etl::dyn_matrix<float, 4> P2_E(32, 16, 7, 7);
-
-    etl::dyn_matrix<float, 2> FC1_W(16 * 7 * 7, 500);
-    etl::dyn_matrix<float, 1> FC1_B(500);
-    etl::dyn_matrix<float, 2> FC1_W_G(16 * 7 * 7, 500);
-    etl::dyn_matrix<float, 1> FC1_B_G(500);
-    etl::dyn_matrix<float, 2> FC1_O(32, 500);
-    etl::dyn_matrix<float, 2> FC1_E(32, 500);
-
-    etl::dyn_matrix<float, 2> FC2_W(500, 10);
-    etl::dyn_matrix<float, 1> FC2_B(10);
-    etl::dyn_matrix<float, 2> FC2_W_G(500, 10);
-    etl::dyn_matrix<float, 1> FC2_B_G(10);
-    etl::dyn_matrix<float, 2> FC2_O(32, 10);
-    etl::dyn_matrix<float, 2> FC2_E(32, 10);
-
+void ml() {
+    std::cout << "ML" << std::endl;
     etl::reset_counters();
 
-    std::cout << "ML" << std::endl;
+    {
+        etl::dyn_matrix<float, 4> I(32, 3, 28, 28);
+        etl::dyn_matrix<float, 2> L(32, 10);
 
-    for (size_t i = 0; i < 10; ++i) {
-        // Forward Propagation
-        C1_O = relu(bias_add_4d(etl::ml::convolution_forward<1, 1, 1, 1>(I, C1_W), C1_B));
-        P1_O = etl::max_pool_2d<2, 2>(C1_O);
+        etl::dyn_matrix<float, 4> C1_W(16, 3, 3, 3);
+        etl::dyn_matrix<float, 1> C1_B(16);
+        etl::dyn_matrix<float, 4> C1_W_G(16, 3, 3, 3);
+        etl::dyn_matrix<float, 1> C1_B_G(16);
+        etl::dyn_matrix<float, 4> C1_O(32, 16, 28, 28);
+        etl::dyn_matrix<float, 4> C1_E(32, 16, 28, 28);
 
-        C2_O = relu(bias_add_4d(etl::ml::convolution_forward<1, 1, 1, 1>(P1_O, C2_W), C2_B));
-        P2_O = etl::max_pool_2d<2, 2>(C2_O);
+        etl::dyn_matrix<float, 4> P1_O(32, 16, 14, 14);
+        etl::dyn_matrix<float, 4> P1_E(32, 16, 14, 14);
 
-        FC1_O = sigmoid(bias_add_2d(etl::reshape<32, 16 * 7 * 7>(P2_O) * FC1_W, FC1_B));
-        FC2_O = sigmoid(bias_add_2d(FC1_O * FC2_W, FC2_B));
+        etl::dyn_matrix<float, 4> C2_W(16, 16, 3, 3);
+        etl::dyn_matrix<float, 1> C2_B(16);
+        etl::dyn_matrix<float, 4> C2_W_G(16, 16, 3, 3);
+        etl::dyn_matrix<float, 1> C2_B_G(16);
+        etl::dyn_matrix<float, 4> C2_O(32, 16, 14, 14);
+        etl::dyn_matrix<float, 4> C2_E(32, 16, 14, 14);
 
-        // Backward propagation of the errors
+        etl::dyn_matrix<float, 4> P2_O(32, 16, 7, 7);
+        etl::dyn_matrix<float, 4> P2_E(32, 16, 7, 7);
 
-        FC2_E = L - FC2_O;                                            // Errors of last layer  (!GPU)
-        FC1_E = FC2_E * trans(FC2_W);                                 // Backpropagate FC2 -> FC1
-        FC1_E = etl::ml::sigmoid_backward(FC1_O, FC1_E);              // Adapt errors of FC1
-        etl::reshape<32, 16 * 7 * 7>(P2_E) = FC1_E * trans(FC1_W);    // FC1 -> MP2
-        C2_E = etl::max_pool_upsample_2d<2, 2>(C2_O, P2_O, P2_E);     // MP2 -> C2
-        C2_E = etl::ml::relu_backward(C2_O, C2_E);                    // Adapt errors of C2
-        P1_E = etl::ml::convolution_backward<1, 1, 1, 1>(C2_E, C2_W); // C2 -> MP1
-        C1_E = etl::max_pool_upsample_2d<2, 2>(C1_O, P1_O, P1_E);     // MP1 -> C1
-        C1_E = etl::ml::relu_backward(C1_O, C1_E);                    // Adapt errors of C1
+        etl::dyn_matrix<float, 2> FC1_W(16 * 7 * 7, 500);
+        etl::dyn_matrix<float, 1> FC1_B(500);
+        etl::dyn_matrix<float, 2> FC1_W_G(16 * 7 * 7, 500);
+        etl::dyn_matrix<float, 1> FC1_B_G(500);
+        etl::dyn_matrix<float, 2> FC1_O(32, 500);
+        etl::dyn_matrix<float, 2> FC1_E(32, 500);
 
-        // Compute the gradients
+        etl::dyn_matrix<float, 2> FC2_W(500, 10);
+        etl::dyn_matrix<float, 1> FC2_B(10);
+        etl::dyn_matrix<float, 2> FC2_W_G(500, 10);
+        etl::dyn_matrix<float, 1> FC2_B_G(10);
+        etl::dyn_matrix<float, 2> FC2_O(32, 10);
+        etl::dyn_matrix<float, 2> FC2_E(32, 10);
 
-        C1_W_G  = etl::ml::convolution_backward_filter<1, 1, 1, 1>(I, C1_E);
-        C1_B_G  = bias_batch_sum_4d(C1_E);
+        for (size_t i = 0; i < 10; ++i) {
+            // Forward Propagation
+            C1_O = relu(bias_add_4d(etl::ml::convolution_forward<1, 1, 1, 1>(I, C1_W), C1_B));
+            P1_O = etl::max_pool_2d<2, 2>(C1_O);
 
-        C2_W_G  = etl::ml::convolution_backward_filter<1, 1, 1, 1>(P1_O, C2_E);
-        C2_B_G  = bias_batch_sum_4d(C2_E);
+            C2_O = relu(bias_add_4d(etl::ml::convolution_forward<1, 1, 1, 1>(P1_O, C2_W), C2_B));
+            P2_O = etl::max_pool_2d<2, 2>(C2_O);
 
-        FC1_W_G = batch_outer(etl::reshape<32, 16 * 7 * 7>(P2_O), FC1_E);
-        FC1_B_G = bias_batch_sum_2d(FC1_E);
+            FC1_O = sigmoid(bias_add_2d(etl::reshape<32, 16 * 7 * 7>(P2_O) * FC1_W, FC1_B));
+            FC2_O = sigmoid(bias_add_2d(FC1_O * FC2_W, FC2_B));
 
-        FC2_W_G = batch_outer(FC1_O, FC2_E);
-        FC2_B_G = bias_batch_sum_2d(FC2_E);
+            // Backward propagation of the errors
 
-        //TODO Apply the gradients
+            FC2_E = L - FC2_O;                                            // Errors of last layer  (!GPU)
+            FC1_E = FC2_E * trans(FC2_W);                                 // Backpropagate FC2 -> FC1
+            FC1_E = etl::ml::sigmoid_backward(FC1_O, FC1_E);              // Adapt errors of FC1
+            etl::reshape<32, 16 * 7 * 7>(P2_E) = FC1_E * trans(FC1_W);    // FC1 -> MP2
+            C2_E = etl::max_pool_upsample_2d<2, 2>(C2_O, P2_O, P2_E);     // MP2 -> C2
+            C2_E = etl::ml::relu_backward(C2_O, C2_E);                    // Adapt errors of C2
+            P1_E = etl::ml::convolution_backward<1, 1, 1, 1>(C2_E, C2_W); // C2 -> MP1
+            C1_E = etl::max_pool_upsample_2d<2, 2>(C1_O, P1_O, P1_E);     // MP1 -> C1
+            C1_E = etl::ml::relu_backward(C1_O, C1_E);                    // Adapt errors of C1
+
+            // Compute the gradients
+
+            C1_W_G = etl::ml::convolution_backward_filter<1, 1, 1, 1>(I, C1_E);
+            C1_B_G = bias_batch_sum_4d(C1_E);
+
+            C2_W_G = etl::ml::convolution_backward_filter<1, 1, 1, 1>(P1_O, C2_E);
+            C2_B_G = bias_batch_sum_4d(C2_E);
+
+            FC1_W_G = batch_outer(etl::reshape<32, 16 * 7 * 7>(P2_O), FC1_E);
+            FC1_B_G = bias_batch_sum_2d(FC1_E);
+
+            FC2_W_G = batch_outer(FC1_O, FC2_E);
+            FC2_B_G = bias_batch_sum_2d(FC2_E);
+
+            //TODO Apply the gradients
+        }
     }
 
     etl::dump_counters();
 }
 
-int main(){
+int main() {
     auto start_time = timer_clock::now();
 
     simple();
@@ -220,5 +225,5 @@ int main(){
 
     std::cout << "duration: " << duration.count() << "ms" << std::endl;
 
-    return (int) fake;
+    return (int)fake;
 }
