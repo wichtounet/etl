9198e8f37a99f2da1f1d3663289b903e7e883929
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-06-08 12:27:04 +0200

    Fix bug in check

--- include/etl/expr/unary_function_expr.hpp
+++ include/etl/expr/unary_function_expr.hpp
@@ -59,7 +59,7 @@ struct unary_function_expr : base_temporary_expr_un<unary_function_expr<A, Impl>
      */
     template <typename C, cpp_disable_if(all_fast<A,C>::value)>
     static void check(const A& a, const C& c) {
-        static constexpr etl::order order_lhs = decay_traits<A>::storage_order;
+        static constexpr etl::order order_lhs = decay_traits<C>::storage_order;
         static constexpr etl::order order_rhs = decay_traits<A>::storage_order;
 
         static_assert(order_lhs == order_rhs, "Cannot change storage order");
