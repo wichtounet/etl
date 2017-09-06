9a29f7aab6eea24ae4198c5efda25e591d1758f7
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-09-06 14:07:48 +0200

    Fix the wrapper

--- include/etl/impl/egblas/relu_der_out.hpp
+++ include/etl/impl/egblas/relu_der_out.hpp
@@ -27,8 +27,10 @@ namespace impl {
 namespace egblas {
 
 #ifdef EGBLAS_HAS_SRELU_DER_OUT
-
 static constexpr bool has_srelu_der_out = true;
+#else
+static constexpr bool has_srelu_der_out = false;
+#endif
 
 /*!
  * \brief Wrappers for single-precision egblas relu_der_out operation
@@ -40,18 +42,25 @@ static constexpr bool has_srelu_der_out = true;
  * \param ldb The leading dimension of b
  */
 inline void relu_der_out(size_t n, float* alpha, float* A , size_t lda, float* B , size_t ldb){
+#ifdef EGBLAS_HAS_SRELU_DER_OUT
     egblas_srelu_der_out(n, *alpha, A, lda, B, ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::relu_der_out");
+#endif
 }
 
-#else
-
-static constexpr bool has_srelu_der_out = false;
-
-#endif
-
 #ifdef EGBLAS_HAS_DRELU_DER_OUT
-
 static constexpr bool has_drelu_der_out = true;
+#else
+static constexpr bool has_drelu_der_out = false;
+#endif
 
 /*!
  * \brief Wrappers for double-precision egblas relu_der_out operation
@@ -63,86 +72,19 @@ static constexpr bool has_drelu_der_out = true;
  * \param ldb The leading dimension of b
  */
 inline void relu_der_out(size_t n, double* alpha, double* A , size_t lda, double* B , size_t ldb){
+#ifdef EGBLAS_HAS_DRELU_DER_OUT
     egblas_drelu_der_out(n, *alpha, A, lda, B, ldb);
-}
-
 #else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
 
-static constexpr bool has_drelu_der_out = false;
-
+    cpp_unreachable("Invalid call to egblas::relu_der_out");
 #endif
-
-#ifdef EGBLAS_HAS_CRELU_DER_OUT
-
-static constexpr bool has_crelu_der_out = true;
-
-/*!
- * \brief Wrappers for complex single-precision egblas relu_der_out operation
- * \param n The size of the vector
- * \param alpha The scaling factor alpha
- * \param A The memory of the vector a
- * \param lda The leading dimension of a
- * \param B The memory of the vector b
- * \param ldb The leading dimension of b
- */
-inline void relu_der_out(size_t n, std::complex<float>* alpha, std::complex<float>* A , size_t lda, std::complex<float>* B , size_t ldb){
-    egblas_crelu_der_out(n, *reinterpret_cast<cuComplex*>(alpha), reinterpret_cast<cuComplex*>(A), lda, reinterpret_cast<cuComplex*>(B), ldb);
 }
-
-/*!
- * \brief Wrappers for complex single-precision egblas relu_der_out operation
- * \param n The size of the vector
- * \param alpha The scaling factor alpha
- * \param A The memory of the vector a
- * \param lda The leading dimension of a
- * \param B The memory of the vector b
- * \param ldb The leading dimension of b
- */
-inline void relu_der_out(size_t n, etl::complex<float>* alpha, etl::complex<float>* A , size_t lda, etl::complex<float>* B , size_t ldb){
-    egblas_crelu_der_out(n, *reinterpret_cast<cuComplex*>(alpha), reinterpret_cast<cuComplex*>(A), lda, reinterpret_cast<cuComplex*>(B), ldb);
-}
-
-#else
-
-static constexpr bool has_crelu_der_out = false;
-
-#endif
-
-#ifdef EGBLAS_HAS_ZRELU_DER_OUT
-
-static constexpr bool has_zrelu_der_out = true;
-
-/*!
- * \brief Wrappers for complex double-precision egblas relu_der_out operation
- * \param n The size of the vector
- * \param alpha The scaling factor alpha
- * \param A The memory of the vector a
- * \param lda The leading dimension of a
- * \param B The memory of the vector b
- * \param ldb The leading dimension of b
- */
-inline void relu_der_out(size_t n, std::complex<double>* alpha, std::complex<double>* A , size_t lda, std::complex<double>* B , size_t ldb){
-    egblas_zrelu_der_out(n, *reinterpret_cast<cuDoubleComplex*>(alpha), reinterpret_cast<cuDoubleComplex*>(A), lda, reinterpret_cast<cuDoubleComplex*>(B), ldb);
-}
-
-/*!
- * \brief Wrappers for complex double-precision egblas relu_der_out operation
- * \param n The size of the vector
- * \param alpha The scaling factor alpha
- * \param A The memory of the vector a
- * \param lda The leading dimension of a
- * \param B The memory of the vector b
- * \param ldb The leading dimension of b
- */
-inline void relu_der_out(size_t n, etl::complex<double>* alpha, etl::complex<double>* A , size_t lda, etl::complex<double>* B , size_t ldb){
-    egblas_zrelu_der_out(n, *reinterpret_cast<cuDoubleComplex*>(alpha), reinterpret_cast<cuDoubleComplex*>(A), lda, reinterpret_cast<cuDoubleComplex*>(B), ldb);
-}
-
-#else
-
-static constexpr bool has_zrelu_der_out = false;
-
-#endif
 
 } //end of namespace egblas
 } //end of namespace impl
