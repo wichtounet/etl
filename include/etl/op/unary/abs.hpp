dcbca3ccbcc7e288e556acf7f2790c88eb2fd4d9
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-09-25 22:17:36 +0200

    Split unary operators

---
+++ include/etl/op/unary/abs.hpp
@@ -0,0 +1,70 @@
+//=======================================================================
+// Copyright (c) 2014-2017 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#pragma once
+
+namespace etl {
+
+/*!
+ * \brief Unary operation taking the absolute value
+ * \tparam T The type of value
+ */
+template <typename T>
+struct abs_unary_op {
+    static constexpr bool linear      = true; ///< Indicates if the operator is linear
+    static constexpr bool thread_safe = true; ///< Indicates if the operator is thread safe or not
+
+    /*!
+     * \brief Indicates if the expression is vectorizable using the
+     * given vector mode
+     * \tparam V The vector mode
+     */
+    template <vector_mode_t V>
+    static constexpr bool vectorizable = !is_complex_t<T>;
+
+    /*!
+     * \brief Indicates if the operator can be computed on GPU
+     */
+    template <typename E>
+    static constexpr bool gpu_computable = false;
+
+    /*!
+     * The vectorization type for V
+     */
+    template <typename V = default_vec>
+    using vec_type       = typename V::template vec_type<T>;
+
+    /*!
+     * \brief Apply the unary operator on x
+     * \param x The value on which to apply the operator
+     * \return The result of applying the unary operator on x
+     */
+    static constexpr T apply(const T& x) noexcept {
+        return std::abs(x);
+    }
+
+    /*!
+     * \brief Compute several applications of the operator at a time
+     * \param x The vector on which to operate
+     * \tparam V The vectorization mode
+     * \return a vector containing several results of the operator
+     */
+    template <typename V = default_vec>
+    static vec_type<V> load(const vec_type<V>& x) noexcept {
+        return V::max(x, V::sub(V::template zero<T>(), x));
+    }
+
+    /*!
+     * \brief Returns a textual representation of the operator
+     * \return a string representing the operator
+     */
+    static std::string desc() noexcept {
+        return "abs";
+    }
+};
+
+} //end of namespace etl
