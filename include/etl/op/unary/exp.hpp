9d3ee99dd9235963858b2299ad5ddd8f5eed0bba
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-09-26 18:05:27 +0200

    Split unary operations

---
+++ include/etl/op/unary/exp.hpp
@@ -0,0 +1,116 @@
+//=======================================================================
+// Copyright (c) 2014-2017 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#pragma once
+
+#include "etl/impl/egblas/exp.hpp"
+
+namespace etl {
+
+/*!
+ * \brief Unary operation computing the exponential
+ * \tparam T The type of value
+ */
+template <typename T>
+struct exp_unary_op {
+    /*!
+     * The vectorization type for V
+     */
+    template <typename V = default_vec>
+    using vec_type       = typename V::template vec_type<T>;
+
+    static constexpr bool linear = true; ///< Indicates if the operator is linear
+    static constexpr bool thread_safe = true;  ///< Indicates if the operator is thread safe or not
+
+    /*!
+     * \brief Indicates if the expression is vectorizable using the
+     * given vector mode
+     * \tparam V The vector mode
+     */
+    template <vector_mode_t V>
+    static constexpr bool vectorizable =
+            (V == vector_mode_t::SSE3 && !is_complex_t<T>)
+        ||  (V == vector_mode_t::AVX && !is_complex_t<T>)
+        ||  (intel_compiler && !is_complex_t<T>);
+
+    /*!
+     * \brief Indicates if the operator can be computed on GPU
+     */
+    template <typename E>
+    static constexpr bool gpu_computable =
+               (is_single_precision_t<T> && impl::egblas::has_sexp)
+            || (is_double_precision_t<T> && impl::egblas::has_dexp)
+            || (is_complex_single_t<T> && impl::egblas::has_cexp)
+            || (is_complex_double_t<T> && impl::egblas::has_zexp);
+
+    /*!
+     * \brief Apply the unary operator on x
+     * \param x The value on which to apply the operator
+     * \return The result of applying the unary operator on x
+     */
+    static constexpr T apply(const T& x) {
+        return std::exp(x);
+    }
+
+    /*!
+     * \brief Compute several applications of the operator at a time
+     * \param x The vector on which to operate
+     * \tparam V The vectorization mode
+     * \return a vector containing several results of the operator
+     */
+    template <typename V = default_vec>
+    static vec_type<V> load(const vec_type<V>& x) noexcept {
+        return V::exp(x);
+    }
+
+    /*!
+     * \brief Compute the result of the operation using the GPU
+     *
+     * \param x The expression of the unary operation
+     *
+     * \return The result of applying the unary operator on x. The result must be a GPU computed expression.
+     */
+    template <typename X>
+    static auto gpu_compute(const X& x) noexcept {
+        decltype(auto) t1 = smart_gpu_compute(x);
+
+        auto t2 = force_temporary_gpu_dim_only(t1);
+
+        T alpha(1.0);
+        impl::egblas::exp(etl::size(x), &alpha, t1.gpu_memory(), 1, t2.gpu_memory(), 1);
+
+        return t2;
+    }
+    /*!
+     * \brief Compute the result of the operation using the GPU
+     *
+     * \param x The expression of the unary operation
+     * \param y The expression into which to store the reuslt
+     */
+    template <typename X, typename Y>
+    static Y& gpu_compute(const X& x, Y& y) noexcept {
+        decltype(auto) t1 = select_smart_gpu_compute(x, y);
+
+        T alpha(1.0);
+        impl::egblas::exp(etl::size(x), &alpha, t1.gpu_memory(), 1, y.gpu_memory(), 1);
+
+        y.validate_gpu();
+        y.invalidate_cpu();
+
+        return y;
+    }
+
+    /*!
+     * \brief Returns a textual representation of the operator
+     * \return a string representing the operator
+     */
+    static std::string desc() noexcept {
+        return "exp";
+    }
+};
+
+} //end of namespace etl
--- include/etl/op/unary_op.hpp
+++ include/etl/op/unary_op.hpp
@@ -23,7 +23,6 @@
 #include "etl/math.hpp"
 #include "etl/impl/cudnn/sigmoid.hpp"
 #include "etl/impl/egblas/sqrt.hpp"
-#include "etl/impl/egblas/exp.hpp"
 #include "etl/impl/egblas/relu_der_out.hpp"
 
 #include "etl/op/unary/minus.hpp"
@@ -44,110 +43,10 @@
 #include "etl/op/unary/tanh.hpp"
 #include "etl/op/unary/sinh.hpp"
 #include "etl/op/unary/cosh.hpp"
+#include "etl/op/unary/exp.hpp"
+#include "etl/op/unary/sigmoid.hpp"
 
 namespace etl {
-
-/*!
- * \brief Unary operation computing the exponential
- * \tparam T The type of value
- */
-template <typename T>
-struct exp_unary_op {
-    /*!
-     * The vectorization type for V
-     */
-    template <typename V = default_vec>
-    using vec_type       = typename V::template vec_type<T>;
-
-    static constexpr bool linear = true; ///< Indicates if the operator is linear
-    static constexpr bool thread_safe = true;  ///< Indicates if the operator is thread safe or not
-
-    /*!
-     * \brief Indicates if the expression is vectorizable using the
-     * given vector mode
-     * \tparam V The vector mode
-     */
-    template <vector_mode_t V>
-    static constexpr bool vectorizable =
-            (V == vector_mode_t::SSE3 && !is_complex_t<T>)
-        ||  (V == vector_mode_t::AVX && !is_complex_t<T>)
-        ||  (intel_compiler && !is_complex_t<T>);
-
-    /*!
-     * \brief Indicates if the operator can be computed on GPU
-     */
-    template <typename E>
-    static constexpr bool gpu_computable =
-               (is_single_precision_t<T> && impl::egblas::has_sexp)
-            || (is_double_precision_t<T> && impl::egblas::has_dexp)
-            || (is_complex_single_t<T> && impl::egblas::has_cexp)
-            || (is_complex_double_t<T> && impl::egblas::has_zexp);
-
-    /*!
-     * \brief Apply the unary operator on x
-     * \param x The value on which to apply the operator
-     * \return The result of applying the unary operator on x
-     */
-    static constexpr T apply(const T& x) {
-        return std::exp(x);
-    }
-
-    /*!
-     * \brief Compute several applications of the operator at a time
-     * \param x The vector on which to operate
-     * \tparam V The vectorization mode
-     * \return a vector containing several results of the operator
-     */
-    template <typename V = default_vec>
-    static vec_type<V> load(const vec_type<V>& x) noexcept {
-        return V::exp(x);
-    }
-
-    /*!
-     * \brief Compute the result of the operation using the GPU
-     *
-     * \param x The expression of the unary operation
-     *
-     * \return The result of applying the unary operator on x. The result must be a GPU computed expression.
-     */
-    template <typename X>
-    static auto gpu_compute(const X& x) noexcept {
-        decltype(auto) t1 = smart_gpu_compute(x);
-
-        auto t2 = force_temporary_gpu_dim_only(t1);
-
-        T alpha(1.0);
-        impl::egblas::exp(etl::size(x), &alpha, t1.gpu_memory(), 1, t2.gpu_memory(), 1);
-
-        return t2;
-    }
-    /*!
-     * \brief Compute the result of the operation using the GPU
-     *
-     * \param x The expression of the unary operation
-     * \param y The expression into which to store the reuslt
-     */
-    template <typename X, typename Y>
-    static Y& gpu_compute(const X& x, Y& y) noexcept {
-        decltype(auto) t1 = select_smart_gpu_compute(x, y);
-
-        T alpha(1.0);
-        impl::egblas::exp(etl::size(x), &alpha, t1.gpu_memory(), 1, y.gpu_memory(), 1);
-
-        y.validate_gpu();
-        y.invalidate_cpu();
-
-        return y;
-    }
-
-    /*!
-     * \brief Returns a textual representation of the operator
-     * \return a string representing the operator
-     */
-    static std::string desc() noexcept {
-        return "exp";
-    }
-};
 
 /*!
  * \brief Unary operation computing the sign
@@ -187,102 +86,6 @@ struct sign_unary_op {
      */
     static std::string desc() noexcept {
         return "sign";
-    }
-};
-
-/*!
- * \brief Unary operation computing the logistic sigmoid
- * \tparam T The type of value
- */
-template <typename T>
-struct sigmoid_unary_op {
-    static constexpr bool linear = true; ///< Indicates if the operator is linear
-    static constexpr bool thread_safe = true;  ///< Indicates if the operator is thread safe or not
-
-    /*!
-     * \brief Indicates if the expression is vectorizable using the
-     * given vector mode
-     * \tparam V The vector mode
-     */
-    template <vector_mode_t V>
-    static constexpr bool vectorizable =
-            (V == vector_mode_t::SSE3 && !is_complex_t<T>)
-        ||  (V == vector_mode_t::AVX && !is_complex_t<T>)
-        ||  (intel_compiler && !is_complex_t<T>);
-
-    /*!
-     * \brief Indicates if the operator can be computed on GPU
-     */
-    template <typename E>
-    static constexpr bool gpu_computable = is_floating<E> && cudnn_enabled;
-
-    /*!
-     * The vectorization type for V
-     */
-    template <typename V = default_vec>
-    using vec_type       = typename V::template vec_type<T>;
-
-    /*!
-     * \brief Apply the unary operator on x
-     * \param x The value on which to apply the operator
-     * \return The result of applying the unary operator on x
-     */
-    static constexpr T apply(const T& x) {
-        return math::logistic_sigmoid(x);
-    }
-
-    /*!
-     * \brief Compute several applications of the operator at a time
-     * \param x The vector on which to operate
-     * \tparam V The vectorization mode
-     * \return a vector containing several results of the operator
-     */
-    template <typename V = default_vec>
-    static vec_type<V> load(const vec_type<V>& x) noexcept {
-        auto one = V::set(T(1));
-
-        auto t1 = V::minus(x);
-        auto t2 = V::exp(t1);
-        auto t3 = V::add(one, t2);
-        return V::div(one, t3);
-    }
-
-    /*!
-     * \brief Compute the result of the operation using the GPU
-     *
-     * \param x The expression of the unary operation
-     *
-     * \return The result of applying the unary operator on x. The result must be a GPU computed expression.
-     */
-    template <typename X>
-    static auto gpu_compute(const X& x) noexcept {
-        decltype(auto) t1 = smart_gpu_compute(x);
-
-        auto t2 = force_temporary_gpu_dim_only(t1);
-        impl::cudnn::sigmoid(t1, t2);
-        return t2;
-    }
-    /*!
-     * \brief Compute the result of the operation using the GPU
-     *
-     * \param x The expression of the unary operation
-     * \param y The expression into which to store the reuslt
-     */
-    template <typename X, typename Y>
-    static Y& gpu_compute(const X& x, Y& y) noexcept {
-        decltype(auto) t1 = select_smart_gpu_compute(x, y);
-
-        impl::cudnn::sigmoid(t1, y);
-
-        return y;
-    }
-
-    /*!
-     * \brief Returns a textual representation of the operator
-     * \return a string representing the operator
-     */
-    static std::string desc() noexcept {
-        return "sigmoid";
     }
 };
 
