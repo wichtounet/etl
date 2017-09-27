34c582fcf23d6bd8d6b8003558409e0ff6b2d9eb
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-09-27 21:16:36 +0200

    GPU support for abs

---
+++ include/etl/impl/egblas/abs.hpp
@@ -0,0 +1,211 @@
+//=======================================================================
+// Copyright (c) 2014-2017 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+/*!
+ * \file
+ * \brief EGBLAS wrappers for the abs operation.
+ */
+
+#pragma once
+
+#ifdef ETL_EGBLAS_MODE
+
+#include "etl/impl/cublas/cuda.hpp"
+
+#include <egblas.hpp>
+
+#endif
+
+namespace etl {
+
+namespace impl {
+
+namespace egblas {
+
+/*!
+ * \brief Indicates if EGBLAS has single-precision absarithm.
+ */
+#ifdef EGBLAS_HAS_SABS
+static constexpr bool has_sabs = true;
+#else
+static constexpr bool has_sabs = false;
+#endif
+
+/*!
+ * \brief Wrappers for single-precision egblas abs operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void abs(size_t n, float* alpha, float* A , size_t lda, float* B , size_t ldb){
+#ifdef EGBLAS_HAS_SABS
+    egblas_sabs(n, *alpha, A, lda, B, ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::abs");
+#endif
+}
+
+/*!
+ * \brief Indicates if EGBLAS has double-precision absarithm.
+ */
+#ifdef EGBLAS_HAS_DABS
+static constexpr bool has_dabs = true;
+#else
+static constexpr bool has_dabs = false;
+#endif
+
+/*!
+ * \brief Wrappers for double-precision egblas abs operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void abs(size_t n, double* alpha, double* A , size_t lda, double* B , size_t ldb){
+#ifdef EGBLAS_HAS_DABS
+    egblas_dabs(n, *alpha, A, lda, B, ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::abs");
+#endif
+}
+
+/*!
+ * \brief Indicates if EGBLAS has complex single-precision absarithm.
+ */
+#ifdef EGBLAS_HAS_CABS
+static constexpr bool has_cabs = true;
+#else
+static constexpr bool has_cabs = false;
+#endif
+
+/*!
+ * \brief Wrappers for complex single-precision egblas abs operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void abs(size_t n, float* alpha, std::complex<float>* A , size_t lda, float* B , size_t ldb){
+#ifdef EGBLAS_HAS_CABS
+    egblas_cabs(n, *(alpha), reinterpret_cast<cuComplex*>(A), lda, (B), ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::abs");
+#endif
+}
+
+/*!
+ * \brief Wrappers for complex single-precision egblas abs operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void abs(size_t n, float* alpha, etl::complex<float>* A , size_t lda, float* B , size_t ldb){
+#ifdef EGBLAS_HAS_CABS
+    egblas_cabs(n, *(alpha), reinterpret_cast<cuComplex*>(A), lda, (B), ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::abs");
+#endif
+}
+
+/*!
+ * \brief Indicates if EGBLAS has complex double-precision absarithm.
+ */
+#ifdef EGBLAS_HAS_ZABS
+static constexpr bool has_zabs = true;
+#else
+static constexpr bool has_zabs = false;
+#endif
+
+/*!
+ * \brief Wrappers for complex double-precision egblas abs operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void abs(size_t n, double* alpha, std::complex<double>* A , size_t lda, double* B , size_t ldb){
+#ifdef EGBLAS_HAS_ZABS
+    egblas_zabs(n, *(alpha), reinterpret_cast<cuDoubleComplex*>(A), lda, (B), ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::abs");
+#endif
+}
+
+/*!
+ * \brief Wrappers for complex double-precision egblas abs operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void abs(size_t n, double* alpha, etl::complex<double>* A , size_t lda, double* B , size_t ldb){
+#ifdef EGBLAS_HAS_ZABS
+    egblas_zabs(n, *(alpha), reinterpret_cast<cuDoubleComplex*>(A), lda, (B), ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::abs");
+#endif
+}
+
+} //end of namespace egblas
+} //end of namespace impl
+} //end of namespace etl
--- include/etl/op/unary/abs.hpp
+++ include/etl/op/unary/abs.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include "etl/impl/egblas/abs.hpp"
+
 namespace etl {
 
 /*!
@@ -30,7 +32,11 @@ struct abs_unary_op {
      * \brief Indicates if the operator can be computed on GPU
      */
     template <typename E>
-    static constexpr bool gpu_computable = false;
+    static constexpr bool gpu_computable =
+               (is_single_precision_t<T> && impl::egblas::has_sabs)
+            || (is_double_precision_t<T> && impl::egblas::has_dabs)
+            || (is_complex_single_t<T> && impl::egblas::has_cabs)
+            || (is_complex_double_t<T> && impl::egblas::has_zabs);
 
     /*!
      * The vectorization type for V
@@ -56,6 +62,44 @@ struct abs_unary_op {
     template <typename V = default_vec>
     static vec_type<V> load(const vec_type<V>& x) noexcept {
         return V::max(x, V::sub(V::template zero<T>(), x));
+    }
+
+    /*!
+     * \brief Compute the result of the operation using the GPU
+     *
+     * \param x The expression of the unary operation
+     *
+     * \return The result of applying the unary operator on x. The result must be a GPU computed expression.
+     */
+    template <typename X>
+    static auto gpu_compute(const X& x) noexcept {
+        decltype(auto) t1 = smart_gpu_compute(x);
+
+        auto t2 = force_temporary_gpu_dim_only(t1);
+
+        T alpha(1.0);
+        impl::egblas::abs(etl::size(x), &alpha, t1.gpu_memory(), 1, t2.gpu_memory(), 1);
+
+        return t2;
+    }
+
+    /*!
+     * \brief Compute the result of the operation using the GPU
+     *
+     * \param x The expression of the unary operation
+     * \param y The expression into which to store the reuslt
+     */
+    template <typename X, typename Y>
+    static Y& gpu_compute(const X& x, Y& y) noexcept {
+        decltype(auto) t1 = select_smart_gpu_compute(x, y);
+
+        T alpha(1.0);
+        impl::egblas::abs(etl::size(x), &alpha, t1.gpu_memory(), 1, y.gpu_memory(), 1);
+
+        y.validate_gpu();
+        y.invalidate_cpu();
+
+        return y;
     }
 
     /*!
