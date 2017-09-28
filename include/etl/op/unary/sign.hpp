f1da4b4aa7e3531b436ca5b8fc374bdcb508d86a
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-09-28 11:15:05 +0200

    GPU support for signum

--- include/etl/op/unary/sign.hpp
+++ include/etl/op/unary/sign.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include "etl/impl/egblas/sign.hpp"
+
 namespace etl {
 
 /*!
@@ -15,8 +17,8 @@ namespace etl {
  */
 template <typename T>
 struct sign_unary_op {
-    static constexpr bool linear = true; ///< Indicates if the operator is linear
-    static constexpr bool thread_safe = true;  ///< Indicates if the operator is thread safe or not
+    static constexpr bool linear      = true; ///< Indicates if the operator is linear
+    static constexpr bool thread_safe = true; ///< Indicates if the operator is thread safe or not
 
     /*!
      * \brief Indicates if the expression is vectorizable using the
@@ -30,7 +32,12 @@ struct sign_unary_op {
      * \brief Indicates if the operator can be computed on GPU
      */
     template <typename E>
-    static constexpr bool gpu_computable = false;
+    static constexpr bool gpu_computable =
+               (is_single_precision_t<T> && impl::egblas::has_ssign)
+            || (is_double_precision_t<T> && impl::egblas::has_dsign)
+            || (is_complex_single_t<T> && impl::egblas::has_csign)
+            || (is_complex_double_t<T> && impl::egblas::has_zsign);
+
 
     /*!
      * \brief Apply the unary operator on x
@@ -39,6 +46,44 @@ struct sign_unary_op {
      */
     static constexpr T apply(const T& x) noexcept {
         return math::sign(x);
+    }
+
+    /*!
+     * \brief Compute the result of the operation using the GPU
+     *
+     * \param x The expression of the unary operation
+     *
+     * \return The result of applying the unary operator on x. The result must be a GPU computed expression.
+     */
+    template <typename X>
+    static auto gpu_compute(const X& x) noexcept {
+        decltype(auto) t1 = smart_gpu_compute(x);
+
+        auto t2 = force_temporary_gpu_dim_only(t1);
+
+        T alpha(1.0);
+        impl::egblas::sign(etl::size(x), &alpha, t1.gpu_memory(), 1, t2.gpu_memory(), 1);
+
+        return t2;
+    }
+
+    /*!
+     * \brief Compute the result of the operation using the GPU
+     *
+     * \param x The expression of the unary operation
+     * \param y The expression into which to store the reuslt
+     */
+    template <typename X, typename Y>
+    static Y& gpu_compute(const X& x, Y& y) noexcept {
+        decltype(auto) t1 = select_smart_gpu_compute(x, y);
+
+        T alpha(1.0);
+        impl::egblas::sign(etl::size(x), &alpha, t1.gpu_memory(), 1, y.gpu_memory(), 1);
+
+        y.validate_gpu();
+        y.invalidate_cpu();
+
+        return y;
     }
 
     /*!
