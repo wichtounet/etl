96d2b69dcd04aa7083d9b4c284116345694dee14
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-09-29 21:52:59 +0200

    GPU support for binary min/max

--- include/etl/op/binary/min.hpp
+++ include/etl/op/binary/min.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include "etl/impl/egblas/min.hpp"
+
 namespace etl {
 
 /*!
@@ -30,7 +32,11 @@ struct min_binary_op {
      * \brief Indicates if the operator can be computed on GPU
      */
     template<typename L, typename R>
-    static constexpr bool gpu_computable = false;
+    static constexpr bool gpu_computable =
+               (all_single_precision<L,R> && impl::egblas::has_smin)
+            || (all_double_precision<L,R> && impl::egblas::has_dmin)
+            || (all_complex_single_precision<L,R> && impl::egblas::has_cmin)
+            || (all_complex_double_precision<L,R> && impl::egblas::has_zmin);
 
     /*!
      * The vectorization type for V
@@ -59,6 +65,46 @@ struct min_binary_op {
     template <typename V = default_vec>
     static vec_type<V> load(const vec_type<V>& lhs, const vec_type<V>& rhs) noexcept {
         return V::min(lhs, rhs);
+    }
+
+    /*!
+     * \brief Compute the result of the operation using the GPU
+     *
+     * \param x The expression of the unary operation
+     *
+     * \return The result of applying the unary operator on x. The result must be a GPU computed expression.
+     */
+    template <typename X, typename Y>
+    static auto gpu_compute(const X& x, const Y& y) noexcept {
+        decltype(auto) t1 = smart_gpu_compute(x);
+        decltype(auto) t2 = smart_gpu_compute(y);
+
+        auto t3 = force_temporary_gpu(t1);
+
+        T alpha(1);
+        impl::egblas::min(etl::size(x), &alpha, t2.gpu_memory(), 1, t3.gpu_memory(), 1);
+
+        return t3;
+    }
+
+    /*!
+     * \brief Compute the result of the operation using the GPU
+     *
+     * \param x The expression of the unary operation
+     * \param y The expression into which to store the reuslt
+     */
+    template <typename X, typename Y, typename YY>
+    static YY& gpu_compute(const X& x, const Y& y, YY& yy) noexcept {
+        decltype(auto) t1 = smart_gpu_compute(x);
+        smart_gpu_compute(y, yy);
+
+        T alpha(1);
+        impl::egblas::min(etl::size(x), &alpha, t1.gpu_memory(), 1, yy.gpu_memory(), 1);
+
+        yy.validate_gpu();
+        yy.invalidate_cpu();
+
+        return yy;
     }
 
     /*!
