73798f716473a41e13c53ceef025b2ec5719de22
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-10-17 12:23:20 +0200

    New set of tests

--- test/src/embedding_lookup.cpp
+++ test/src/embedding_lookup.cpp
@@ -76,3 +76,60 @@ TEMPLATE_TEST_CASE_2("embedding_lookup/1", "[embedding_lookup]", T, float, doubl
     REQUIRE_EQUALS(c(5, 1), T(1.1) + T(2));
     REQUIRE_EQUALS(c(5, 2), T(1.1) + T(3));
 }
+
+TEMPLATE_TEST_CASE_2("batch_embedding_lookup/0", "[batch_embedding_lookup]", T, float, double) {
+    etl::fast_matrix<T, 2, 6> a({1, 2, 3, 2, 6, 0, 1, 3, 2, 2, 6, 0});
+    etl::fast_matrix<T, 8, 3> b{ 1, 2, 3,  4, 5, 6,  0.1, 0.2, 0.3,  -1.0, -1.0, 0.0,  1, 1, 1,  2, 2, 2,  0, 0, 0,  2, 1, 3 };
+
+    etl::fast_matrix<T, 2, 6, 3> c;
+
+    c = batch_embedding_lookup(a, b);
+
+    REQUIRE_EQUALS(c(0, 0, 0), T(4));
+    REQUIRE_EQUALS(c(0, 0, 1), T(5));
+    REQUIRE_EQUALS(c(0, 0, 2), T(6));
+
+    REQUIRE_EQUALS(c(0, 1, 0), T(0.1));
+    REQUIRE_EQUALS(c(0, 1, 1), T(0.2));
+    REQUIRE_EQUALS(c(0, 1, 2), T(0.3));
+
+    REQUIRE_EQUALS(c(0, 2, 0), T(-1.0));
+    REQUIRE_EQUALS(c(0, 2, 1), T(-1.0));
+    REQUIRE_EQUALS(c(0, 2, 2), T(0.0));
+
+    REQUIRE_EQUALS(c(0, 3, 0), T(0.1));
+    REQUIRE_EQUALS(c(0, 3, 1), T(0.2));
+    REQUIRE_EQUALS(c(0, 3, 2), T(0.3));
+
+    REQUIRE_EQUALS(c(0, 4, 0), T(0));
+    REQUIRE_EQUALS(c(0, 4, 1), T(0));
+    REQUIRE_EQUALS(c(0, 4, 2), T(0));
+
+    REQUIRE_EQUALS(c(0, 5, 0), T(1));
+    REQUIRE_EQUALS(c(0, 5, 1), T(2));
+    REQUIRE_EQUALS(c(0, 5, 2), T(3));
+
+    REQUIRE_EQUALS(c(1, 0, 0), T(4));
+    REQUIRE_EQUALS(c(1, 0, 1), T(5));
+    REQUIRE_EQUALS(c(1, 0, 2), T(6));
+
+    REQUIRE_EQUALS(c(1, 1, 0), T(-1.0));
+    REQUIRE_EQUALS(c(1, 1, 1), T(-1.0));
+    REQUIRE_EQUALS(c(1, 1, 2), T(0.0));
+
+    REQUIRE_EQUALS(c(1, 2, 0), T(0.1));
+    REQUIRE_EQUALS(c(1, 2, 1), T(0.2));
+    REQUIRE_EQUALS(c(1, 2, 2), T(0.3));
+
+    REQUIRE_EQUALS(c(1, 3, 0), T(0.1));
+    REQUIRE_EQUALS(c(1, 3, 1), T(0.2));
+    REQUIRE_EQUALS(c(1, 3, 2), T(0.3));
+
+    REQUIRE_EQUALS(c(1, 4, 0), T(0));
+    REQUIRE_EQUALS(c(1, 4, 1), T(0));
+    REQUIRE_EQUALS(c(1, 4, 2), T(0));
+
+    REQUIRE_EQUALS(c(1, 5, 0), T(1));
+    REQUIRE_EQUALS(c(1, 5, 1), T(2));
+    REQUIRE_EQUALS(c(1, 5, 2), T(3));
+}
