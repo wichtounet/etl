bdf6843bde08eb0cf3cb344ae273dfe72e4db4c5
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-11-23 15:22:11 +0100

    Wrappers for axpy

---
+++ include/etl/impl/egblas/axpy.hpp
@@ -0,0 +1,200 @@
+//=======================================================================
+// Copyright (c) 2014-2017 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+/*!
+ * \file
+ * \brief EGBLAS wrappers for the axpy operation.
+ */
+
+#pragma once
+
+#ifdef ETL_EGBLAS_MODE
+
+#include "etl/impl/cublas/cuda.hpp"
+
+#include <egblas.hpp>
+
+#endif
+
+namespace etl {
+
+namespace impl {
+
+namespace egblas {
+
+#ifdef EGBLAS_HAS_SAXPY
+static constexpr bool has_saxpy = true;
+#else
+static constexpr bool has_saxpy = false;
+#endif
+
+/*!
+ * \brief Wrappers for single-precision egblas axpy operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void axpy(size_t n, float* alpha, float* A , size_t lda, float* B , size_t ldb){
+#ifdef EGBLAS_HAS_SAXPY
+    egblas_saxpy(n, *alpha, A, lda, B, ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::axpy");
+#endif
+}
+
+#ifdef EGBLAS_HAS_DAXPY
+static constexpr bool has_daxpy = true;
+#else
+static constexpr bool has_daxpy = false;
+#endif
+
+/*!
+ * \brief Wrappers for double-precision egblas axpy operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void axpy(size_t n, double* alpha, double* A , size_t lda, double* B , size_t ldb){
+#ifdef EGBLAS_HAS_DAXPY
+    egblas_daxpy(n, *alpha, A, lda, B, ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::axpy");
+#endif
+}
+
+#ifdef EGBLAS_HAS_CAXPY
+static constexpr bool has_caxpy = true;
+#else
+static constexpr bool has_caxpy = false;
+#endif
+
+/*!
+ * \brief Wrappers for complex single-precision egblas axpy operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void axpy(size_t n, std::complex<float>* alpha, std::complex<float>* A , size_t lda, std::complex<float>* B , size_t ldb){
+#ifdef EGBLAS_HAS_CAXPY
+    egblas_caxpy(n, *reinterpret_cast<cuComplex*>(alpha), reinterpret_cast<cuComplex*>(A), lda, reinterpret_cast<cuComplex*>(B), ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::axpy");
+#endif
+}
+
+/*!
+ * \brief Wrappers for complex single-precision egblas axpy operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void axpy(size_t n, etl::complex<float>* alpha, etl::complex<float>* A , size_t lda, etl::complex<float>* B , size_t ldb){
+#ifdef EGBLAS_HAS_CAXPY
+    egblas_caxpy(n, *reinterpret_cast<cuComplex*>(alpha), reinterpret_cast<cuComplex*>(A), lda, reinterpret_cast<cuComplex*>(B), ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::axpy");
+#endif
+}
+
+
+#ifdef EGBLAS_HAS_ZAXPY
+static constexpr bool has_zaxpy = true;
+#else
+static constexpr bool has_zaxpy = false;
+#endif
+
+/*!
+ * \brief Wrappers for complex double-precision egblas axpy operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void axpy(size_t n, std::complex<double>* alpha, std::complex<double>* A , size_t lda, std::complex<double>* B , size_t ldb){
+#ifdef EGBLAS_HAS_ZAXPY
+    egblas_zaxpy(n, *reinterpret_cast<cuDoubleComplex*>(alpha), reinterpret_cast<cuDoubleComplex*>(A), lda, reinterpret_cast<cuDoubleComplex*>(B), ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::axpy");
+#endif
+}
+
+/*!
+ * \brief Wrappers for complex double-precision egblas axpy operation
+ * \param n The size of the vector
+ * \param alpha The scaling factor alpha
+ * \param A The memory of the vector a
+ * \param lda The leading dimension of a
+ * \param B The memory of the vector b
+ * \param ldb The leading dimension of b
+ */
+inline void axpy(size_t n, etl::complex<double>* alpha, etl::complex<double>* A , size_t lda, etl::complex<double>* B , size_t ldb){
+#ifdef EGBLAS_HAS_ZAXPY
+    egblas_zaxpy(n, *reinterpret_cast<cuDoubleComplex*>(alpha), reinterpret_cast<cuDoubleComplex*>(A), lda, reinterpret_cast<cuDoubleComplex*>(B), ldb);
+#else
+    cpp_unused(n);
+    cpp_unused(alpha);
+    cpp_unused(A);
+    cpp_unused(lda);
+    cpp_unused(B);
+    cpp_unused(ldb);
+
+    cpp_unreachable("Invalid call to egblas::axpy");
+#endif
+}
+
+} //end of namespace egblas
+} //end of namespace impl
+} //end of namespace etl
