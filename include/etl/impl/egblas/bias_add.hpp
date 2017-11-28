c8f7f693686a231f8167fccd9b562a1b80af76be
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-11-28 20:51:12 +0100

    Use bias_add_4d from egblas

--- include/etl/impl/egblas/bias_add.hpp
+++ include/etl/impl/egblas/bias_add.hpp
@@ -103,6 +103,86 @@ inline void bias_add_2d(size_t m, size_t n, const double* x, size_t incx, const 
 #endif
 }
 
+/*!
+ * \brief Indicates if EGBLAS has single-precision absarithm.
+ */
+#ifdef EGBLAS_HAS_SBIAS_ADD_4D
+static constexpr bool has_sbias_add_4d = true;
+#else
+static constexpr bool has_sbias_add_4d = false;
+#endif
+
+/*!
+ * \brief Add the 1D bias to to the batched 1D input
+ * \param m The first dimension of x and y
+ * \param n The second dimension of x and y
+ * \param x The vector x (GPU memory)
+ * \param incx The stride of x
+ * \param b The vector x (GPU memory)
+ * \param incb The stride of x
+ * \param y The vector y (GPU memory)
+ * \param incy The stride of y
+ */
+inline void bias_add_4d(size_t m, size_t n, size_t o, size_t p, const float* x, size_t incx, const float* b, size_t incb, float* y, size_t incy){
+#ifdef EGBLAS_HAS_SBIAS_ADD_4D
+    inc_counter("egblas");
+    egblas_sbias_add_4d(m, n, o, p, x, incx, b, incb, y, incy);
+#else
+    cpp_unused(m);
+    cpp_unused(n);
+    cpp_unused(o);
+    cpp_unused(p);
+    cpp_unused(x);
+    cpp_unused(incx);
+    cpp_unused(b);
+    cpp_unused(incb);
+    cpp_unused(y);
+    cpp_unused(incy);
+
+    cpp_unreachable("Invalid call to egblas::bias_add_4d");
+#endif
+}
+
+/*!
+ * \brief Indicates if EGBLAS has double-precision absarithm.
+ */
+#ifdef EGBLAS_HAS_DBIAS_ADD_4D
+static constexpr bool has_dbias_add_4d = true;
+#else
+static constexpr bool has_dbias_add_4d = false;
+#endif
+
+/*!
+ * \brief Add the 1D bias to to the batched 1D input
+ * \param m The first dimension of x and y
+ * \param n The second dimension of x and y
+ * \param x The vector x (GPU memory)
+ * \param incx The stride of x
+ * \param b The vector x (GPU memory)
+ * \param incb The stride of x
+ * \param y The vector y (GPU memory)
+ * \param incy The stride of y
+ */
+inline void bias_add_4d(size_t m, size_t n, size_t o, size_t p, const double* x, size_t incx, const double* b, size_t incb, double* y, size_t incy){
+#ifdef EGBLAS_HAS_DBIAS_ADD_4D
+    inc_counter("egblas");
+    egblas_dbias_add_4d(m, n, o, p, x, incx, b, incb, y, incy);
+#else
+    cpp_unused(m);
+    cpp_unused(n);
+    cpp_unused(o);
+    cpp_unused(p);
+    cpp_unused(x);
+    cpp_unused(incx);
+    cpp_unused(b);
+    cpp_unused(incb);
+    cpp_unused(y);
+    cpp_unused(incy);
+
+    cpp_unreachable("Invalid call to egblas::bias_add_4d");
+#endif
+}
+
 } //end of namespace egblas
 } //end of namespace impl
 } //end of namespace etl
