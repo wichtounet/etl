6c3c450406c1e13ba21194e979a2cde14c433aa1
Baptiste Wicht <baptiste.wicht@gmail.com>
2018-04-30 08:42:07 +0200

    New utility to disable threads of BLAS

---
+++ include/etl/impl/blas/blas.hpp
@@ -0,0 +1,24 @@
+//=======================================================================
+// Copyright (c) 2014-2018 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#pragma once
+
+#ifdef ETL_MKL_MODE
+
+#define disable_blas_threads()                    \
+    auto etl_mkl_threads = mkl_get_max_threads(); \
+    mkl_set_num_threads(1);
+
+#define restore_blas_threads() mkl_set_num_threads(etl_mkl_threads);
+
+#else
+
+#define disable_blas_threads()                    \
+
+#define restore_blas_threads()
+
+#endif
