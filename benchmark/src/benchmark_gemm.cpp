1f7d0ee133de0f1cf902688e4a9cf8309b686323
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-06-03 21:14:42 +0200

    Add new tags to the benchmark

--- benchmark/src/benchmark_gemm.cpp
+++ benchmark/src/benchmark_gemm.cpp
@@ -429,7 +429,7 @@ CPM_BENCH(){
         );
 }
 
-CPM_DIRECT_SECTION_TWO_PASS_NS_PF("r = a *o b (s)", outer_policy,
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("r = a *o b (s) [outer]", outer_policy,
     FLOPS([](size_t d1, size_t d2){ return d1 * d2; }),
     CPM_SECTION_INIT([](size_t d1, size_t d2){ return std::make_tuple(svec(d1), svec(d2), smat(d1, d2)); }),
     CPM_SECTION_FUNCTOR("default", [](svec& a, svec& b, smat& c){ c = etl::outer(a, b); }),
@@ -437,7 +437,7 @@ CPM_DIRECT_SECTION_TWO_PASS_NS_PF("r = a *o b (s)", outer_policy,
     BLAS_SECTION_FUNCTOR("blas", [](svec& a, svec& b, smat& c){ c = selected_helper(etl::outer_impl::BLAS, etl::outer(a, b)); })
 )
 
-CPM_DIRECT_SECTION_TWO_PASS_NS_PF("r = batch_outer(a,b) (s)", outer_policy,
+CPM_DIRECT_SECTION_TWO_PASS_NS_PF("r = batch_outer(a,b) (s) [batch_outer]", outer_policy,
     FLOPS([](size_t d1, size_t d2){ return 128UL * d1 * d2; }),
     CPM_SECTION_INIT([](size_t d1, size_t d2){ return std::make_tuple(smat(128UL, d1), smat(128UL, d2), smat(d1, d2)); }),
     CPM_SECTION_FUNCTOR("default", [](smat& a, smat& b, smat& c){ c = etl::batch_outer(a, b); }),
