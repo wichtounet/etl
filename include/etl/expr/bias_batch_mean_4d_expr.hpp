367a213e9ac3448c8afd514a673cc3a925f2664d
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-06-07 14:18:21 +0200

    Fix mixing CPU/GPU instructions

--- include/etl/expr/bias_batch_mean_4d_expr.hpp
+++ include/etl/expr/bias_batch_mean_4d_expr.hpp
@@ -82,25 +82,29 @@ struct bias_batch_mean_4d_expr : base_temporary_expr_un<bias_batch_mean_4d_expr<
             standard_evaluator::pre_assign_rhs(a);
 
             a.ensure_cpu_up_to_date();
-            a.ensure_gpu_up_to_date();
 
             auto batch_fun_k = [&](const size_t first, const size_t last) {
-                for (size_t k = first; k < last; ++k) {
-                    T mean(0);
+                CPU_SECTION {
+                    for (size_t k = first; k < last; ++k) {
+                        T mean(0);
 
-                    for (size_t b = 0; b < etl::dim<0>(a); ++b) {
-                        mean += sum(a(b)(k));
-                    }
+                        for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                            mean += sum(a(b)(k));
+                        }
 
-                    if constexpr (Mean) {
-                        lhs(k) = mean / N;
-                    } else {
-                        lhs(k) = mean;
+                        if constexpr (Mean) {
+                            lhs(k) = mean / N;
+                        } else {
+                            lhs(k) = mean;
+                        }
                     }
                 }
             };
 
             engine_dispatch_1d_serial(batch_fun_k, 0, K, 2UL);
+
+            lhs.validate_cpu();
+            lhs.invalidate_gpu();
         }
     }
 
@@ -126,23 +130,30 @@ struct bias_batch_mean_4d_expr : base_temporary_expr_un<bias_batch_mean_4d_expr<
 
         check(a, lhs);
 
+        lhs.ensure_cpu_up_to_date();
+
         auto batch_fun_k = [&](const size_t first, const size_t last) {
-            for (size_t k = first; k < last; ++k) {
-                T mean(0);
+            CPU_SECTION {
+                for (size_t k = first; k < last; ++k) {
+                    T mean(0);
 
-                for (size_t b = 0; b < etl::dim<0>(a); ++b) {
-                    mean += sum(a(b)(k));
-                }
+                    for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                        mean += sum(a(b)(k));
+                    }
 
-                if constexpr (Mean) {
-                    lhs(k) += mean / N;
-                } else {
-                    lhs(k) += mean;
+                    if constexpr (Mean) {
+                        lhs(k) += mean / N;
+                    } else {
+                        lhs(k) += mean;
+                    }
                 }
             }
         };
 
         engine_dispatch_1d_serial(batch_fun_k, 0, K, 2UL);
+
+        lhs.validate_cpu();
+        lhs.invalidate_gpu();
     }
 
     /*!
@@ -167,23 +178,30 @@ struct bias_batch_mean_4d_expr : base_temporary_expr_un<bias_batch_mean_4d_expr<
 
         check(a, lhs);
 
+        lhs.ensure_cpu_up_to_date();
+
         auto batch_fun_k = [&](const size_t first, const size_t last) {
-            for (size_t k = first; k < last; ++k) {
-                T mean(0);
+            CPU_SECTION {
+                for (size_t k = first; k < last; ++k) {
+                    T mean(0);
 
-                for (size_t b = 0; b < etl::dim<0>(a); ++b) {
-                    mean += sum(a(b)(k));
-                }
+                    for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                        mean += sum(a(b)(k));
+                    }
 
-                if constexpr (Mean) {
-                    lhs(k) -= mean / N;
-                } else {
-                    lhs(k) -= mean;
+                    if constexpr (Mean) {
+                        lhs(k) -= mean / N;
+                    } else {
+                        lhs(k) -= mean;
+                    }
                 }
             }
         };
 
         engine_dispatch_1d_serial(batch_fun_k, 0, K, 2UL);
+
+        lhs.validate_cpu();
+        lhs.invalidate_gpu();
     }
 
     /*!
@@ -208,23 +226,30 @@ struct bias_batch_mean_4d_expr : base_temporary_expr_un<bias_batch_mean_4d_expr<
 
         check(a, lhs);
 
+        lhs.ensure_cpu_up_to_date();
+
         auto batch_fun_k = [&](const size_t first, const size_t last) {
-            for (size_t k = first; k < last; ++k) {
-                T mean(0);
+            CPU_SECTION {
+                for (size_t k = first; k < last; ++k) {
+                    T mean(0);
 
-                for (size_t b = 0; b < etl::dim<0>(a); ++b) {
-                    mean += sum(a(b)(k));
-                }
+                    for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                        mean += sum(a(b)(k));
+                    }
 
-                if constexpr (Mean) {
-                    lhs(k) *= mean / N;
-                } else {
-                    lhs(k) *= mean;
+                    if constexpr (Mean) {
+                        lhs(k) *= mean / N;
+                    } else {
+                        lhs(k) *= mean;
+                    }
                 }
             }
         };
 
         engine_dispatch_1d_serial(batch_fun_k, 0, K, 2UL);
+
+        lhs.validate_cpu();
+        lhs.invalidate_gpu();
     }
 
     /*!
@@ -249,23 +274,30 @@ struct bias_batch_mean_4d_expr : base_temporary_expr_un<bias_batch_mean_4d_expr<
 
         check(a, lhs);
 
+        lhs.ensure_cpu_up_to_date();
+
         auto batch_fun_k = [&](const size_t first, const size_t last) {
-            for (size_t k = first; k < last; ++k) {
-                T mean(0);
+            CPU_SECTION {
+                for (size_t k = first; k < last; ++k) {
+                    T mean(0);
 
-                for (size_t b = 0; b < etl::dim<0>(a); ++b) {
-                    mean += sum(a(b)(k));
-                }
+                    for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                        mean += sum(a(b)(k));
+                    }
 
-                if constexpr (Mean) {
-                    lhs(k) /= mean / N;
-                } else {
-                    lhs(k) /= mean;
+                    if constexpr (Mean) {
+                        lhs(k) /= mean / N;
+                    } else {
+                        lhs(k) /= mean;
+                    }
                 }
             }
         };
 
         engine_dispatch_1d_serial(batch_fun_k, 0, K, 2UL);
+
+        lhs.validate_cpu();
+        lhs.invalidate_gpu();
     }
 
     /*!
@@ -290,23 +322,30 @@ struct bias_batch_mean_4d_expr : base_temporary_expr_un<bias_batch_mean_4d_expr<
 
         check(a, lhs);
 
+        lhs.ensure_cpu_up_to_date();
+
         auto batch_fun_k = [&](const size_t first, const size_t last) {
-            for (size_t k = first; k < last; ++k) {
-                T mean(0);
+            CPU_SECTION {
+                for (size_t k = first; k < last; ++k) {
+                    T mean(0);
 
-                for (size_t b = 0; b < etl::dim<0>(a); ++b) {
-                    mean += sum(a(b)(k));
-                }
+                    for (size_t b = 0; b < etl::dim<0>(a); ++b) {
+                        mean += sum(a(b)(k));
+                    }
 
-                if constexpr (Mean) {
-                    lhs(k) %= mean / N;
-                } else {
-                    lhs(k) %= mean;
+                    if constexpr (Mean) {
+                        lhs(k) %= mean / N;
+                    } else {
+                        lhs(k) %= mean;
+                    }
                 }
             }
         };
 
         engine_dispatch_1d_serial(batch_fun_k, 0, K, 2UL);
+
+        lhs.validate_cpu();
+        lhs.invalidate_gpu();
     }
 
     /*!
