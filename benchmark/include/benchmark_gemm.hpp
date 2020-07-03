14e19e78d9c98e8e9730aa4fb0fb7a85ee6bc1f5
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-07-03 17:44:40 +0200

    Cleanup code style

--- benchmark/include/benchmark_gemm.hpp
+++ benchmark/include/benchmark_gemm.hpp
@@ -7,29 +7,22 @@
 
 #pragma once
 
-using outer_policy = NARY_POLICY(
-    VALUES_POLICY(10, 50, 100, 500, 1000, 2000, 3000),
-    VALUES_POLICY(10, 50, 100, 500, 1000, 2000, 3000)
-    );
+using outer_policy = NARY_POLICY(VALUES_POLICY(10, 50, 100, 500, 1000, 2000, 3000),
+                                 VALUES_POLICY(10, 50, 100, 500, 1000, 2000, 3000));
 
-using bias_add_policy = NARY_POLICY(
-    VALUES_POLICY(10,  20,  30,  40,  50,  60,  70,  80,  90, 100),
-    VALUES_POLICY(128, 128, 128, 128, 128, 128, 128, 128, 128, 128),
-    VALUES_POLICY(100, 100, 100, 100, 100, 100, 100, 100, 100, 100),
-    VALUES_POLICY(100, 100, 100, 100, 100, 100, 100, 100, 100, 100)
-    );
+using bias_add_policy = NARY_POLICY(VALUES_POLICY(10, 20, 30, 40, 50, 60, 70, 80, 90, 100),
+                                    VALUES_POLICY(128, 128, 128, 128, 128, 128, 128, 128, 128, 128),
+                                    VALUES_POLICY(100, 100, 100, 100, 100, 100, 100, 100, 100, 100),
+                                    VALUES_POLICY(100, 100, 100, 100, 100, 100, 100, 100, 100, 100));
 
-using bias_add_2d_policy = NARY_POLICY(
-    VALUES_POLICY(128, 128, 128, 128, 128, 128, 128, 128, 128, 128),
-    VALUES_POLICY(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
-    );
+using bias_add_2d_policy = NARY_POLICY(VALUES_POLICY(128, 128, 128, 128, 128, 128, 128, 128, 128, 128),
+                                       VALUES_POLICY(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000));
 
-using square_policy = NARY_POLICY(
-    VALUES_POLICY(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000),
-    VALUES_POLICY(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)); using small_square_policy = NARY_POLICY(
-    VALUES_POLICY(100, 150, 200, 250, 300, 350, 400, 450, 500),
-    VALUES_POLICY(100, 150, 200, 250, 300, 350, 400, 450, 500));
+using square_policy = NARY_POLICY(VALUES_POLICY(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000),
+                                  VALUES_POLICY(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000));
 
-using gemv_policy = NARY_POLICY(
-    VALUES_POLICY(50, 100, 250, 500, 750, 1000, 2000, 3000, 4000, 5000, 6000),
-    VALUES_POLICY(50, 100, 250, 500, 750, 1000, 2000, 3000, 4000, 5000, 6000));
+using small_square_policy = NARY_POLICY(VALUES_POLICY(100, 150, 200, 250, 300, 350, 400, 450, 500),
+                                        VALUES_POLICY(100, 150, 200, 250, 300, 350, 400, 450, 500));
+
+using gemv_policy = NARY_POLICY(VALUES_POLICY(50, 100, 250, 500, 750, 1000, 2000, 3000, 4000, 5000, 6000),
+                                VALUES_POLICY(50, 100, 250, 500, 750, 1000, 2000, 3000, 4000, 5000, 6000));
