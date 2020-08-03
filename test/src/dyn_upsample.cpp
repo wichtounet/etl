2cefcdb30bb726b2b43d2a28a174bd5b052ce49a
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-08-03 16:32:40 +0200

    Complete the tests

--- test/src/dyn_upsample.cpp
+++ test/src/dyn_upsample.cpp
@@ -77,6 +77,59 @@ TEMPLATE_TEST_CASE_2("dyn_upsample/2d/2", "[pooling]", Z, float, double) {
     REQUIRE_EQUALS(c(2, 2), 4.0);
 }
 
+TEMPLATE_TEST_CASE_2("dyn_upsample/2d/4", "[pooling]", Z, float, double) {
+    etl::dyn_matrix<Z> a(3, 3, etl::values(1.0, 3.0, 4.0, 5.0, 7.0, 8.0, 5.0, 7.0, 8.0));
+    etl::dyn_matrix<Z> c(4, 4);
+
+    c = etl::upsample_2d(a, 2, 2, 2, 2, 1, 1);
+
+    REQUIRE_EQUALS(c(0, 0), 1.0);
+    REQUIRE_EQUALS(c(0, 1), 3.0);
+    REQUIRE_EQUALS(c(0, 2), 3.0);
+    REQUIRE_EQUALS(c(0, 3), 4.0);
+
+    REQUIRE_EQUALS(c(1, 0), 5.0);
+    REQUIRE_EQUALS(c(1, 1), 7.0);
+    REQUIRE_EQUALS(c(1, 2), 7.0);
+    REQUIRE_EQUALS(c(1, 3), 8.0);
+
+    REQUIRE_EQUALS(c(2, 0), 5.0);
+    REQUIRE_EQUALS(c(2, 1), 7.0);
+    REQUIRE_EQUALS(c(2, 2), 7.0);
+    REQUIRE_EQUALS(c(2, 3), 8.0);
+
+    REQUIRE_EQUALS(c(3, 0), 5.0);
+    REQUIRE_EQUALS(c(3, 1), 7.0);
+    REQUIRE_EQUALS(c(3, 2), 7.0);
+    REQUIRE_EQUALS(c(3, 3), 8.0);
+}
+
+TEMPLATE_TEST_CASE_2("dyn_upsample/2d/5", "[pooling]", Z, float, double) {
+    etl::dyn_matrix<Z> a(3, 3, etl::values(0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0));
+    etl::dyn_matrix<Z> c(2, 2);
+
+    c = etl::upsample_2d(a, 2, 2, 2, 2, 2, 2);
+
+    REQUIRE_EQUALS(c(0, 0), 4.0);
+    REQUIRE_EQUALS(c(0, 1), 4.0);
+
+    REQUIRE_EQUALS(c(1, 0), 4.0);
+    REQUIRE_EQUALS(c(1, 1), 4.0);
+}
+
+TEMPLATE_TEST_CASE_2("dyn_upsample/2d/3", "[pooling]", Z, float, double) {
+    etl::dyn_matrix<Z, 2> a(2, 2, etl::values(1.0, 2.0, 3.0, 4.0));
+    etl::dyn_matrix<Z, 2> c(2, 2);
+
+    c = etl::upsample_2d(a, 2, 2, 2, 2, 1, 1);
+
+    REQUIRE_EQUALS(c(0, 0), 1.0);
+    REQUIRE_EQUALS(c(0, 1), 2.0);
+
+    REQUIRE_EQUALS(c(1, 0), 3.0);
+    REQUIRE_EQUALS(c(1, 1), 4.0);
+}
+
 TEMPLATE_TEST_CASE_2("dyn_upsample/3d/1", "[pooling]", Z, float, double) {
     etl::dyn_matrix<Z, 3> a(1, 2, 2, etl::values(1.0, 2.0, 3.0, 4.0));
     etl::dyn_matrix<Z, 3> c(1, 4, 4);
--- test/src/upsample.cpp
+++ test/src/upsample.cpp
@@ -77,6 +77,59 @@ TEMPLATE_TEST_CASE_2("upsample/2d/2", "[pooling]", Z, float, double) {
     REQUIRE_EQUALS(c(2, 2), 4.0);
 }
 
+TEMPLATE_TEST_CASE_2("upsample/2d/3", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 2, 2> a({1.0, 2.0, 3.0, 4.0});
+    etl::fast_matrix<Z, 2, 2> c;
+
+    c = etl::upsample_2d<2, 2, 2, 2, 1, 1>(a);
+
+    REQUIRE_EQUALS(c(0, 0), 1.0);
+    REQUIRE_EQUALS(c(0, 1), 2.0);
+
+    REQUIRE_EQUALS(c(1, 0), 3.0);
+    REQUIRE_EQUALS(c(1, 1), 4.0);
+}
+
+TEMPLATE_TEST_CASE_2("upsample/2d/4", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 3, 3> a({1.0, 3.0, 4.0, 5.0, 7.0, 8.0, 5.0, 7.0, 8.0});
+    etl::fast_matrix<Z, 4, 4> c;
+
+    c = etl::upsample_2d<2, 2, 2, 2, 1, 1>(a);
+
+    REQUIRE_EQUALS(c(0, 0), 1.0);
+    REQUIRE_EQUALS(c(0, 1), 3.0);
+    REQUIRE_EQUALS(c(0, 2), 3.0);
+    REQUIRE_EQUALS(c(0, 3), 4.0);
+
+    REQUIRE_EQUALS(c(1, 0), 5.0);
+    REQUIRE_EQUALS(c(1, 1), 7.0);
+    REQUIRE_EQUALS(c(1, 2), 7.0);
+    REQUIRE_EQUALS(c(1, 3), 8.0);
+
+    REQUIRE_EQUALS(c(2, 0), 5.0);
+    REQUIRE_EQUALS(c(2, 1), 7.0);
+    REQUIRE_EQUALS(c(2, 2), 7.0);
+    REQUIRE_EQUALS(c(2, 3), 8.0);
+
+    REQUIRE_EQUALS(c(3, 0), 5.0);
+    REQUIRE_EQUALS(c(3, 1), 7.0);
+    REQUIRE_EQUALS(c(3, 2), 7.0);
+    REQUIRE_EQUALS(c(3, 3), 8.0);
+}
+
+TEMPLATE_TEST_CASE_2("upsample/2d/5", "[pooling]", Z, float, double) {
+    etl::fast_matrix<Z, 3, 3> a({0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0});
+    etl::fast_matrix<Z, 2, 2> c;
+
+    c = etl::upsample_2d<2, 2, 2, 2, 2, 2>(a);
+
+    REQUIRE_EQUALS(c(0, 0), 4.0);
+    REQUIRE_EQUALS(c(0, 1), 4.0);
+
+    REQUIRE_EQUALS(c(1, 0), 4.0);
+    REQUIRE_EQUALS(c(1, 1), 4.0);
+}
+
 TEMPLATE_TEST_CASE_2("upsample/3d/1", "[pooling]", Z, float, double) {
     etl::fast_matrix<Z, 1, 2, 2> a({1.0, 2.0, 3.0, 4.0});
     etl::fast_matrix<Z, 1, 4, 4> c;
