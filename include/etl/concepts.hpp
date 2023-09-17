2ffb8376d78d02a6ed7d82e65bd683e847d49f14
Baptiste Wicht <baptiste.wicht@gmail.com>
2023-09-17 19:15:54 +0200

    Better concepts

--- include/etl/concepts.hpp
+++ include/etl/concepts.hpp
@@ -19,8 +19,17 @@ namespace concepts_detail {
 template<typename T>
 concept etl_expr = decay_traits<T>::is_etl;
 
+template <typename T>
+concept etl_complex = cpp::is_specialization_of_v<etl::complex, std::decay_t<T>>;
+
+template <typename T>
+concept std_complex = cpp::is_specialization_of_v<std::complex, std::decay_t<T>>;
+
+template <typename T>
+concept complex_c = etl_complex<T> || std_complex<T>;
+
 template<typename T>
-concept etl_complex_expr = etl_expr<T> && is_complex_t<value_t<T>>;
+concept etl_complex_expr = etl_expr<T> && complex_c<value_t<T>>;
 
 template <typename T>
 concept unary_expr_c = cpp::is_specialization_of_v<etl::unary_expr, std::decay_t<T>>;
@@ -34,10 +43,26 @@ concept slice_view_c = cpp::is_specialization_of_v<etl::slice_view, std::decay_t
 template <typename T>
 concept dyn_matrix_view_c = traits_detail::is_dyn_matrix_view<T>::value;
 
-// TODO Translate all this to concepts
 template <typename T>
-concept etl_value_class =
-    is_fast_matrix<T> || is_custom_fast_matrix<T> || is_dyn_matrix<T> || is_custom_dyn_matrix<T> || is_sparse_matrix<T> || is_gpu_dyn_matrix<T>;
+concept fast_matrix_c = traits_detail::is_fast_matrix_impl<std::decay_t<T>>::value;
+
+template <typename T>
+concept custom_fast_matrix_c = traits_detail::is_custom_fast_matrix_impl<std::decay_t<T>>::value;
+
+template <typename T>
+concept dyn_matrix_c = traits_detail::is_dyn_matrix_impl<std::decay_t<T>>::value;
+
+template <typename T>
+concept gpu_dyn_matrix_c = traits_detail::is_gpu_dyn_matrix_impl<std::decay_t<T>>::value;
+
+template <typename T>
+concept custom_dyn_matrix_c = traits_detail::is_custom_dyn_matrix_impl<std::decay_t<T>>::value;
+
+template <typename T>
+concept sparse_matrix_c = traits_detail::is_sparse_matrix_impl<std::decay_t<T>>::value;
+
+template <typename T>
+concept etl_value_class = fast_matrix_c<T> || custom_fast_matrix_c<T> || dyn_matrix_c<T> || custom_dyn_matrix_c<T> || sparse_matrix_c<T> || gpu_dyn_matrix_c<T>;
 
 template <typename T>
 concept simple_lhs = etl_value_class<T> || unary_expr_c<T> || sub_view_c<T> || slice_view_c<T> || dyn_matrix_view_c<T>;
